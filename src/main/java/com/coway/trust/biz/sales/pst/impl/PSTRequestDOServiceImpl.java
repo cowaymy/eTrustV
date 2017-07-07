ee9772844b53f8a82ac30d870f002e10db91593f
dev06 <vacant@coway.com.my>
2017-07-07 06:14:38 +0000

    [Sales] PST Request DO 프로그램 샘플 작성

---
+++ src/main/java/com/coway/trust/biz/sales/pst/impl/PSTRequestDOServiceImpl.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.coway.trust.biz.sales.pst.impl;
+
+import com.coway.trust.biz.sales.pst.PSTRequestDOService;
+
+/**
+ * @author Yunseok_Jang
+ *
+ */
+public class PSTRequestDOServiceImpl implements PSTRequestDOService {
+
+}
---
+++ src/main/java/com/coway/trust/web/sales/pst/PSTRequestDOController.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.coway.trust.web.sales.pst;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.coway.trust.biz.sales.pst.PSTRequestDOService;
+
+/**
+ * @author Yunseok_Jang
+ *
+ */
+@Controller
+@RequestMapping(value = "/sales/pst")
+public class PSTRequestDOController {
+
+	private static final Logger logger = LoggerFactory.getLogger(PSTRequestDOController.class);
+	
+	@Resource(name = "PSTRequestDOService")
+	private PSTRequestDOService pstRequestDOService;
+	
+}
