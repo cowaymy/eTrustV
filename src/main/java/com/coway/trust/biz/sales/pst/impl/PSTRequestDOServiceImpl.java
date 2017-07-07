b4bf1196127a35a04291a3f9d9faeeef25f0f16f
dev02 <vacant@coway.com.my>
2017-07-07 06:39:00 +0000

    - 오류 수정.

--- src/main/java/com/coway/trust/biz/sales/pst/impl/PSTRequestDOServiceImpl.java
+++ src/main/java/com/coway/trust/biz/sales/pst/impl/PSTRequestDOServiceImpl.java
@@ -3,12 +3,15 @@
  */
 package com.coway.trust.biz.sales.pst.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.coway.trust.biz.sales.pst.PSTRequestDOService;
 
 /**
  * @author Yunseok_Jang
  *
  */
+@Service("PSTRequestDOService")
 public class PSTRequestDOServiceImpl implements PSTRequestDOService {
 
 }
