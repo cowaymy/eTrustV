df9bc8588198a52acfc414252d20b454f74011b5
dev06 <vacant@coway.com.my>
2017-07-10 08:10:20 +0000

    [Sales] PST Request DO 프로그램 샘플 작성

--- src/main/java/com/coway/trust/web/sales/SalesConstants.java
+++ src/main/java/com/coway/trust/web/sales/SalesConstants.java
@@ -1,5 +1,10 @@
 package com.coway.trust.web.sales;
 
 public class SalesConstants {
-
+	
+	//COMMON CODE : PST_TRANSIT_TYPE
+	public static final String SALES_PSTREQ = "1144";
+	public static final String SALES_PSTDO  = "1145";
+	public static final String SALES_PSTCAN = "1146";
+	
 }
