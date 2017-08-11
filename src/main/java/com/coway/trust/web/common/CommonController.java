6448d6ff310f4225996408c9ae0819bcb1675eab
dev02 <vacant@coway.com.my>
2017-08-11 08:03:10 +0000

    - CommonController 분리 작업

--- src/main/java/com/coway/trust/web/common/CommonController.java
+++ src/main/java/com/coway/trust/web/common/CommonController.java
@@ -2,7 +2,6 @@ package com.coway.trust.web.common;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,21 +15,15 @@ import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.coway.trust.AppConstants;
 import com.coway.trust.biz.common.CommonService;
 import com.coway.trust.cmmn.exception.ApplicationException;
-import com.coway.trust.cmmn.model.ReturnMessage;
-import com.coway.trust.cmmn.model.SessionVO;
 import com.coway.trust.config.DatabaseDrivenMessageSource;
 import com.coway.trust.util.CommonUtils;
 import com.coway.trust.util.EgovResourceCloseHelper;
