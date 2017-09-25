8940f4e89fbd85b0bfb30746fb08bd349364dc06
dev16 <vacant@coway.com.my>
2017-09-25 09:36:26 +0000

    Billing Void  by 2017.09.25유명현

---
+++ src/main/java/com/coway/trust/biz/payment/invoice/service/BillingVoidService.java
@@ -0,0 +1,24 @@
+package com.coway.trust.biz.payment.invoice.service;
+
+import java.util.List;
+import java.util.Map;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+public interface BillingVoidService {
+    	 
+		/**
+      	 * selectStatementView 조회
+      	 * @param params
+      	 * @return
+      	 */
+		EgovMap selectStatementView(Map<String, Object> params);
+		
+		/**
+      	 * selectInvoiceDetailList 조회
+      	 * @param params
+      	 * @return
+      	 */
+		List<EgovMap> selectInvoiceDetailList(Map<String, Object> params);
+		
+      
+}
