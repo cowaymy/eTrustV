c096a29b9804720b3ce611829812d552f48a2975
dev02 <vacant@coway.com.my>
2017-11-02 02:46:12 +0000

    수정.

--- src/main/java/com/coway/trust/biz/common/type/FileType.java
+++ src/main/java/com/coway/trust/biz/common/type/FileType.java
@@ -1,7 +1,7 @@
 package com.coway.trust.biz.common.type;
 
 public enum FileType {
-	WEB("W"), WEB_DIRECT_RESOURCE("WR"), MOBILE("M"), CALL_CENTER("C");
+	WEB("W"), WEB_DIRECT_RESOURCE("D"), MOBILE("M"), CALL_CENTER("C");
 
 	private String code = "";
 
