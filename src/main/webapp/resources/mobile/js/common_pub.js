e5124bb09b1b9a1e8f17d544e6301faa6705324b
dev11 <vacant@coway.com.my>
2017-11-03 08:51:02 +0000

    - 모바일 레프트 메뉴 스크립트 수정 - jQuery UI Autocomplete 추가 : ~\html_common\00_template_basic.html - ~\Payment \ Fund Trasfer.html : input 크기 수정(auto)

--- src/main/webapp/resources/mobile/js/common_pub.js
+++ src/main/webapp/resources/mobile/js/common_pub.js
@@ -1,6 +1,14 @@
 $(function(){
 	"use strict";
 	/*
+	 * Document Ready
+	 */
+	$(document).ready(function(){
+		// Left Navigation
+		lnbFn();
+	});
+
+	/*
 	 * Left Menu Tree Event
 	 */
 	$(document).on(
@@ -10,32 +18,52 @@ $(function(){
 		var thisUl=$(this).parent().parent("ul").find("li ul");
 		if(thisHasChild>0){
 			if(thisLi.children("ul").is(":visible")){
-				$(this).removeClass("on")
-				thisLi.children("ul").slideUp("500");
+				$(this).removeClass("on");
+				thisLi.children("ul").slideUp(500);
 			}else{
-				thisUl.parent("li").children("a").removeClass("on")
-				thisUl.slideUp("500");
-				$(this).addClass("on")
-				thisLi.children("ul").slideDown("500");
+				thisUl.parent("li").children("a").removeClass("on");
+				thisUl.slideUp(500);
+				$(this).addClass("on");
+				thisLi.children("ul").slideDown(500);
 			}
 			//$(this).removeClass("on");
 			return false;
-		}else{
 		}
 	});
-
+	
 	/*
-	 * Document Ready
+	 * Left Navigation Function
 	 */
-	$(document).ready(function(){
-		
-		/*
-		 * Left Navigation
-		 * 닫힘(컨테이너크기), 열림(0), 속도(ms)
-		 */
-		lnbFn(-375,0,300);
+	function lnbFn(){
+		var navBg = $(".nav_wrap"),
+			navContainer = $(".nav_container"),
+			navBtn = $(".btn-nav"),
+			navClosed = $(".btn-close"),
+			navExtended = 0,
+			navDecreased = -(navBg.width()),
+			animateSpeed = 300;
 
-		/* 레프트메뉴 3뎁스 하위메뉴 (4뎁스)가 있을 경우 앞에 화살표 표시 on/off */ 
+		navBtn.on("click",function(){
+			navBg.show();
+			navContainer.css("left",navDecreased);
+			navContainer.animate({
+				left : navExtended
+			},animateSpeed);
+		});
+		navClosed.on("click",function(){
+			navContainer.animate({
+				left : navDecreased
+			},animateSpeed,function(){
+				navBg.hide();
+			});
+		});
+		navBg.on("click",function(evt){
+			var target = $(evt.target);
+			if (target.is(navBg)) {
+			  navClosed.trigger("click");
+			}
+		});
+		//레프트메뉴 3뎁스 하위메뉴 (4뎁스)가 있을 경우 앞에 화살표 표시 on/off
 		var depth3 = $(".inb_menu").children("li").children("ul").children("li").children("ul").children("li");
 
 		for( var i = 0; i < depth3.length; i++ ){
@@ -43,39 +71,5 @@ $(function(){
 				depth3.eq(i).children("a").addClass("none");
 			}
 		}
-	})
-	
-	/*
-	 * Left Navigation Function
-	 */
-	function lnbFn(decreased,extended,speed){
-		var navBtn = $('.btn-nav'),
-			navClosed = $('.btn-close'),
-			navBg = $('.nav_wrap'),
-			navContainer = $('.nav_container'),
-			navExtended = extended,
-			navDecreased = decreased,
-			animateSpeed = speed;
-
-		navBtn.on('click',function(){
-			navBg.show();
-			navContainer.css('left',navDecreased);
-			navContainer.animate({
-				left : navExtended
-			},animateSpeed);
-		});
-		navClosed.on('click',function(){
-			navContainer.animate({
-				left : navDecreased
-			},animateSpeed,function(){
-				navBg.hide();
-			});
-		});
-		navBg.on('click',function(evt){
-			var target = $(evt.target);
-			if (target.is(navBg)) {
-			  navClosed.trigger("click");
-			} 
-		})
-	}
+	};
 });
