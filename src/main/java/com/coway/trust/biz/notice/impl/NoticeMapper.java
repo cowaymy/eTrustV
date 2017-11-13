bb53819c00e35e9d33db4464ed0a347f0da3218e
dev14 <vacant@coway.com.my>
2017-11-13 02:00:01 +0000

    NoticeMapper

---
+++ src/main/java/com/coway/trust/biz/notice/impl/NoticeMapper.java
@@ -0,0 +1,30 @@
+package com.coway.trust.biz.notice.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.coway.trust.biz.notice.NoticeVO;
+
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+@Mapper("noticeMapper")
+public interface NoticeMapper {
+
+	List<EgovMap> noticeList(Map<String, Object> params) throws Exception;
+	
+	void insertNotice(Map<String, Object> params) throws Exception; 
+	
+	List<EgovMap> selectCodeList(Map<String, Object> params) throws Exception;
+	
+	int getNtceNOSeq() throws Exception;
+	
+	EgovMap noticeInfo(Map<String, Object> params) throws Exception;
+	
+	void updateNotice(Map<String, Object> params) throws Exception;
+	
+	void upViewCnt(Map<String, Object> params) throws Exception;
+	
+//	EgovMap getAttachmentFileInfo(Map<String, Object> params) throws Exception;
+	
+}
