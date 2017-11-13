6dc57dd592721c041cf3c8b00b78c48e7cef239b
dev14 <vacant@coway.com.my>
2017-11-13 02:00:50 +0000

    NoticeService

---
+++ src/main/java/com/coway/trust/biz/notice/NoticeService.java
@@ -0,0 +1,27 @@
+package com.coway.trust.biz.notice;
+
+import java.util.List;
+import java.util.Map;
+
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+public interface NoticeService {
+	
+	List<EgovMap> selectCodeList(Map<String, Object> params) throws Exception;
+	
+	List<EgovMap> noticeList(Map<String, Object> params) throws Exception;
+	
+	int getNtceNOSeq() throws Exception;
+	
+	void insertNotice(Map<String, Object> params) throws Exception;
+	
+	EgovMap noticeInfo(Map<String, Object> params)throws Exception;
+	
+	void deleteNotice(int ntceNo) throws Exception;
+	
+	void updateNotice(Map<String, Object> params) throws Exception;
+	
+	void upViewCnt(Map<String, Object> params) throws Exception;
+	
+//	EgovMap getAttachmentFileInfo(Map<String, Object> params) throws Exception;
+}
