b3abebc8cd51925b0217586c3ab0a89aec497810
etc11 <vacant@coway.com.my>
2018-10-25 12:22:33 +0000

    Added exception search listing for SMSOO-54547

--- src/main/java/com/coway/trust/web/eAccounting/staffClaim/StaffClaimController.java
+++ src/main/java/com/coway/trust/web/eAccounting/staffClaim/StaffClaimController.java
@@ -43,57 +43,59 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 public class StaffClaimController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PettyCashController.class);
-	
+
 	@Value("${app.name}")
 	private String appName;
-	
+
 	@Value("${web.resource.upload.file}")
 	private String uploadDir;
-	
+
 	// DataBase message accessor....
 	@Autowired
 	private MessageSourceAccessor messageAccessor;
-	
+
 	@Autowired
 	private StaffClaimService staffClaimService;
-	
+
 	@Autowired
 	private StaffClaimApplication staffClaimApplication;
-	
+
 	@Autowired
 	private WebInvoiceService webInvoiceService;
-	
+
 	@RequestMapping(value = "/staffClaimMgmt.do")
 	public String staffClaimMgmt(ModelMap model) {
 		return "eAccounting/staffClaim/staffClaim";
 	}
-	
+
 	@RequestMapping(value = "/selectStaffClaimList.do")
 	public ResponseEntity<List<EgovMap>> selectStaffClaimList(@RequestParam Map<String, Object> params, HttpServletRequest request, ModelMap model, SessionVO sessionVO) {
-		
+
 		String costCentr = CommonUtils.isEmpty(sessionVO.getCostCentr()) ? "0" : sessionVO.getCostCentr();
 		if(!"A1101".equals(costCentr)) {
-			params.put("loginUserId", sessionVO.getUserId());
+	         if(!"54547".equals(Integer.toString(sessionVO.getUserId()))) {
+	                params.put("loginUserId", sessionVO.getUserId());
+	         }
 		}
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		String[] appvPrcssStus = request.getParameterValues("appvPrcssStus");
-		
+
 		params.put("appvPrcssStus", appvPrcssStus);
-		
+
 		List<EgovMap> claimList = staffClaimService.selectStaffClaimList(params);
-		
+
 		return ResponseEntity.ok(claimList);
 	}
-	
+
 	@RequestMapping(value = "/newStaffClaimPop.do")
 	public String newStaffClaimPop(@RequestParam Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		List<EgovMap> taxCodeFlagList = staffClaimService.selectTaxCodeStaffClaimFlag();
-		
+
 		model.addAttribute("callType", params.get("callType"));
 		model.addAttribute(CommonConstants.USER_ID, sessionVO.getUserId());
 		model.addAttribute("userName", sessionVO.getUserName());
@@ -101,78 +103,78 @@ public class StaffClaimController {
 		model.addAttribute("taxCodeList", new Gson().toJson(taxCodeFlagList));
 		return "eAccounting/staffClaim/staffClaimNewExpensesPop";
 	}
-	
+
 	@RequestMapping(value = "/selectTaxCodeStaffClaimFlag.do", method = RequestMethod.GET)
 	public ResponseEntity<List<EgovMap>> selectTaxCodeStaffClaimFlag(Model model) {
-		
+
 		List<EgovMap> taxCodeFlagList = staffClaimService.selectTaxCodeStaffClaimFlag();
-		
+
 		return ResponseEntity.ok(taxCodeFlagList);
 	}
-	
+
 	@RequestMapping(value = "/attachFileUpload.do", method = RequestMethod.POST)
 	public ResponseEntity<ReturnMessage> attachFileUpload(MultipartHttpServletRequest request, @RequestParam Map<String, Object> params, Model model, SessionVO sessionVO) throws Exception {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		List<EgovFormBasedFileVo> list = EgovFileUploadUtil.uploadFiles(request, uploadDir,
 				File.separator + "eAccounting" + File.separator + "staffClaim", AppConstants.UPLOAD_MAX_FILE_SIZE, true);
-		
+
 		LOGGER.debug("list.size : {}", list.size());
 
 		params.put(CommonConstants.USER_ID, sessionVO.getUserId());
 
 		// serivce 에서 파일정보를 가지고, DB 처리.
 		staffClaimApplication.insertStaffClaimAttachBiz(FileVO.createList(list), FileType.WEB_DIRECT_RESOURCE,  params);
-		
+
 		params.put("attachFiles", list);
-		
+
 		ReturnMessage message = new ReturnMessage();
 		message.setCode(AppConstants.SUCCESS);
 		message.setData(params);
 		message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		
+
 		return ResponseEntity.ok(message);
 	}
-	
+
 	@RequestMapping(value = "/insertStaffClaimExp.do", method = RequestMethod.POST)
 	public ResponseEntity<ReturnMessage> insertStaffClaimExp(@RequestBody Map<String, Object> params, Model model, SessionVO sessionVO) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		params.put(CommonConstants.USER_ID, sessionVO.getUserId());
 		params.put("userName", sessionVO.getUserName());
-		
+
 		// TODO insert
 		staffClaimService.insertStaffClaimExp(params);
-		
+
 		ReturnMessage message = new ReturnMessage();
 		message.setCode(AppConstants.SUCCESS);
 		message.setData(params);
 		message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		
+
 		return ResponseEntity.ok(message);
 	}
-	
+
 	@RequestMapping(value = "/selectStaffClaimItemList.do", method = RequestMethod.GET)
 	public ResponseEntity<List<EgovMap>> selectStaffClaimItemList(@RequestParam Map<String, Object> params, ModelMap model) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		List<EgovMap> itemList = staffClaimService.selectStaffClaimItems((String) params.get("clmNo"));
-		
+
 		return ResponseEntity.ok(itemList);
 	}
-	
+
 	@RequestMapping(value = "/viewStaffClaimPop.do")
 	public String viewStaffClaimPop(@RequestParam Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		// TODO selectExpenseItems
 		List<EgovMap> itemList = staffClaimService.selectStaffClaimItems((String) params.get("clmNo"));
 		List<EgovMap> taxCodeFlagList = staffClaimService.selectTaxCodeStaffClaimFlag();
-		
+
 		model.addAttribute("callType", params.get("callType"));
 		model.addAttribute(CommonConstants.USER_ID, sessionVO.getUserId());
 		model.addAttribute("userName", sessionVO.getUserName());
@@ -185,17 +187,17 @@ public class StaffClaimController {
 		}
 		return "eAccounting/staffClaim/staffClaimViewExpensesPop";
 	}
-	
+
 	@RequestMapping(value = "/selectStaffClaimInfo.do", method = RequestMethod.GET)
 	public ResponseEntity<EgovMap> selectStaffClaimInfo(@RequestParam Map<String, Object> params, ModelMap model) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		EgovMap info = staffClaimService.selectStaffClaimInfo(params);
 		List<EgovMap> itemGrp = staffClaimService.selectStaffClaimItemGrp(params);
-		
+
 		info.put("itemGrp", itemGrp);
-		
+
 		String atchFileGrpId = String.valueOf(info.get("atchFileGrpId"));
 		LOGGER.debug("atchFileGrpId =====================================>>  " + atchFileGrpId);
 		// atchFileGrpId db column type number -> null인 경우 nullPointExecption (String.valueOf 처리)
@@ -204,121 +206,121 @@ public class StaffClaimController {
 			List<EgovMap> attachList = staffClaimService.selectAttachList(atchFileGrpId);
 			info.put("attachList", attachList);
 		}
-		
+
 		return ResponseEntity.ok(info);
 	}
-	
+
 	@RequestMapping(value = "/attachFileUpdate.do", method = RequestMethod.POST)
 	public ResponseEntity<ReturnMessage> attachFileUpdate(MultipartHttpServletRequest request, @RequestParam Map<String, Object> params, Model model, SessionVO sessionVO) throws Exception {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		List<EgovFormBasedFileVo> list = EgovFileUploadUtil.uploadFiles(request, uploadDir,
 				File.separator + "eAccounting" + File.separator + "staffClaim", AppConstants.UPLOAD_MAX_FILE_SIZE, true);
-		
+
 		LOGGER.debug("list.size : {}", list.size());
 
 		params.put(CommonConstants.USER_ID, sessionVO.getUserId());
 
 		// serivce 에서 파일정보를 가지고, DB 처리.
 		staffClaimApplication.updateStaffClaimAttachBiz(FileVO.createList(list), FileType.WEB_DIRECT_RESOURCE, params);
-		
+
 		params.put("attachFiles", list);
-		
+
 		ReturnMessage message = new ReturnMessage();
 		message.setCode(AppConstants.SUCCESS);
 		message.setData(params);
 		message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		
+
 		return ResponseEntity.ok(message);
 	}
-	
+
 	@RequestMapping(value = "/updateStaffClaimExp.do", method = RequestMethod.POST)
 	public ResponseEntity<ReturnMessage> updateStaffClaimExp(@RequestBody Map<String, Object> params, Model model, SessionVO sessionVO) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		params.put(CommonConstants.USER_ID, sessionVO.getUserId());
 		params.put("userName", sessionVO.getUserName());
-		
+
 		// TODO insert
 		staffClaimService.updateStaffClaimExp(params);
-		
+
 		ReturnMessage message = new ReturnMessage();
 		message.setCode(AppConstants.SUCCESS);
 		message.setData(params);
 		message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		
+
 		return ResponseEntity.ok(message);
 	}
-	
+
 	@RequestMapping(value = "/approveLinePop.do")
 	public String approveLinePop(ModelMap model) {
 		return "eAccounting/staffClaim/approveLinePop";
 	}
-	
+
 	@RequestMapping(value = "/registrationMsgPop.do")
 	public String registrationMsgPop(ModelMap model) {
 		return "eAccounting/staffClaim/registrationMsgPop";
 	}
-	
+
 	@RequestMapping(value = "/approveLineSubmit.do", method = RequestMethod.POST)
 	public ResponseEntity<ReturnMessage> approveLineSubmit(@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		String appvPrcssNo = webInvoiceService.selectNextAppvPrcssNo();
 		params.put("appvPrcssNo", appvPrcssNo);
 		params.put(CommonConstants.USER_ID, sessionVO.getUserId());
 		params.put("userName", sessionVO.getUserName());
-		
+
 		// TODO
 		staffClaimService.insertApproveManagement(params);
-		
+
 		ReturnMessage message = new ReturnMessage();
 		message.setCode(AppConstants.SUCCESS);
 		message.setData(params);
 		message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		
+
 		return ResponseEntity.ok(message);
 	}
-	
+
 	@RequestMapping(value = "/completedMsgPop.do", method = RequestMethod.POST)
 	public String completedMsgPop(@RequestParam Map<String, Object> params, ModelMap model) {
 		model.addAttribute("callType", params.get("callType"));
 		model.addAttribute("clmNo", params.get("clmNo"));
 		return "eAccounting/staffClaim/completedMsgPop";
 	}
-	
+
 	@RequestMapping(value = "/deleteStaffClaimExp.do", method = RequestMethod.POST)
 	public ResponseEntity<ReturnMessage> deleteStaffClaimExp(@RequestBody Map<String, Object> params, Model model, SessionVO sessionVO) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		params.put(CommonConstants.USER_ID, sessionVO.getUserId());
 		params.put("userName", sessionVO.getUserName());
-		
+
 		// TODO insert
 		staffClaimApplication.deleteStaffClaimAttachBiz(FileType.WEB_DIRECT_RESOURCE, params);
-		
+
 		ReturnMessage message = new ReturnMessage();
 		message.setCode(AppConstants.SUCCESS);
 		message.setData(params);
 		message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		
+
 		return ResponseEntity.ok(message);
 	}
-	
+
 	@RequestMapping(value = "/getAppvItemOfClmUn.do", method = RequestMethod.POST)
 	public ResponseEntity<ReturnMessage> getAppvItemOfClmUn(@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		EgovMap info = staffClaimService.selectStaffClaimInfoForAppv(params);
 		List<EgovMap> itemGrp = staffClaimService.selectStaffClaimItemGrpForAppv(params);
-		
+
 		info.put("itemGrp", itemGrp);
-		
+
 		String atchFileGrpId = String.valueOf(info.get("atchFileGrpId"));
 		LOGGER.debug("atchFileGrpId =====================================>>  " + atchFileGrpId);
 		// atchFileGrpId db column type number -> null인 경우 nullPointExecption (String.valueOf 처리)
@@ -327,24 +329,24 @@ public class StaffClaimController {
 			List<EgovMap> attachList = staffClaimService.selectAttachList(atchFileGrpId);
 			info.put("attachList", attachList);
 		}
-		
+
 		ReturnMessage message = new ReturnMessage();
 		message.setCode(AppConstants.SUCCESS);
 		message.setData(info);
 		message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		
+
 		return ResponseEntity.ok(message);
 	}
-	
+
 	@RequestMapping(value = "/checkOnceAMonth.do", method = RequestMethod.POST)
 	public ResponseEntity<ReturnMessage> checkOnceAMonth(@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
-		
+
 		LOGGER.debug("params =====================================>>  " + params);
-		
+
 		int cnt = staffClaimService.checkOnceAMonth(params);
-		
+
 		ReturnMessage message = new ReturnMessage();
-		
+
 		if(cnt > 0) {
 			message.setCode(AppConstants.FAIL);
 			message.setMessage("You can only request once a month.");
@@ -352,7 +354,7 @@ public class StaffClaimController {
 			message.setCode(AppConstants.SUCCESS);
 		}
 		message.setData(cnt);
-		
+
 		return ResponseEntity.ok(message);
 	}
 
