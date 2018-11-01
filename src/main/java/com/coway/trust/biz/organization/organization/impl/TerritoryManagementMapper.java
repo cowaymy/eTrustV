0beb1860a336e02d37a0883fa561c8724d9cf3d4
etc02 <vacant@coway.com.my>
2018-11-01 10:46:05 +0000

    Data Insert for SO

--- src/main/java/com/coway/trust/biz/organization/organization/impl/TerritoryManagementMapper.java
+++ src/main/java/com/coway/trust/biz/organization/organization/impl/TerritoryManagementMapper.java
@@ -12,32 +12,36 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 public interface TerritoryManagementMapper {
 
 	List<EgovMap> selectList(Map<String, Object> params);
-	
+
 	EgovMap  cody42Vaild(Map<String, Object> params);
 	EgovMap  dream43Vaild(Map<String, Object> params);
-	
+
 	void insertCody(Map<String, Object> params);
 
 	String selectRequestNo();
-	
+
 	List<EgovMap> selectTerritory(Map<String, Object> params);
-	
+
 	List<EgovMap> selectMagicAddress(Map<String, Object> params);
-	
+
 	List<EgovMap> select19M(Map<String, Object> params);
-	
+
 	void updateSYS0064M(EgovMap params);
-	
+
 	void updateORG0019M(EgovMap params);
-	
+
 	void updateORG0019MFlag(EgovMap params);
-	
+
 	void insertDreamServiceCenter(Map<String, Object> params);
+
+	void insertSalesOffice(Map<String, Object> params);
 
 	void updateSYS0064MDream(EgovMap params);
 
+	void updateSYS0064MSO(EgovMap params);
+
 	List<EgovMap> selectBranchCode(Map<String, Object> params);
-	
+
 	List<EgovMap> selectState(Map<String, Object> params);
 
 	List<EgovMap> selectCurrentTerritory(Map<String, Object> params);
--- src/main/java/com/coway/trust/biz/organization/organization/impl/TerritoryManagementServiceImpl.java
+++ src/main/java/com/coway/trust/biz/organization/organization/impl/TerritoryManagementServiceImpl.java
@@ -25,10 +25,10 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 @Service("territoryManagementService")
 public class TerritoryManagementServiceImpl extends EgovAbstractServiceImpl implements TerritoryManagementService{
 	private static final Logger logger = LoggerFactory.getLogger(TerritoryManagementService.class);
-	
+
 	@Resource(name = "territoryManagementMapper")
 	private TerritoryManagementMapper territoryManagementMapper;
-	
+
 	@Resource(name = "memberListMapper")
 	private MemberListMapper memberListMapper;
 
@@ -36,17 +36,17 @@ public class TerritoryManagementServiceImpl extends EgovAbstractServiceImpl impl
 	public List<EgovMap> selectList(Map<String, Object> params) {
 		return territoryManagementMapper.selectList(params);
 	}
-	
+
 	@Override
 	public List<EgovMap> selectTerritory(Map<String, Object> params) {
 		return territoryManagementMapper.selectTerritory(params);
 	}
-	
+
 	@Override
 	public List<EgovMap> selectMagicAddress(Map<String, Object> params) {
 		return territoryManagementMapper.selectMagicAddress(params);
 	}
-	
+
 	@Override
 	public EgovMap uploadVaild(Map<String, Object> params, SessionVO sessionVO) {
 
@@ -108,6 +108,23 @@ public class TerritoryManagementServiceImpl extends EgovAbstractServiceImpl impl
 				territoryManagementMapper.insertDreamServiceCenter(list43);
 			}
 			break;
+		case "45": // Sales Office
+
+			Map<String, Object> list45 = new HashMap<>();
+
+			for (int i = 0; i <= page; i++) {
+				start = i * size;
+				end = size;
+
+				if(i == page){
+					end = list.size();
+				}
+
+				list45.put("list",
+						list.stream().skip(start).limit(end).collect(Collectors.toCollection(ArrayList::new)));
+				territoryManagementMapper.insertSalesOffice(list45);
+			}
+			break;
 		default:
 			logger.info("Unknown type...... bType : {}", bType);
 			break;
@@ -163,15 +180,25 @@ public class TerritoryManagementServiceImpl extends EgovAbstractServiceImpl impl
     		if(select19M.size() > 0){
     			for(int i=0; i<select19M.size(); i++){
     				territoryManagementMapper.updateSYS0064M(select19M.get(i));
-    				//전에 썼던 것을 N으로 바꿔줘야된다(area_id로 이전 데이터가 쌓이니까 
+    				//전에 썼던 것을 N으로 바꿔줘야된다(area_id로 이전 데이터가 쌓이니까
     				//area_id로 n을 주고 밑에서 area_id랑 reqstNo로 구분해 y로 바꿔주므로 n으로 바꿈)
     				territoryManagementMapper.updateORG0019MFlag(select19M.get(i));
     				//19M에 AVAIL_FLAG 'Y'로 변경, CONFIRM STUS 4
     				territoryManagementMapper.updateORG0019M(select19M.get(i));
     			}
-    			
+
     		}
-    		
+
+		}else if(params.get("brnchType").toString().equals("45")){
+    		List<EgovMap> select19M = territoryManagementMapper.select19M(params);
+    		logger.debug("select19M {}", select19M);
+    		if(select19M.size() > 0){
+    			for(int i=0; i<select19M.size(); i++){
+    				territoryManagementMapper.updateSYS0064MSO(select19M.get(i));
+    				territoryManagementMapper.updateORG0019MFlag(select19M.get(i));
+    				territoryManagementMapper.updateORG0019M(select19M.get(i));
+    			}
+    		}
 		}else{
 			//reqstNo로 19M에 데이터를 다 가져옴
     		List<EgovMap> select19M = territoryManagementMapper.select19M(params);
@@ -180,28 +207,28 @@ public class TerritoryManagementServiceImpl extends EgovAbstractServiceImpl impl
     		if(select19M.size() > 0){
     			for(int i=0; i<select19M.size(); i++){
     				territoryManagementMapper.updateSYS0064MDream(select19M.get(i));
-    				//전에 썼던 것을 N으로 바꿔줘야된다(area_id로 이전 데이터가 쌓이니까 
+    				//전에 썼던 것을 N으로 바꿔줘야된다(area_id로 이전 데이터가 쌓이니까
     				//area_id로 n을 주고 밑에서 area_id랑 reqstNo로 구분해 y로 바꿔주므로 n으로 바꿈)
     				territoryManagementMapper.updateORG0019MFlag(select19M.get(i));
     				//19M에 AVAIL_FLAG 'Y'로 변경, CONFIRM STUS 4
     				territoryManagementMapper.updateORG0019M(select19M.get(i));
     			}
-    			
+
     		}
-    		
+
 		}
 		return true;
 	}
-	
+
 	public EgovMap getDocNo(String docNoId){
 		int tmp = Integer.parseInt(docNoId);
 		String docNo = "";
 		EgovMap selectDocNo = memberListMapper.selectDocNo(docNoId);
 		logger.debug("selectDocNo : {}",selectDocNo);
 		String prefix = "";
-		
+
 		if(Integer.parseInt((String) selectDocNo.get("docNoId").toString()) == tmp){
-			
+
 			if(selectDocNo.get("c2") != null){
 				prefix = (String) selectDocNo.get("c2");
 			}else{
@@ -215,7 +242,7 @@ public class TerritoryManagementServiceImpl extends EgovAbstractServiceImpl impl
 		}
 		return selectDocNo;
 	}
-	
+
 	public String getNextDocNo(String prefixNo,String docNo){
 		String nextDocNo = "";
 		int docNoLength=0;
@@ -228,7 +255,7 @@ public class TerritoryManagementServiceImpl extends EgovAbstractServiceImpl impl
 			System.out.println("들어와얗ㅁ");
 			docNoLength = docNo.length();
 		}
-		
+
 		int nextNo = Integer.parseInt(docNo) + 1;
 		nextDocNo = String.format("%0"+docNoLength+"d", nextNo);
 		logger.debug("nextDocNo : {}",nextDocNo);
@@ -239,7 +266,7 @@ public class TerritoryManagementServiceImpl extends EgovAbstractServiceImpl impl
 	public List<EgovMap> selectBranchCode(Map<String, Object> params) {
 		return territoryManagementMapper.selectBranchCode(params);
 	}
-	
+
 	@Override
 	public List<EgovMap> selectState(Map<String, Object> params) {
 		return territoryManagementMapper.selectState(params);
