a3f59dfe2c3df560674784ea61dae6e72fb6135b
Tommy, Thun Ping Yi <pingyi.thun@coway.com.my>
2019-03-29 07:38:17 +0000

    Add selectHTOrderBasicInfoByOrderId - TPY

--- src/main/java/com/coway/trust/biz/payment/invoice/service/InvoicePOService.java
+++ src/main/java/com/coway/trust/biz/payment/invoice/service/InvoicePOService.java
@@ -5,35 +5,38 @@ import java.util.Map;
 import com.coway.trust.cmmn.model.SessionVO;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 public interface InvoicePOService{
-    
+
 	/**
 	 * InvoicePO Master 조회
 	 * @param params
 	 * @return
 	 */
 	List<EgovMap> selectOrderBasicInfoByOrderId(Map<String, Object> params);
-	
+
+	List<EgovMap> selectHTOrderBasicInfoByOrderId(Map<String, Object> params);
+
+
 	/**
 	 * InvoicePO Data 조회
 	 * @param params
 	 * @return
 	 */
 	List<EgovMap> selectOrderDataByOrderId(Map<String, Object> params);
-	
+
 	/**
 	 * update Invoice Statement
 	 * @param params
 	 * @return
 	 */
 	int updateInvoiceStatement(Map<String, Object> params);
-	
+
 	/**
 	 * InvoiceStatement 조회
 	 * @param params
 	 * @return
 	 */
 	List<EgovMap>selectInvoiceStatementByOrdId(Map<String, Object> params);
-	
+
 	/**
 	 * InvoiceStatement 저장
 	 * @param params
