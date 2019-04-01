8e6012665334900e1e57ee8b0843206d2b415277
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-04-01 10:11:32 +0000

    Restructure File

--- src/main/java/com/coway/trust/biz/services/as/InHouseRepairService.java
+++ src/main/java/com/coway/trust/biz/services/as/InHouseRepairService.java
@@ -1,27 +1,30 @@
 package com.coway.trust.biz.services.as;
 
 import java.util.List;
-import java.util.Map;
 
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 01/04/2019    ONGHC      1.0.1       - Restructure File
+ *********************************************************************************************/
+import java.util.Map;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface InHouseRepairService {
 
-	List<EgovMap> selInhouseList(Map<String, Object> params);
-	
-	List<EgovMap> selInhouseDetailList(Map<String, Object> params);
-	
-	List<EgovMap> getCallLog(Map<String, Object> params);
-	
-	List<EgovMap> getProductMasters(Map<String, Object> params);
-	
-	List<EgovMap> getProductDetails(Map<String, Object> params);
-	
-	EgovMap  isAbStck(Map<String, Object> params); 
-	
-	 
-	
+  List<EgovMap> selInhouseList(Map<String, Object> params);
 
-	
+  List<EgovMap> selInhouseDetailList(Map<String, Object> params);
+
+  List<EgovMap> getASRulstSVC0004DInfo(Map<String, Object> params);
+
+  List<EgovMap> getCallLog(Map<String, Object> params);
+
+  List<EgovMap> getProductMasters(Map<String, Object> params);
+
+  List<EgovMap> getProductDetails(Map<String, Object> params);
+
+  EgovMap isAbStck(Map<String, Object> params);
+
 }
