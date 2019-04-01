e39122f070cc1b0d60cc74a1f26d1860e259c816
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-04-01 10:09:51 +0000

    Restructure File

--- src/main/java/com/coway/trust/web/services/as/InHouseRepairController.java
+++ src/main/java/com/coway/trust/web/services/as/InHouseRepairController.java
@@ -32,227 +32,219 @@ import com.coway.trust.cmmn.model.SessionVO;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 01/04/2019    ONGHC      1.0.1       - Restructure File
+ *********************************************************************************************/
 
 @Controller
 @RequestMapping(value = "/services/inhouse")
 
 public class InHouseRepairController {
-	private static final Logger logger = LoggerFactory.getLogger(InHouseRepairController.class);
-	
-	@Resource(name = "InHouseRepairService")
-	private InHouseRepairService inHouseRepairService;
-	
+  private static final Logger logger = LoggerFactory.getLogger(InHouseRepairController.class);
 
-	@Autowired
-	private MessageSourceAccessor messageAccessor;
-	
+  @Resource(name = "InHouseRepairService")
+  private InHouseRepairService inHouseRepairService;
 
-	
-	@RequestMapping(value = "/inhouseList.do")
-	public String main(@RequestParam Map<String, Object> params, ModelMap model) {
-		
-		// 호출될 화면
-		return "services/as/inhouseRepairList";
-	}
-	
-	@RequestMapping(value = "/inhouseDPop.do")
-	public String inhouseDPop(@RequestParam Map<String, Object> params, ModelMap model ,SessionVO sessionVO) {
-		
-		
-		logger.debug("sessionVO:" +sessionVO.toString());
-		model.put("mode", (String)params.get("mode"));   
-		model.put("ORD_ID", (String)params.get("ORD_ID"));   
-		model.put("ORD_NO", (String)params.get("ORD_NO"));   
-		model.put("AS_NO", (String)params.get("AS_NO"));   
-		model.put("AS_ID", (String)params.get("AS_ID"));   
-		
-		logger.debug("sessionVO==============================>");
-		logger.debug("memid: ["+sessionVO.getMemId()+"]");
-		logger.debug("sessionVO==============================>");
-		
-		model.put("USER_ID", sessionVO.getMemId());    
-		model.put("USER_NAME", sessionVO.getUserName());
+  @Autowired
+  private MessageSourceAccessor messageAccessor;
 
-		model.put("BRANCH_NAME", sessionVO.getBranchName());
-		model.put("BRANCH_ID", sessionVO.getUserBranchId());
-		   
-		
-		
-		// 호출될 화면
-		return "services/as/inRHouseNewRepairPop"; 
-	}
-	
-	
-	
-	@RequestMapping(value = "/inHouseAsResultEditBasicPop.do")
-	public String inHouseAsResultEditBasicPop(@RequestParam Map<String, Object> params, ModelMap model ,SessionVO sessionVO) {
-		
-		model.put("mode", (String)params.get("mode"));   
-		model.put("ORD_ID", (String)params.get("ORD_ID"));   
-		model.put("ORD_NO", (String)params.get("ORD_NO"));   
-		model.put("AS_NO", (String)params.get("AS_NO"));   
-		model.put("AS_ID", (String)params.get("AS_ID"));   
-		model.put("AS_RESULT_NO", (String)params.get("AS_RESULT_NO"));   
-		model.put("AS_RESULT_ID", (String)params.get("AS_RESULT_ID"));   
-		model.put("USER_ID", sessionVO.getMemId());
-		model.put("USER_NAME", sessionVO.getUserName());
+  @RequestMapping(value = "/inhouseList.do")
+  public String main(@RequestParam Map<String, Object> params, ModelMap model) {
+    logger.debug("===========================/inhouseList.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/inhouseList.do===============================");
+    return "services/as/inhouseRepairList";
+  }
 
-		model.put("BRANCH_NAME", sessionVO.getBranchName());
-		model.put("BRANCH_ID", sessionVO.getUserBranchId());
-		
-		
-		
-		// 호출될 화면
-		return "services/as/inHouseAsResultEditBasicPop"; 
-	}
-	
-	
-	
-	
-	
-	@RequestMapping(value = "/selInhouseList.do", method = RequestMethod.GET)
-	public ResponseEntity<List<EgovMap>> selInhouseList(@RequestParam Map<String, Object> params,HttpServletRequest request, ModelMap model) {
-		
-		logger.debug("selInhouseList in.............");
-		logger.debug("params : {}", params);
-		
-		
-		String[] repStateList =  request.getParameterValues("repState");
-		params.put("repStateList",repStateList);
-		
-		List<EgovMap> mList = inHouseRepairService.selInhouseList(params);
-		
-	
-		logger.debug("mList : {}", mList);
-		return ResponseEntity.ok(mList); 
-	}
-	
-	
+  @RequestMapping(value = "/inhouseDPop.do")
+  public String inhouseDPop(@RequestParam Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
+    logger.debug("===========================/inhouseDPop.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("== USER_ID " + sessionVO.getMemId());
+    logger.debug("== USER_NAME " + sessionVO.getUserName());
+    logger.debug("== BRANCH_NAME " + sessionVO.getBranchName());
+    logger.debug("== BRANCH_ID " + sessionVO.getUserBranchId());
+    logger.debug("===========================/inhouseDPop.do===============================");
 
-	@RequestMapping(value = "/selInhouseDetailList.do", method = RequestMethod.GET)
-	public ResponseEntity<List<EgovMap>> selInhouseDetailList(@RequestParam Map<String, Object> params,HttpServletRequest request, ModelMap model) {
-		
-		logger.debug("selInhouseDetailList in.............");
-		logger.debug("params : {}", params);
-		
-		List<EgovMap> dList =  null;
-		dList = inHouseRepairService.selInhouseDetailList(params);
-		
-	
-		logger.debug("dList : {}", dList);
-		return ResponseEntity.ok(dList); 
-	}
-	
-	
+    model.put("mode", (String) params.get("mode"));
+    model.put("ORD_ID", (String) params.get("ORD_ID"));
+    model.put("ORD_NO", (String) params.get("ORD_NO"));
+    model.put("AS_NO", (String) params.get("AS_NO"));
+    model.put("AS_ID", (String) params.get("AS_ID"));
+    model.put("USER_ID", sessionVO.getMemId());
+    model.put("USER_NAME", sessionVO.getUserName());
+    model.put("BRANCH_NAME", sessionVO.getBranchName());
+    model.put("BRANCH_ID", sessionVO.getUserBranchId());
 
+    return "services/as/inRHouseNewRepairPop";
+  }
 
-	@RequestMapping(value = "/save.do", method = RequestMethod.GET)
-	public ResponseEntity<ReturnMessage> save(@RequestParam Map<String, Object> params, Model model  ,HttpServletRequest request, SessionVO sessionVO) {
-		
-		logger.debug("in  save ");
-		logger.debug("			pram set  log");   
-		logger.debug("					" + params.toString());
-		logger.debug("			pram set end  ");  
-		
-		params.put("updator", sessionVO.getUserId());   
-		int  rtnValue = 1;
-		
-		List<EgovMap>  asresultmst			= (List<EgovMap>)  params.get("asResultM");
-		List<EgovMap>  add					 	= (List<EgovMap>)  params.get("add");
-		List<EgovMap>  remove				= (List<EgovMap>)  params.get("remove");
-		List<EgovMap>  update 				= (List<EgovMap>)  params.get("update");
-		List<EgovMap>  inhouse 				= (List<EgovMap>)  params.get("inhouse");
-		   
-		logger.debug("asresultmst ===>"+asresultmst.toString());  
-		logger.debug("asresultmst ===>"+inhouse.toString());  
-		
-		logger.debug("add ===>"+add.toString());
-		logger.debug("remove ===>"+remove.toString());
-		logger.debug("update ===>"+update.toString());
-		
-		
-		//inHouseRepairService.asResult_insert(params);  
-		
-		ReturnMessage message = new ReturnMessage();
-		if(rtnValue >0 ){
-			message.setCode(AppConstants.SUCCESS);
-			message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		}else{
-			message.setCode(AppConstants.FAIL);
-			message.setMessage(messageAccessor.getMessage(AppConstants.MSG_FAIL));
-		}
-		return ResponseEntity.ok(message);  
-	}
-	
-	
-	@RequestMapping(value = "/update.do", method = RequestMethod.GET)
-	public ResponseEntity<ReturnMessage> update(@RequestParam Map<String, Object> params, Model model  ,HttpServletRequest request, SessionVO sessionVO) {
-		
-		logger.debug("in  update ");
-		logger.debug("			pram set  log");
-		logger.debug("					" + params.toString());
-		logger.debug("			pram set end  ");  
-		
-		params.put("USER_ID", sessionVO.getUserId());   
-		   
-		
-		int  rtnValue = 1;
-		//inHouseRepairService.addASRemark(params);  
-		
-		ReturnMessage message = new ReturnMessage();
-		if(rtnValue >0 ){
-			message.setCode(AppConstants.SUCCESS);
-			message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		}else{
-			message.setCode(AppConstants.FAIL);
-			message.setMessage(messageAccessor.getMessage(AppConstants.MSG_FAIL));
-		}
-		return ResponseEntity.ok(message);  
-	}
-	
-	@RequestMapping(value = "/mListUpdate.do", method = RequestMethod.POST)
-	public ResponseEntity<ReturnMessage> mListUpdate(@RequestBody Map<String, Object> params, Model model  ,HttpServletRequest request, SessionVO sessionVO) {
-		
-		logger.debug("in  mListUpdate ");
-		logger.debug("			pram set  log");
-		logger.debug("					" + params.toString());
-		logger.debug("			pram set end  ");  
-		
-		params.put("USER_ID", sessionVO.getUserId());   
-		
-		//LinkedHashMap  updateList = (LinkedHashMap)  params.get("update");
-		   
-		logger.debug("updateList ===>"+params.get("update"));  
-		
-		int  rtnValue =  1;
-		//inHouseRepairService.asResultBasic_update(params);  
-		
-		ReturnMessage message = new ReturnMessage();
-		  
-		if(rtnValue >0 ){
-			message.setCode(AppConstants.SUCCESS);
-			message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
-		}else{
-			message.setCode(AppConstants.FAIL);
-			message.setMessage(messageAccessor.getMessage(AppConstants.MSG_FAIL));
-		}
-		return ResponseEntity.ok(message);  
-	}
-	
-	
+  @RequestMapping(value = "/inHouseAsResultEditBasicPop.do")
+  public String inHouseAsResultEditBasicPop(@RequestParam Map<String, Object> params, ModelMap model,
+      SessionVO sessionVO) {
 
-	@RequestMapping(value = "/getCallLog", method = RequestMethod.GET)
-	public ResponseEntity<List<EgovMap>> getCallLog(@RequestParam Map<String, Object> params,HttpServletRequest request, ModelMap model) {
-		
-		logger.debug("in  getCallLog.....");		
-		logger.debug("params : {}", params.toString());
-		
-		List<EgovMap>  list = inHouseRepairService.getCallLog(params);
-		
-		return ResponseEntity.ok(list);  
-	}
-	
-	
-	
-	
+    logger.debug("===========================/inHouseAsResultEditBasicPop.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/inHouseAsResultEditBasicPop.do===============================");
+
+    model.put("mode", (String) params.get("mode"));
+    model.put("ORD_ID", (String) params.get("ORD_ID"));
+    model.put("ORD_NO", (String) params.get("ORD_NO"));
+    model.put("AS_NO", (String) params.get("AS_NO"));
+    model.put("AS_ID", (String) params.get("AS_ID"));
+    model.put("AS_RESULT_NO", (String) params.get("AS_RESULT_NO"));
+    model.put("AS_RESULT_ID", (String) params.get("AS_RESULT_ID"));
+    model.put("USER_ID", sessionVO.getMemId());
+    model.put("USER_NAME", sessionVO.getUserName());
+
+    model.put("BRANCH_NAME", sessionVO.getBranchName());
+    model.put("BRANCH_ID", sessionVO.getUserBranchId());
+
+    // 호출될 화면
+    return "services/as/inHouseAsResultEditBasicPop";
+  }
+
+  @RequestMapping(value = "/selInhouseList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selInhouseList(@RequestParam Map<String, Object> params,
+      HttpServletRequest request, ModelMap model) {
+
+    logger.debug("===========================/selInhouseList.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/selInhouseList.do===============================");
+
+    String[] repStateList = request.getParameterValues("repState");
+    params.put("repStateList", repStateList);
+
+    List<EgovMap> mList = inHouseRepairService.selInhouseList(params);
+
+    return ResponseEntity.ok(mList);
+  }
+
+  @RequestMapping(value = "/selInhouseDetailList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selInhouseDetailList(@RequestParam Map<String, Object> params,
+      HttpServletRequest request, ModelMap model) {
+
+    logger.debug("===========================/selInhouseDetailList.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/selInhouseDetailList.do===============================");
+
+    List<EgovMap> dList = null;
+    dList = inHouseRepairService.selInhouseDetailList(params);
+
+    return ResponseEntity.ok(dList);
+  }
+
+  @RequestMapping(value = "/getASRulstSVC0004DInfo.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> getASRulstSVC0004DInfo(@RequestParam Map<String, Object> params,
+      HttpServletRequest request, ModelMap model) {
+
+    logger.debug("===========================/getASRulstSVC0004DInfo.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/getASRulstSVC0004DInfo.do===============================");
+
+    List<EgovMap> dList = null;
+    dList = inHouseRepairService.getASRulstSVC0004DInfo(params);
+
+    return ResponseEntity.ok(dList);
+  }
+
+  @RequestMapping(value = "/save.do", method = RequestMethod.GET)
+  public ResponseEntity<ReturnMessage> save(@RequestParam Map<String, Object> params, Model model,
+      HttpServletRequest request, SessionVO sessionVO) {
+    logger.debug("===========================/save.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/save.do===============================");
+
+    params.put("updator", sessionVO.getUserId());
+    int rtnValue = 1;
+
+    List<EgovMap> asresultmst = (List<EgovMap>) params.get("asResultM");
+    List<EgovMap> add = (List<EgovMap>) params.get("add");
+    List<EgovMap> remove = (List<EgovMap>) params.get("remove");
+    List<EgovMap> update = (List<EgovMap>) params.get("update");
+    List<EgovMap> inhouse = (List<EgovMap>) params.get("inhouse");
+
+    logger.debug("== asresultmst = " + asresultmst.toString());
+    logger.debug("== asresultmst = " + inhouse.toString());
+
+    logger.debug("== ADD = " + add.toString());
+    logger.debug("== REMOVE = " + remove.toString());
+    logger.debug("== UPDATE = " + update.toString());
+
+    // inHouseRepairService.asResult_insert(params);
+
+    ReturnMessage message = new ReturnMessage();
+    if (rtnValue > 0) {
+      message.setCode(AppConstants.SUCCESS);
+      message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
+    } else {
+      message.setCode(AppConstants.FAIL);
+      message.setMessage(messageAccessor.getMessage(AppConstants.MSG_FAIL));
+    }
+    return ResponseEntity.ok(message);
+  }
+
+  @RequestMapping(value = "/update.do", method = RequestMethod.GET)
+  public ResponseEntity<ReturnMessage> update(@RequestParam Map<String, Object> params, Model model,
+      HttpServletRequest request, SessionVO sessionVO) {
+    logger.debug("===========================/update.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/update.do===============================");
+
+    params.put("USER_ID", sessionVO.getUserId());
+
+    int rtnValue = 1;
+    // inHouseRepairService.addASRemark(params);
+
+    ReturnMessage message = new ReturnMessage();
+    if (rtnValue > 0) {
+      message.setCode(AppConstants.SUCCESS);
+      message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
+    } else {
+      message.setCode(AppConstants.FAIL);
+      message.setMessage(messageAccessor.getMessage(AppConstants.MSG_FAIL));
+    }
+    return ResponseEntity.ok(message);
+  }
+
+  @RequestMapping(value = "/mListUpdate.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> mListUpdate(@RequestBody Map<String, Object> params, Model model,
+      HttpServletRequest request, SessionVO sessionVO) {
+    logger.debug("===========================/mListUpdate.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/mListUpdate.do===============================");
+
+    params.put("USER_ID", sessionVO.getUserId());
+
+    // LinkedHashMap updateList = (LinkedHashMap) params.get("update");
+
+    int rtnValue = 1;
+    // inHouseRepairService.asResultBasic_update(params);
+
+    ReturnMessage message = new ReturnMessage();
+
+    if (rtnValue > 0) {
+      message.setCode(AppConstants.SUCCESS);
+      message.setMessage(messageAccessor.getMessage(AppConstants.MSG_SUCCESS));
+    } else {
+      message.setCode(AppConstants.FAIL);
+      message.setMessage(messageAccessor.getMessage(AppConstants.MSG_FAIL));
+    }
+    return ResponseEntity.ok(message);
+  }
+
+  @RequestMapping(value = "/getCallLog", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> getCallLog(@RequestParam Map<String, Object> params, HttpServletRequest request,
+      ModelMap model) {
+    logger.debug("===========================/getCallLog.do===============================");
+    logger.debug("== params " + params.toString());
+    logger.debug("===========================/getCallLog.do===============================");
+
+    List<EgovMap> list = inHouseRepairService.getCallLog(params);
+
+    return ResponseEntity.ok(list);
+  }
+
 }
