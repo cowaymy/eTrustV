738d3cd5755aea4fd4fc3472d7e551506589a61d
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-04-01 10:13:18 +0000

    Restructure File

--- src/main/java/com/coway/trust/biz/services/as/impl/InHouseRepairServiceImpl.java
+++ src/main/java/com/coway/trust/biz/services/as/impl/InHouseRepairServiceImpl.java
@@ -20,47 +20,53 @@ import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 import oracle.sql.DATE;
 
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 01/04/2019    ONGHC      1.0.1       - Restructure File
+ *********************************************************************************************/
+
 @Service("InHouseRepairService")
-public class  InHouseRepairServiceImpl  extends EgovAbstractServiceImpl implements InHouseRepairService{
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(InHouseRepairServiceImpl.class);
-	
-	@Resource(name = "InHouseRepairMapper")
-	private InHouseRepairMapper inHouseRepairMapper;
-	
-	@Override
-	public List<EgovMap> selInhouseList(Map<String, Object> params) {
-		return inHouseRepairMapper.selInhouseList(params);
-	}  
-	 
-	
-	@Override
-	public List<EgovMap> selInhouseDetailList(Map<String, Object> params) {
-		return inHouseRepairMapper.selInhouseDetailList(params);
-	}  
-	
-	
-	@Override 
-	public List<EgovMap> getCallLog(Map<String, Object> params) {
-		return inHouseRepairMapper.getCallLog(params);
-	} 
-	
-   
-	@Override 
-	public List<EgovMap> getProductDetails(Map<String, Object> params) {
-		return inHouseRepairMapper.getProductDetails(params);
-	}
-	
- 
-	@Override 
-	public List<EgovMap> getProductMasters(Map<String, Object> params) {
-		return inHouseRepairMapper.getProductMasters(params);
-	}
-	
-	
-	@Override 
-	public  EgovMap  isAbStck(Map<String, Object> params) {
-		return inHouseRepairMapper.isAbStck(params);
-	}
-	
+public class InHouseRepairServiceImpl extends EgovAbstractServiceImpl implements InHouseRepairService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(InHouseRepairServiceImpl.class);
+
+  @Resource(name = "InHouseRepairMapper")
+  private InHouseRepairMapper inHouseRepairMapper;
+
+  @Override
+  public List<EgovMap> selInhouseList(Map<String, Object> params) {
+    return inHouseRepairMapper.selInhouseList(params);
+  }
+
+  @Override
+  public List<EgovMap> selInhouseDetailList(Map<String, Object> params) {
+    return inHouseRepairMapper.selInhouseDetailList(params);
+  }
+
+  @Override
+  public List<EgovMap> getASRulstSVC0004DInfo(Map<String, Object> params) {
+    return inHouseRepairMapper.getASRulstSVC0004DInfo(params);
+  }
+
+  @Override
+  public List<EgovMap> getCallLog(Map<String, Object> params) {
+    return inHouseRepairMapper.getCallLog(params);
+  }
+
+  @Override
+  public List<EgovMap> getProductDetails(Map<String, Object> params) {
+    return inHouseRepairMapper.getProductDetails(params);
+  }
+
+  @Override
+  public List<EgovMap> getProductMasters(Map<String, Object> params) {
+    return inHouseRepairMapper.getProductMasters(params);
+  }
+
+  @Override
+  public EgovMap isAbStck(Map<String, Object> params) {
+    return inHouseRepairMapper.isAbStck(params);
+  }
+
 }
