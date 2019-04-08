872c86543d211f408bba0522c71d6774deb177ee
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-04-08 01:43:18 +0000

    mailAddrViewHistoryAjax and instAddrViewHistoryAjax

--- src/main/java/com/coway/trust/biz/sales/order/impl/OrderRegisterMapper.java
+++ src/main/java/com/coway/trust/biz/sales/order/impl/OrderRegisterMapper.java
@@ -42,126 +42,132 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 @Mapper("orderRegisterMapper")
 public interface OrderRegisterMapper {
 
-	String selectDocNo(int docNoId);
+  String selectDocNo(int docNoId);
 
-	String selectDocNoS(Map<String, Object> params);
+  String selectDocNoS(Map<String, Object> params);
 
-	EgovMap selectSrvCntcInfo(Map<String, Object> params);
+  EgovMap selectSrvCntcInfo(Map<String, Object> params);
 
-	EgovMap selectStockPrice(Map<String, Object> params);
+  EgovMap selectStockPrice(Map<String, Object> params);
 
-	List<EgovMap> selectDocSubmissionList(Map<String, Object> params);
+  List<EgovMap> selectDocSubmissionList(Map<String, Object> params);
 
-	List<EgovMap> selectPromotionByAppTypeStock(Map<String, Object> params);
+  List<EgovMap> selectPromotionByAppTypeStock(Map<String, Object> params);
 
-	List<EgovMap> selectPromotionByAppTypeStock2(Map<String, Object> params);
+  List<EgovMap> selectPromotionByAppTypeStock2(Map<String, Object> params);
 
-	EgovMap selectProductPromotionPriceByPromoStockID(Map<String, Object> params);
+  EgovMap selectProductPromotionPriceByPromoStockID(Map<String, Object> params);
 
-	EgovMap selectProductPromotionPriceByPromoStockIDNew(Map<String, Object> params);
+  EgovMap selectProductPromotionPriceByPromoStockIDNew(Map<String, Object> params);
 
-	EgovMap selectProductPromotionPriceByPromoStockIDNewCorp(Map<String, Object> params);
+  EgovMap selectProductPromotionPriceByPromoStockIDNewCorp(Map<String, Object> params);
 
-	EgovMap selectTrialNo(Map<String, Object> params);
+  EgovMap selectTrialNo(Map<String, Object> params);
 
-	EgovMap selectMemberByMemberIDCode(Map<String, Object> paraselectVerifyOldSalesOrderNoValidityICarems);
+  EgovMap selectMemberByMemberIDCode(Map<String, Object> paraselectVerifyOldSalesOrderNoValidityICarems);
 
-	List<EgovMap> selectMemberList(Map<String, Object> params);
+  List<EgovMap> selectMemberList(Map<String, Object> params);
 
-	EgovMap selectBankById(Map<String, Object> params);
+  EgovMap selectBankById(Map<String, Object> params);
 
-	List<EgovMap> selectBOMList(Map<String, Object> params);
+  List<EgovMap> selectBOMList(Map<String, Object> params);
 
-	EgovMap selectOldOrderId(String salesOrdNo);
+  EgovMap selectOldOrderId(String salesOrdNo);
 
-	EgovMap selectSvcExpire(int srvSoId);
+  EgovMap selectSvcExpire(int srvSoId);
 
-	EgovMap selectVerifyOldSalesOrderNoValidity(int salesOrdIdOld);
+  EgovMap selectVerifyOldSalesOrderNoValidity(int salesOrdIdOld);
 
-	EgovMap selectVerifyOldSalesOrderNoValidityICare(int salesOrdIdOld);
+  EgovMap selectVerifyOldSalesOrderNoValidityICare(int salesOrdIdOld);
 
-	EgovMap selectSalesOrderM(Map<String, Object> params);
+  EgovMap selectSalesOrderM(Map<String, Object> params);
 
-	EgovMap selectSalesOrderRentalScheme(Map<String, Object> params);
+  EgovMap selectSalesOrderRentalScheme(Map<String, Object> params);
 
-	EgovMap selectAccRentLedgers(int salesOrdId);
+  EgovMap selectAccRentLedgers(int salesOrdId);
 
-	EgovMap selectRentalInstNo(int salesOrdId);
+  EgovMap selectRentalInstNo(int salesOrdId);
 
-	BigDecimal selectRentAmt(int salesOrdId);
+  BigDecimal selectRentAmt(int salesOrdId);
 
-	EgovMap selectPromoDesc(int promoId);
+  EgovMap selectPromoDesc(int promoId);
 
-	void insertSalesOrderM(SalesOrderMVO salesOrderMVO);
+  void insertSalesOrderM(SalesOrderMVO salesOrderMVO);
 
-	void insertSalesOrderD(SalesOrderDVO salesOrderDVO);
+  void insertSalesOrderD(SalesOrderDVO salesOrderDVO);
 
-	void insertInstallation(InstallationVO installationVO);
+  void insertInstallation(InstallationVO installationVO);
 
-	void insertRentPaySet(RentPaySetVO rentPaySetVO);
+  void insertRentPaySet(RentPaySetVO rentPaySetVO);
 
-	void insertCustBillMaster(CustBillMasterVO custBillMasterVO);
+  void insertCustBillMaster(CustBillMasterVO custBillMasterVO);
 
-	void insertEStatementReq(EStatementReqVO eStatementReqVO);
+  void insertEStatementReq(EStatementReqVO eStatementReqVO);
 
-	void insertAccClaimAdt(AccClaimAdtVO accClaimAdtVO);
+  void insertAccClaimAdt(AccClaimAdtVO accClaimAdtVO);
 
-	void insertRentalScheme(RentalSchemeVO rentalSchemeVO);
+  void insertRentalScheme(RentalSchemeVO rentalSchemeVO);
 
-	void insertCcpDecisionM(CcpDecisionMVO ccpDecisionMVO);
+  void insertCcpDecisionM(CcpDecisionMVO ccpDecisionMVO);
 
-	void insertDocSubmission(DocSubmissionVO docSubmissionVO);
+  void insertDocSubmission(DocSubmissionVO docSubmissionVO);
 
-	void insertSrvMembershipSales(SrvMembershipSalesVO srvMembershipSalesVO);
+  void insertSrvMembershipSales(SrvMembershipSalesVO srvMembershipSalesVO);
 
-	void insertSrvConfiguration(SrvConfigurationVO srvConfigurationVO);
+  void insertSrvConfiguration(SrvConfigurationVO srvConfigurationVO);
 
-	void insertSrvConfigSetting(SrvConfigSettingVO srvConfigSettingVO);
+  void insertSrvConfigSetting(SrvConfigSettingVO srvConfigSettingVO);
 
-	void insertSrvConfigPeriod(SrvConfigPeriodVO srvConfigPeriodVO);
+  void insertSrvConfigPeriod(SrvConfigPeriodVO srvConfigPeriodVO);
 
-	void insertSrvConfigFilter(SrvConfigFilterVO srvConfigFilterVO);
+  void insertSrvConfigFilter(SrvConfigFilterVO srvConfigFilterVO);
 
-	void insertCallEntry(CallEntryVO callEntryVO);
+  void insertCallEntry(CallEntryVO callEntryVO);
 
-	void insertCallResult(CallResultVO callResultVO);
+  void insertCallResult(CallResultVO callResultVO);
 
-	void insertInstallEntry(InstallEntryVO installEntryVO);
+  void insertInstallEntry(InstallEntryVO installEntryVO);
 
-	void insertInstallResult(InstallResultVO installResultVO);
+  void insertInstallResult(InstallResultVO installResultVO);
 
-	void insertSalesOrderLog(SalesOrderLogVO salesOrderLogVO);
+  void insertSalesOrderLog(SalesOrderLogVO salesOrderLogVO);
 
-	void insertGSTEURCertificate(GSTEURCertificateVO gstEURCertificateVO);
+  void insertGSTEURCertificate(GSTEURCertificateVO gstEURCertificateVO);
 
-	void insertSalesOrderContract(SalesOrderContractVO salesOrderContractVO);
+  void insertSalesOrderContract(SalesOrderContractVO salesOrderContractVO);
 
-	void updateCustBillId(SalesOrderMVO salesOrderMVO);
+  void updateCustBillId(SalesOrderMVO salesOrderMVO);
 
-	EgovMap selectLoginInfo(Map<String, Object> params);
+  EgovMap selectLoginInfo(Map<String, Object> params);
 
-	EgovMap selectCheckAccessRight(Map<String, Object> params);
+  EgovMap selectCheckAccessRight(Map<String, Object> params);
 
-	List<EgovMap> selectProductCodeList(Map<String, Object> params);
+  List<EgovMap> selectProductCodeList(Map<String, Object> params);
 
-	List<EgovMap> selectServicePackageList(Map<String, Object> params);
+  List<EgovMap> selectServicePackageList(Map<String, Object> params);
 
-	List<EgovMap> selectServicePackageList2(Map<String, Object> params);
+  List<EgovMap> selectServicePackageList2(Map<String, Object> params);
 
-	EgovMap selectServiceContractPackage(Map<String, Object> params);
+  EgovMap selectServiceContractPackage(Map<String, Object> params);
 
-	List<EgovMap> selectPrevOrderNoList(Map<String, Object> params);
+  List<EgovMap> selectPrevOrderNoList(Map<String, Object> params);
 
-	List<EgovMap> selectProductComponent(Map<String, Object> params);
+  List<EgovMap> selectProductComponent(Map<String, Object> params);
 
-	EgovMap selectProductComponentDefaultKey(Map<String, Object> params);
+  EgovMap selectProductComponentDefaultKey(Map<String, Object> params);
 
-	void insertASEntry(ASEntryVO asEntryVo);
+  void insertASEntry(ASEntryVO asEntryVo);
 
-	EgovMap selectOutstandingAmt(int salesOrdId);
+  EgovMap selectOutstandingAmt(int salesOrdId);
 
-	EgovMap selectOutrightPlusOutstandingAmt(int salesOrdId);
+  EgovMap selectOutrightPlusOutstandingAmt(int salesOrdId);
 
-	EgovMap selectEKeyinSofCheck(Map<String, Object> params);
+  int chkCrtAS(SalesOrderDVO salesOrderDVO);
+
+  EgovMap selectEKeyinSofCheck(Map<String, Object> params);
+
+  List<EgovMap> mailAddrViewHistoryAjax(Map<String, Object> params);
+
+  List<EgovMap> instAddrViewHistoryAjax(Map<String, Object> params);
 
 }
