0c858af1644632be41d7d85c9640932731a82d3a
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-04-26 09:10:23 +0000

    Add filterChangeQtyTst

--- src/main/java/com/coway/trust/api/mobile/services/heartService/HeartServiceResultDetailForm.java
+++ src/main/java/com/coway/trust/api/mobile/services/heartService/HeartServiceResultDetailForm.java
@@ -14,96 +14,99 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(value = "HeartServiceResultDetailForm", description = "HeartServiceResultDetailForm")
 public class HeartServiceResultDetailForm {
 
-	@ApiModelProperty(value = "필터 코드 [default : '' 전체] 예) T010", example = "CT100337")
-	private String filterCode;
+  @ApiModelProperty(value = "필터 코드 [default : '' 전체] 예) T010", example = "CT100337")
+  private String filterCode;
 
-	@ApiModelProperty(value = "chgid예) 20170820", example = "28092017")
-	private int exchangeId;
+  @ApiModelProperty(value = "chgid예) 20170820", example = "28092017")
+  private int exchangeId;
 
-	@ApiModelProperty(value = "필터 교체 수량 예) 20170827", example = "29092017")
-	private int filterChangeQty;
+  @ApiModelProperty(value = "필터 교체 수량 예) 20170827", example = "29092017")
+  private int filterChangeQty;
 
-	
-	@ApiModelProperty(value = "대체 필터 코드(123456)", example = "")
-	private int alternativeFilterCode;
+  @ApiModelProperty(value = "필터 교체 수량 예) 20170827", example = "29092017")
+  private int filterChangeQtyTst;
 
-	@ApiModelProperty(value = "교체 필터 바코드", example = "")
-	private String filterBarcdSerialNo;
-	
-	public int getAlternativeFilterCode() {
-		return alternativeFilterCode;
-	}
+  @ApiModelProperty(value = "대체 필터 코드(123456)", example = "")
+  private int alternativeFilterCode;
 
-	public void setAlternativeFilterCode(int alternativeFilterCode) {
-		this.alternativeFilterCode = alternativeFilterCode;
-	}
+  @ApiModelProperty(value = "교체 필터 바코드", example = "")
+  private String filterBarcdSerialNo;
 
-	public String getFilterBarcdSerialNo() {
-		return filterBarcdSerialNo;
-	}
+  public int getAlternativeFilterCode() {
+    return alternativeFilterCode;
+  }
 
-	public void setFilterBarcdSerialNo(String filterBarcdSerialNo) {
-		this.filterBarcdSerialNo = filterBarcdSerialNo;
-	}
+  public void setAlternativeFilterCode(int alternativeFilterCode) {
+    this.alternativeFilterCode = alternativeFilterCode;
+  }
 
-	public String getFilterCode() {
-		return filterCode;
-	}
+  public String getFilterBarcdSerialNo() {
+    return filterBarcdSerialNo;
+  }
 
-	public void setFilterCode(String filterCode) {
-		this.filterCode = filterCode;
-	}
+  public void setFilterBarcdSerialNo(String filterBarcdSerialNo) {
+    this.filterBarcdSerialNo = filterBarcdSerialNo;
+  }
 
+  public String getFilterCode() {
+    return filterCode;
+  }
 
-	public int getExchangeId() {
-		return exchangeId;
-	}
+  public void setFilterCode(String filterCode) {
+    this.filterCode = filterCode;
+  }
 
-	public void setExchangeId(int exchangeId) {
-		this.exchangeId = exchangeId;
-	}
+  public int getExchangeId() {
+    return exchangeId;
+  }
 
-	public int getFilterChangeQty() {
-		return filterChangeQty;
-	}
+  public void setExchangeId(int exchangeId) {
+    this.exchangeId = exchangeId;
+  }
 
-	public void setFilterChangeQty(int filterChangeQty) {
-		this.filterChangeQty = filterChangeQty;
-	}
+  public int getFilterChangeQty() {
+    return filterChangeQty;
+  }
 
-	
-	public static List<Map<String, Object>>  createMaps(List<HeartServiceResultDetailForm> heartServiceResultDetailForms) {
+  public void setFilterChangeQty(int filterChangeQty) {
+    this.filterChangeQty = filterChangeQty;
+  }
 
-		List<Map<String, Object>> list = new ArrayList<>();
-		Map<String, Object> map;
-	
-		
-		for(HeartServiceResultDetailForm form : heartServiceResultDetailForms){
-			map = BeanConverter.toMap(form);
-			list.add(map);
-		}
-		return list;
-	}
-	
+  public int getFilterChangeQtyTst() {
+    return filterChangeQtyTst;
+  }
 
-	
-	public static List<Object>  createMaps1(List<HeartServiceResultDetailForm> heartServiceResultDetailForms) {
+  public void setFilterChangeQtyTst(int filterChangeQtyTst) {
+    this.filterChangeQtyTst = filterChangeQtyTst;
+  }
 
-		List<Object> list = new ArrayList<>();
-		Map<String, Object> map;
-	
-		
-		for(HeartServiceResultDetailForm form : heartServiceResultDetailForms){
-			map = BeanConverter.toMap(form);
-			map.put("stkId", map.get("filterCode"));
-			map.put("exchangeId", map.get("exchangeId"));
-			map.put("name", map.get("filterChangeQty"));
-			map.put("filterBarcdSerialNo", map.get("filterBarcdSerialNo"));
-			
-			list.add(map);
-		}
-		return list;
-	}
-	
-	
+  public static List<Map<String, Object>> createMaps(List<HeartServiceResultDetailForm> heartServiceResultDetailForms) {
+
+    List<Map<String, Object>> list = new ArrayList<>();
+    Map<String, Object> map;
+
+    for (HeartServiceResultDetailForm form : heartServiceResultDetailForms) {
+      map = BeanConverter.toMap(form);
+      list.add(map);
+    }
+    return list;
+  }
+
+  public static List<Object> createMaps1(List<HeartServiceResultDetailForm> heartServiceResultDetailForms) {
+
+    List<Object> list = new ArrayList<>();
+    Map<String, Object> map;
+
+    for (HeartServiceResultDetailForm form : heartServiceResultDetailForms) {
+      map = BeanConverter.toMap(form);
+      map.put("stkId", map.get("filterCode"));
+      map.put("exchangeId", map.get("exchangeId"));
+      map.put("name", map.get("filterChangeQty"));
+      map.put("filterBarcdSerialNo", map.get("filterBarcdSerialNo"));
+
+      list.add(map);
+    }
+    return list;
+  }
+
 }
