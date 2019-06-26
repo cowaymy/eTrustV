a85b05fc449564c92b944f7267f3c149aab61b39
Tommy, Thun Ping Yi <pingyi.thun@coway.com.my>
2019-06-26 05:49:45 +0000

    Add checkSvcRentalMembershipInfo - TPY

--- src/main/java/com/coway/trust/biz/services/bs/impl/HsManualMapper.java
+++ src/main/java/com/coway/trust/biz/services/bs/impl/HsManualMapper.java
@@ -328,6 +328,8 @@ public interface HsManualMapper {
 
   EgovMap checkSvcMembershipInfo(Map<String, Object> params);
 
+  EgovMap checkSvcRentalMembershipInfo(Map<String, Object> params);
+
   EgovMap checkRentalStatusInfo(Map<String, Object> params);
 
   EgovMap checkOrderStatusInfo(Map<String, Object> params);
