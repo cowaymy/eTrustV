58beb4a7b8f5c5c9c7fbdcd412f567b28581f0df
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-09-06 01:48:12 +0000

    ADD CHECKING AND RE-FORMAT FILE

--- src/main/java/com/coway/trust/biz/logistics/stockmovement/StockMovementService.java
+++ src/main/java/com/coway/trust/biz/logistics/stockmovement/StockMovementService.java
@@ -7,48 +7,52 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface StockMovementService {
 
-	String insertStockMovementInfo(Map<String, Object> param);
+  String insertStockMovementInfo(Map<String, Object> param);
 
-	List<EgovMap> selectStockMovementNoList(Map<String, Object> params);
+  List<EgovMap> selectStockMovementNoList(Map<String, Object> params);
 
-	List<EgovMap> selectStockMovementMainList(Map<String, Object> params);
+  List<EgovMap> selectStockMovementMainList(Map<String, Object> params);
 
-	List<EgovMap> selectStockMovementDeliveryList(Map<String, Object> params);
+  List<EgovMap> selectStockMovementDeliveryList(Map<String, Object> params);
 
-	List<EgovMap> selectTolocationItemList(Map<String, Object> smap);
+  List<EgovMap> selectTolocationItemList(Map<String, Object> smap);
 
-	int stockMovementItemDeliveryQty(Map<String, Object> params);
+  int stockMovementItemDeliveryQty(Map<String, Object> params);
 
-	Map<String, Object> selectStockMovementDataDetail(String param);
+  Map<String, Object> selectStockMovementDataDetail(String param);
 
-	void deleteDeliveryStockMovement(Map<String, Object> param);
+  void deleteDeliveryStockMovement(Map<String, Object> param);
 
-	List<EgovMap> addStockMovementInfo(Map<String, Object> param);
+  List<EgovMap> addStockMovementInfo(Map<String, Object> param);
 
-	Map<String, Object> stockMovementReqDelivery(Map<String, Object> param);
+  Map<String, Object> stockMovementReqDelivery(Map<String, Object> param);
 
-	List<EgovMap> selectStockMovementSerial(Map<String, Object> params);
+  List<EgovMap> selectStockMovementSerial(Map<String, Object> params);
 
-	Map<String, Object> stockMovementDeliveryIssue(Map<String, Object> params);
+  Map<String, Object> stockMovementDeliveryIssue(Map<String, Object> params);
 
-	List<EgovMap> selectStockMovementDeliverySerial(Map<String, Object> params);
+  List<EgovMap> selectStockMovementDeliverySerial(Map<String, Object> params);
 
-	List<EgovMap> selectStockMovementMtrDocInfoList(Map<String, Object> params);
+  List<EgovMap> selectStockMovementMtrDocInfoList(Map<String, Object> params);
 
-	void insertStockBooking(Map<String, Object> params);
-	
-	List<EgovMap> selectGetSerialDataCall(Map<String, Object> params);
-	
-	void deleteSmoNo(Map<String, Object> param);
+  void insertStockBooking(Map<String, Object> params);
 
-	String selectDefToLocation(Map<String, Object> param);
+  List<EgovMap> selectGetSerialDataCall(Map<String, Object> params);
 
-	String defToLoc(Map<String, Object> param);
+  void deleteSmoNo(Map<String, Object> param);
 
-	List<EgovMap> SelectStockfromForecast(Map<String, Object> smap);
+  String selectDefToLocation(Map<String, Object> param);
 
-	String insertStockMovementbyForecast(Map<String, Object> param);
+  String defToLoc(Map<String, Object> param);
 
-	String insertStockMovementForOnLoanUnit(Map<String, Object> param);
+  List<EgovMap> SelectStockfromForecast(Map<String, Object> smap);
+
+  String insertStockMovementbyForecast(Map<String, Object> param);
+
+  String insertStockMovementForOnLoanUnit(Map<String, Object> param);
+
+  int getASNo(Map<String, Object> params);
+
+  int chkASNoExist(Map<String, Object> params);
 
 }
