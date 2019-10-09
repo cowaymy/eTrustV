1499181af27d7e5c3802514020bae39adbc0afe0
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-10-09 07:12:05 +0000

    AMEND FOR LATEST CHANGES

--- src/main/java/com/coway/trust/biz/logistics/pointofsales/PointOfSalesService.java
+++ src/main/java/com/coway/trust/biz/logistics/pointofsales/PointOfSalesService.java
@@ -5,32 +5,45 @@ import java.util.Map;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
-public interface PointOfSalesService {
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 09/10/2019    ONGHC      1.0.1       - AMEND FOR LATEST CHANGES
+ *********************************************************************************************/
 
-	List<EgovMap> PosSearchList(Map<String, Object> params);
-	
-	List<EgovMap> posItemList(Map<String, Object> params);
-	
-	List<EgovMap> selectTypeList(Map<String, Object> params);
-	
-	List<EgovMap>  selectPointOfSalesSerial(Map<String, Object> params);
-	
-	//List<EgovMap>  selectPosReqNoList(Map<String, Object> params);
-	
-	String insertPosInfo(Map<String, Object> params);
-	
-	String  insertGiInfo(Map<String, Object> params);
-	
-	Map<String, Object> PosDataDetail(String param);
-	
-	List<EgovMap> selectSerial(Map<String, Object> params);
-	
-	void insertStockBooking(Map<String, Object> params);
-	
-	List<EgovMap> selectMaterialDocList(Map<String, Object> params);
-	
-	int selectOtherReqChk(Map<String, Object> params);
-	
-	void deleteStoNo(Map<String, Object> param);
-		
+public interface PointOfSalesService {
+  List<EgovMap> getTrxTyp(Map<String, Object> params);
+
+  List<EgovMap> PosSearchList(Map<String, Object> params);
+
+  List<EgovMap> posItemList(Map<String, Object> params);
+
+  List<EgovMap> selectTypeList(Map<String, Object> params);
+
+  List<EgovMap> selectAdjRsn(Map<String, Object> params);
+
+  List<EgovMap> getRqstLocLst(Map<String, Object> params);
+
+  List<EgovMap> selectPointOfSalesSerial(Map<String, Object> params);
+
+  // List<EgovMap> selectPosReqNoList(Map<String, Object> params);
+
+  String insertPosInfo(Map<String, Object> params);
+
+  String insertGiInfo(Map<String, Object> params);
+
+  Map<String, Object> PosDataDetail(String param);
+
+  List<EgovMap> selectSerial(Map<String, Object> params);
+
+  void insertStockBooking(Map<String, Object> params);
+
+  List<EgovMap> selectMaterialDocList(Map<String, Object> params);
+
+  int selectOtherReqChk(Map<String, Object> params);
+
+  void deleteStoNo(Map<String, Object> param);
+
+  EgovMap selectAttachmentInfo(Map<String, Object> params);
+
 }
