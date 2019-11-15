4224fa09172152ef8ce069d6fc9e34f86f317e00
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-11-15 09:19:16 +0000

    Add Combo Promotion Feature

--- src/main/java/com/coway/trust/web/sales/order/OrderRequestController.java
+++ src/main/java/com/coway/trust/web/sales/order/OrderRequestController.java
@@ -38,202 +38,227 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 @RequestMapping(value = "/sales/order")
 public class OrderRequestController {
 
-	private static Logger logger = LoggerFactory.getLogger(OrderRequestController.class);
+  private static Logger logger = LoggerFactory.getLogger(OrderRequestController.class);
 
-	@Resource(name = "orderRequestService")
-	private OrderRequestService orderRequestService;
+  @Resource(name = "orderRequestService")
+  private OrderRequestService orderRequestService;
 
-	@Resource(name = "orderDetailService")
-	private OrderDetailService orderDetailService;
+  @Resource(name = "orderDetailService")
+  private OrderDetailService orderDetailService;
 
-	@Resource(name = "orderLedgerService")
-	private OrderLedgerService orderLedgerService;
+  @Resource(name = "orderLedgerService")
+  private OrderLedgerService orderLedgerService;
 
-	@Autowired
-	private MessageSourceAccessor messageAccessor;
+  @Autowired
+  private MessageSourceAccessor messageAccessor;
 
-	@RequestMapping(value = "/orderRequestPop.do")
-	public String orderRequestPop(@RequestParam Map<String, Object>params, ModelMap model, SessionVO sessionVO) throws Exception {
+  @RequestMapping(value = "/orderRequestPop.do")
+  public String orderRequestPop(@RequestParam Map<String, Object> params, ModelMap model, SessionVO sessionVO)
+      throws Exception {
 
-		String callCenterYn = "N";
+    String callCenterYn = "N";
 
-		if(CommonUtils.isNotEmpty(params.get(AppConstants.CALLCENTER_TOKEN_KEY))){
-			callCenterYn = "Y";
-		}
-
-		EgovMap orderDetail = orderDetailService.selectOrderBasicInfo(params, sessionVO);//APP_TYPE_ID CUST_ID
-
-		model.put("orderDetail", orderDetail);
-		model.put("ordReqType",  params.get("ordReqType"));
-		model.put("toDay", 		 CommonUtils.getFormattedString(SalesConstants.DEFAULT_DATE_FORMAT1));
-
-		String toDay = CommonUtils.getFormattedString(SalesConstants.DEFAULT_DATE_FORMAT1);
-
-		model.put("toDay", toDay);
-		model.put("callCenterYn", callCenterYn);
-
-		return "sales/order/orderRequestPop";
-	}
-
-    @RequestMapping(value = "/selectResnCodeList.do", method = RequestMethod.GET)
-    public ResponseEntity<List<EgovMap>> selectResnCodeList(@RequestParam Map<String, Object> params)    {
-    	List<EgovMap> rsltList = orderRequestService.selectResnCodeList(params);
-    	return ResponseEntity.ok(rsltList);
+    if (CommonUtils.isNotEmpty(params.get(AppConstants.CALLCENTER_TOKEN_KEY))) {
+      callCenterYn = "Y";
     }
 
-    @RequestMapping(value = "/selectOrderLastRentalBillLedger1.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> selectOrderLastRentalBillLedger1(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.selectOrderLastRentalBillLedger1(params);
-    	return ResponseEntity.ok(rslt);
-    }
+    EgovMap orderDetail = orderDetailService.selectOrderBasicInfo(params, sessionVO);// APP_TYPE_ID
+                                                                                     // CUST_ID
 
-	@RequestMapping(value = "/requestCancelOrder.do", method = RequestMethod.POST)
-	public ResponseEntity<ReturnMessage> requestCancelOrder(@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) throws Exception {
+    model.put("orderDetail", orderDetail);
+    model.put("ordReqType", params.get("ordReqType"));
+    model.put("toDay", CommonUtils.getFormattedString(SalesConstants.DEFAULT_DATE_FORMAT1));
 
-		ReturnMessage message = orderRequestService.requestCancelOrder(params, sessionVO);
+    String toDay = CommonUtils.getFormattedString(SalesConstants.DEFAULT_DATE_FORMAT1);
 
-		return ResponseEntity.ok(message);
-	}
+    model.put("toDay", toDay);
+    model.put("callCenterYn", callCenterYn);
 
-	@RequestMapping(value = "/requestProdExch.do", method = RequestMethod.POST)
-	public ResponseEntity<ReturnMessage> requestProdExch(@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) throws Exception {
+    return "sales/order/orderRequestPop";
+  }
 
-		ReturnMessage message = orderRequestService.requestProductExchange(params, sessionVO);
+  @RequestMapping(value = "/selectResnCodeList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selectResnCodeList(@RequestParam Map<String, Object> params) {
+    List<EgovMap> rsltList = orderRequestService.selectResnCodeList(params);
+    return ResponseEntity.ok(rsltList);
+  }
 
-		return ResponseEntity.ok(message);
-	}
+  @RequestMapping(value = "/selectOrderLastRentalBillLedger1.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> selectOrderLastRentalBillLedger1(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.selectOrderLastRentalBillLedger1(params);
+    return ResponseEntity.ok(rslt);
+  }
 
-	@RequestMapping(value = "/requestSchmConv.do", method = RequestMethod.POST)
-	public ResponseEntity<ReturnMessage> requestSchmConv(@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) throws Exception {
+  @RequestMapping(value = "/requestCancelOrder.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> requestCancelOrder(@RequestBody Map<String, Object> params, ModelMap model,
+      SessionVO sessionVO) throws Exception {
 
-		ReturnMessage message = orderRequestService.requestSchmConv(params, sessionVO);
+    ReturnMessage message = orderRequestService.requestCancelOrder(params, sessionVO);
 
-		return ResponseEntity.ok(message);
-	}
+    return ResponseEntity.ok(message);
+  }
 
-	@RequestMapping(value = "/requestAppExch.do", method = RequestMethod.POST)
-	public ResponseEntity<ReturnMessage> requestAppExch(@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) throws Exception {
+  @RequestMapping(value = "/requestProdExch.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> requestProdExch(@RequestBody Map<String, Object> params, ModelMap model,
+      SessionVO sessionVO) throws Exception {
 
-		ReturnMessage message = orderRequestService.requestApplicationExchange(params, sessionVO);
+    ReturnMessage message = orderRequestService.requestProductExchange(params, sessionVO);
 
-		return ResponseEntity.ok(message);
-	}
+    return ResponseEntity.ok(message);
+  }
 
-	@RequestMapping(value = "/requestOwnershipTransfer.do", method = RequestMethod.POST)
-	public ResponseEntity<ReturnMessage> requestOwnershipTransfer(@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) throws Exception {
+  @RequestMapping(value = "/requestSchmConv.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> requestSchmConv(@RequestBody Map<String, Object> params, ModelMap model,
+      SessionVO sessionVO) throws Exception {
 
-		ReturnMessage message = orderRequestService.requestOwnershipTransfer(params, sessionVO);
+    ReturnMessage message = orderRequestService.requestSchmConv(params, sessionVO);
 
-		return ResponseEntity.ok(message);
-	}
+    return ResponseEntity.ok(message);
+  }
 
-    @RequestMapping(value = "/selectCompleteASIDByOrderIDSolutionReason.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> selectCompleteASIDByOrderIDSolutionReason(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.selectCompleteASIDByOrderIDSolutionReason(params);
-    	return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/requestAppExch.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> requestAppExch(@RequestBody Map<String, Object> params, ModelMap model,
+      SessionVO sessionVO) throws Exception {
 
-	@RequestMapping(value = "/loginUserId.do", method = RequestMethod.GET)
-	public ResponseEntity<EgovMap> loginUserId(SessionVO sessionVO) throws Exception {
+    ReturnMessage message = orderRequestService.requestApplicationExchange(params, sessionVO);
 
-		EgovMap map = new EgovMap();
+    return ResponseEntity.ok(message);
+  }
 
-		map.put("userId", sessionVO.getUserId());
+  @RequestMapping(value = "/requestOwnershipTransfer.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> requestOwnershipTransfer(@RequestBody Map<String, Object> params, ModelMap model,
+      SessionVO sessionVO) throws Exception {
 
-		return ResponseEntity.ok(map);
-	}
+    ReturnMessage message = orderRequestService.requestOwnershipTransfer(params, sessionVO);
 
-    @RequestMapping(value = "/selectSalesOrderSchemeList.do", method = RequestMethod.GET)
-    public ResponseEntity<List<EgovMap>> selectSalesOrderSchemeList(@RequestParam Map<String, Object> params)    {
-    	List<EgovMap> rsltList = orderRequestService.selectSalesOrderSchemeList(params);
-    	return ResponseEntity.ok(rsltList);
-    }
+    return ResponseEntity.ok(message);
+  }
 
-    @RequestMapping(value = "/selectSchemePriceSettingByPromoCode.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> selectSchemePriceSettingByPromoCode(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.selectSchemePriceSettingByPromoCode(params);
-    	return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/selectCompleteASIDByOrderIDSolutionReason.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> selectCompleteASIDByOrderIDSolutionReason(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.selectCompleteASIDByOrderIDSolutionReason(params);
+    return ResponseEntity.ok(rslt);
+  }
 
-    @RequestMapping(value = "/selectSchemePartSettingBySchemeIDList.do", method = RequestMethod.GET)
-    public ResponseEntity<List<EgovMap>> selectSchemePartSettingBySchemeIDList(@RequestParam Map<String, Object> params)    {
-    	List<EgovMap> rsltList = orderRequestService.selectSchemePartSettingBySchemeIDList(params);
-    	return ResponseEntity.ok(rsltList);
-    }
+  @RequestMapping(value = "/loginUserId.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> loginUserId(SessionVO sessionVO) throws Exception {
 
-	@RequestMapping(value = "/schemConvPop.do")
-	public String schemConvPop() {
-		return "sales/order/schemConvPop";
-	}
+    EgovMap map = new EgovMap();
 
-    @RequestMapping(value = "/selectValidateInfo.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> selectValidateInfo(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.selectValidateInfo(params);
-    	return ResponseEntity.ok(rslt);
-    }
+    map.put("userId", sessionVO.getUserId());
 
-    @RequestMapping(value = "/selectValidateInfoSimul.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> selectValidateInfoSimul(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.selectValidateInfoSimul(params);
-    	return ResponseEntity.ok(rslt);
-    }
+    return ResponseEntity.ok(map);
+  }
 
-    @RequestMapping(value = "/selectOrderSimulatorViewByOrderNo.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> selectOrderSimulatorViewByOrderNo(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.selectOrderSimulatorViewByOrderNo(params);
-    	return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/selectSalesOrderSchemeList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selectSalesOrderSchemeList(@RequestParam Map<String, Object> params) {
+    List<EgovMap> rsltList = orderRequestService.selectSalesOrderSchemeList(params);
+    return ResponseEntity.ok(rsltList);
+  }
 
-    @RequestMapping(value = "/selectOderOutsInfo.do", method = RequestMethod.GET)
-    public ResponseEntity<List<EgovMap>> selectOderOutsInfo(@RequestParam Map<String, Object> params)    {
-    	List<EgovMap> ordOutInfoList = orderLedgerService.getOderOutsInfo(params);
-    	return ResponseEntity.ok(ordOutInfoList);
-    }
+  @RequestMapping(value = "/selectSchemePriceSettingByPromoCode.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> selectSchemePriceSettingByPromoCode(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.selectSchemePriceSettingByPromoCode(params);
+    return ResponseEntity.ok(rslt);
+  }
 
-    @RequestMapping(value = "/selectObligtPriod.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> selectObligtPriod(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.selectObligtPriod(params);
-    	return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/selectSchemePartSettingBySchemeIDList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selectSchemePartSettingBySchemeIDList(@RequestParam Map<String, Object> params) {
+    List<EgovMap> rsltList = orderRequestService.selectSchemePartSettingBySchemeIDList(params);
+    return ResponseEntity.ok(rsltList);
+  }
 
-    @RequestMapping(value = "/selectPenaltyAmt.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> selectPenaltyAmt(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.selectPenaltyAmt(params);
-    	return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/schemConvPop.do")
+  public String schemConvPop() {
+    return "sales/order/schemConvPop";
+  }
 
-    @RequestMapping(value = "/checkeAutoDebitDeduction.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> checkeAutoDebitDeduction(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.checkeAutoDebitDeduction(params);
-    	return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/selectValidateInfo.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> selectValidateInfo(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.selectValidateInfo(params);
+    return ResponseEntity.ok(rslt);
+  }
 
-  //BY KV order installation no yet complete (CallLog Type - 257, CCR0001D - 20, SAL00046 - Active )
-    @RequestMapping(value = "/validOCRStus.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> validOCRStus(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.validOCRStus(params);
-    	return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/selectValidateInfoSimul.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> selectValidateInfoSimul(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.selectValidateInfoSimul(params);
+    return ResponseEntity.ok(rslt);
+  }
 
-    /* Valid OCR Status - (CallLog Type - 257, Stus - 1, SAL00046 - NO RECORD  )*/
-    @RequestMapping(value = "/validOCRStus2.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> validOCRStus2(@RequestParam Map<String, Object> params)    {
-    	EgovMap rslt = orderRequestService.validOCRStus2(params);
-    	return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/selectOrderSimulatorViewByOrderNo.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> selectOrderSimulatorViewByOrderNo(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.selectOrderSimulatorViewByOrderNo(params);
+    return ResponseEntity.ok(rslt);
+  }
 
-     //BY KV -order cancellation no yet complete sal0020d)
-    @RequestMapping(value = "/validOCRStus3.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> validOCRStus3(@RequestParam Map<String, Object> params)    {
-        EgovMap rslt = orderRequestService.validOCRStus3(params);
-        return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/selectOderOutsInfo.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selectOderOutsInfo(@RequestParam Map<String, Object> params) {
+    List<EgovMap> ordOutInfoList = orderLedgerService.getOderOutsInfo(params);
+    return ResponseEntity.ok(ordOutInfoList);
+  }
 
-    //By KV - order cancellation complete sal0020d ; log0038d active
-    @RequestMapping(value = "/validOCRStus4.do", method = RequestMethod.GET)
-    public ResponseEntity<EgovMap> validOCRStus4(@RequestParam Map<String, Object> params)    {
-        EgovMap rslt = orderRequestService.validOCRStus4(params);
-        return ResponseEntity.ok(rslt);
-    }
+  @RequestMapping(value = "/selectObligtPriod.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> selectObligtPriod(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.selectObligtPriod(params);
+    return ResponseEntity.ok(rslt);
+  }
 
+  @RequestMapping(value = "/selectPenaltyAmt.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> selectPenaltyAmt(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.selectPenaltyAmt(params);
+    return ResponseEntity.ok(rslt);
+  }
+
+  @RequestMapping(value = "/checkeAutoDebitDeduction.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> checkeAutoDebitDeduction(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.checkeAutoDebitDeduction(params);
+    return ResponseEntity.ok(rslt);
+  }
+
+  // BY KV order installation no yet complete (CallLog Type - 257, CCR0001D -
+  // 20, SAL00046 - Active )
+  @RequestMapping(value = "/validOCRStus.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> validOCRStus(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.validOCRStus(params);
+    return ResponseEntity.ok(rslt);
+  }
+
+  /* Valid OCR Status - (CallLog Type - 257, Stus - 1, SAL00046 - NO RECORD ) */
+  @RequestMapping(value = "/validOCRStus2.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> validOCRStus2(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.validOCRStus2(params);
+    return ResponseEntity.ok(rslt);
+  }
+
+  // BY KV -order cancellation no yet complete sal0020d)
+  @RequestMapping(value = "/validOCRStus3.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> validOCRStus3(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.validOCRStus3(params);
+    return ResponseEntity.ok(rslt);
+  }
+
+  // By KV - order cancellation complete sal0020d ; log0038d active
+  @RequestMapping(value = "/validOCRStus4.do", method = RequestMethod.GET)
+  public ResponseEntity<EgovMap> validOCRStus4(@RequestParam Map<String, Object> params) {
+    EgovMap rslt = orderRequestService.validOCRStus4(params);
+    return ResponseEntity.ok(rslt);
+  }
+
+  @RequestMapping(value = "/selectCodeList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selectCodeList(@RequestParam Map<String, Object> params) {
+    logger.debug("==============================selectCodeList====================================");
+    logger.debug("= PARAMS = " + params.toString());
+    logger.debug("==============================selectCodeList====================================");
+    List<EgovMap> rsltList = orderRequestService.selectCodeList(params);
+    return ResponseEntity.ok(rsltList);
+  }
+
+  @RequestMapping(value = "/chkCboSal.do", method = RequestMethod.GET)
+  public ResponseEntity<Integer> chkCboSal(@RequestParam Map<String, Object> params) {
+    logger.debug("=============================chkCboSal=====================================");
+    logger.debug("= PARAMS = " + params.toString());
+    logger.debug("=============================chkCboSal=====================================");
+    Integer stat = orderRequestService.chkCboSal(params);
+    return ResponseEntity.ok(stat);
+  }
 }
