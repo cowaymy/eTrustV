c597b6540647d9aa97dc68911270338589e957e3
etc95 <vacant@coway.com.my>
2019-11-27 03:04:19 +0000

    2019.11.27 [ KR_HAN ] 조회조건 추가

--- src/main/java/com/coway/trust/api/mobile/payment/paymentList/PaymentListApiController.java
+++ src/main/java/com/coway/trust/api/mobile/payment/paymentList/PaymentListApiController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,13 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coway.trust.AppConstants;
 import com.coway.trust.api.mobile.payment.payment.PaymentDto;
+import com.coway.trust.biz.payment.mobileAppTicket.service.MobileAppTicketService;
 import com.coway.trust.biz.payment.payment.service.PaymentListApiService;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
@@ -46,6 +50,9 @@ public class PaymentListApiController {
 	@Resource(name = "paymentListApiService")
 	private PaymentListApiService paymentListApiService;
 
+	@Resource(name = "mobileAppTicketService")
+	private MobileAppTicketService mobileAppTicketService;
+
 	@Autowired
 	private MessageSourceAccessor messageAccessor;
 
@@ -72,4 +79,17 @@ public class PaymentListApiController {
        return ResponseEntity.ok(paymentList);
 	}
 
+	  @RequestMapping(value = "/selectMobileAppTicketStatus", method = RequestMethod.GET)
+	  public ResponseEntity<List<PaymentListDto>> selectMobileAppTicketJsonList(@RequestParam Map<String, Object> params,
+	      HttpServletRequest request, ModelMap model) {
+
+	    List<EgovMap> selectTicketList = null;
+
+	    selectTicketList = mobileAppTicketService.selectMobileAppTicketStatus(params);
+
+	    List<PaymentListDto> ticketList = selectTicketList.stream().map(r -> PaymentListDto.create(r)).collect(Collectors.toList());
+
+	    // 데이터 리턴.
+	    return ResponseEntity.ok(ticketList);
+	  }
 }
