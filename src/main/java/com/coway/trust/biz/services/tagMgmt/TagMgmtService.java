c50dd19c254490321a0196da8ce97c72a606b8b6
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-12-09 01:40:12 +0000

    - RE-STRUCTURE TagMgmtService - ADD FILE UPLOAD FUNCTION

--- src/main/java/com/coway/trust/biz/services/tagMgmt/TagMgmtService.java
+++ src/main/java/com/coway/trust/biz/services/tagMgmt/TagMgmtService.java
@@ -8,32 +8,42 @@ import com.coway.trust.cmmn.model.SessionVO;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 09/12/2019    ONGHC      1.0.1       - RE-STRUCTURE TagMgmtService
+ *                                      - ADD FILE UPLOAD FUNCTION
+ *********************************************************************************************/
+
 public interface TagMgmtService {
 
-	List<EgovMap> getTagStatus(Map<String, Object> params);
+  List<EgovMap> getTagStatus(Map<String, Object> params);
 
-	EgovMap getDetailTagStatus(Map<String, Object> params);
+  EgovMap getDetailTagStatus(Map<String, Object> params);
 
-	int addRemarkResult(Map<String, Object> params, SessionVO sessionVO)  throws ParseException;
+  List<EgovMap> getTagMgntStat(Map<String, Object> params);
 
-	List<EgovMap> getTagRemark(Map<String, Object> params);
+  int addRemarkResult(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
 
-	List<EgovMap> getMainDeptList();
+  List<EgovMap> getTagRemark(Map<String, Object> params);
 
-	List<EgovMap> getSubDeptList(Map<String, Object> params);
+  List<EgovMap> getMainDeptList();
 
-	List<EgovMap> getMainInquiryList();
+  List<EgovMap> getSubDeptList(Map<String, Object> params);
 
-	List<EgovMap> getSubInquiryList(Map<String, Object> params);
+  List<EgovMap> getMainInquiryList();
 
-	EgovMap getOrderInfo(Map<String, Object> params);
+  List<EgovMap> getSubInquiryList(Map<String, Object> params);
 
-	EgovMap getCallerInfo(Map<String, Object> params);
+  EgovMap getOrderInfo(Map<String, Object> params);
 
-	EgovMap selectOrderSalesmanViewByOrderID(Map<String, Object> params);
+  EgovMap getCallerInfo(Map<String, Object> params);
 
-	EgovMap selectOrderServiceMemberViewByOrderID(Map<String, Object> params);
+  EgovMap selectOrderSalesmanViewByOrderID(Map<String, Object> params);
 
-	List<EgovMap> getAttachList(Map<String, Object> params);
+  EgovMap selectOrderServiceMemberViewByOrderID(Map<String, Object> params);
 
+  List<EgovMap> getAttachList(Map<String, Object> params);
+
+  List<EgovMap> getAttachList2(Map<String, Object> params);
 }
