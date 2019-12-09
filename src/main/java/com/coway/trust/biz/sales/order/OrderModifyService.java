6301b87e234000038f5d0de1bea51460de86b173
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-12-09 09:21:41 +0000

    ADD chkCboPromPck

--- src/main/java/com/coway/trust/biz/sales/order/OrderModifyService.java
+++ src/main/java/com/coway/trust/biz/sales/order/OrderModifyService.java
@@ -14,58 +14,60 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface OrderModifyService {
 
-	void updateOrderBasinInfo(Map<String, Object> params, SessionVO sessionVO);
+  void updateOrderBasinInfo(Map<String, Object> params, SessionVO sessionVO);
 
-	EgovMap selectBillGrpMailingAddr(Map<String, Object> params) throws Exception;
+  EgovMap selectBillGrpMailingAddr(Map<String, Object> params) throws Exception;
 
-	void updateOrderMailingAddress(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
+  void updateOrderMailingAddress(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
 
-	EgovMap selectBillGrpCntcPerson(Map<String, Object> params) throws Exception;
+  EgovMap selectBillGrpCntcPerson(Map<String, Object> params) throws Exception;
 
-	void updateCntcPerson(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
+  void updateCntcPerson(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
 
-	EgovMap checkNricEdit(Map<String, Object> params) throws Exception;
+  EgovMap checkNricEdit(Map<String, Object> params) throws Exception;
 
-	EgovMap selectCustomerInfo(Map<String, Object> params) throws Exception;
+  EgovMap selectCustomerInfo(Map<String, Object> params) throws Exception;
 
-	EgovMap checkNricExist(Map<String, Object> params) throws Exception;
+  EgovMap checkNricExist(Map<String, Object> params) throws Exception;
 
-	void updateNric(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
+  void updateNric(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
 
-	EgovMap selectInstallInfo(Map<String, Object> params) throws Exception;
+  EgovMap selectInstallInfo(Map<String, Object> params) throws Exception;
 
-	EgovMap selectInstallAddrInfo(Map<String, Object> params) throws Exception;
+  EgovMap selectInstallAddrInfo(Map<String, Object> params) throws Exception;
 
-	EgovMap selectInstallCntcInfo(Map<String, Object> params) throws Exception;
+  EgovMap selectInstallCntcInfo(Map<String, Object> params) throws Exception;
 
-	EgovMap selectInstRsltCount(Map<String, Object> params) throws Exception;
+  EgovMap selectInstRsltCount(Map<String, Object> params) throws Exception;
 
-	EgovMap selectGSTZRLocationCount(Map<String, Object> params) throws Exception;
+  EgovMap selectGSTZRLocationCount(Map<String, Object> params) throws Exception;
 
-	EgovMap selectGSTZRLocationByAddrIdCount(Map<String, Object> params) throws Exception;
+  EgovMap selectGSTZRLocationByAddrIdCount(Map<String, Object> params) throws Exception;
 
-	void updateInstallInfo(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
+  void updateInstallInfo(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
 
-	EgovMap selectRentPaySetInfo(Map<String, Object> params) throws Exception;
+  EgovMap selectRentPaySetInfo(Map<String, Object> params) throws Exception;
 
-	void updatePaymentChannel(Map<String, Object> params, SessionVO sessionVO) throws Exception;
+  void updatePaymentChannel(Map<String, Object> params, SessionVO sessionVO) throws Exception;
 
-	void saveDocSubmission(OrderVO orderVO, SessionVO sessionVO) throws Exception;
+  void saveDocSubmission(OrderVO orderVO, SessionVO sessionVO) throws Exception;
 
-	List<EgovMap> selectReferralList(Map<String, Object> params);
+  List<EgovMap> selectReferralList(Map<String, Object> params);
 
-	List<EgovMap> selectStateCodeList(Map<String, Object> params);
+  List<EgovMap> selectStateCodeList(Map<String, Object> params);
 
-	void saveReferral(OrderModifyVO orderModifyVO, SessionVO sessionVO) throws Exception;
+  void saveReferral(OrderModifyVO orderModifyVO, SessionVO sessionVO) throws Exception;
 
-	void updatePromoPriceInfo(SalesOrderMVO salesOrderMVO, SessionVO sessionVO) throws Exception;
+  void updatePromoPriceInfo(SalesOrderMVO salesOrderMVO, SessionVO sessionVO) throws Exception;
 
-	void updateGSTEURCertificate(GSTEURCertificateVO gSTEURCertificateVO, SessionVO sessionVO);
+  void updateGSTEURCertificate(GSTEURCertificateVO gSTEURCertificateVO, SessionVO sessionVO);
 
-	void updateOrderMailingAddress2(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
+  void updateOrderMailingAddress2(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
 
-	EgovMap getInstallDetail(Map<String, Object> params);
-	
-	void updateCcpOrderBasinInfo(Map<String, Object> params, SessionVO sessionVO);
+  EgovMap getInstallDetail(Map<String, Object> params);
+
+  void updateCcpOrderBasinInfo(Map<String, Object> params, SessionVO sessionVO);
+
+  int chkCboPromPck(Map<String, Object> params);
 
 }
