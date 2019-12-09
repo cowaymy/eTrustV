2780af0a781a63463efb8c50d72e8723cf672ec2
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2019-12-09 01:40:38 +0000

    - RE-STRUCTURE TagMgmtServiceImpl - ADD FILE UPLOAD FUNCTION

--- src/main/java/com/coway/trust/biz/services/tagMgmt/impl/TagMgmtServiceImpl.java
+++ src/main/java/com/coway/trust/biz/services/tagMgmt/impl/TagMgmtServiceImpl.java
@@ -16,116 +16,118 @@ import com.coway.trust.cmmn.model.SessionVO;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 09/12/2019    ONGHC      1.0.1       - RE-STRUCTURE TagMgmtServiceImpl
+ *                                      - ADD FILE UPLOAD FUNCTION
+ *********************************************************************************************/
+
 @Service("tagMgmtService")
 public class TagMgmtServiceImpl implements TagMgmtService {
 
-	private static final Logger logger = LoggerFactory.getLogger(HolidayService.class);
+  private static final Logger logger = LoggerFactory.getLogger(HolidayService.class);
 
-	@Resource(name = "tagMgmtMapper")
-	private TagMgmtMapper tagMgmtMapper;
+  @Resource(name = "tagMgmtMapper")
+  private TagMgmtMapper tagMgmtMapper;
 
-	@Override
-	public List<EgovMap> getTagStatus(Map<String, Object> params) {
+  @Override
+  public List<EgovMap> getTagStatus(Map<String, Object> params) {
+    return tagMgmtMapper.selectTagStatus(params);
+  }
 
-		return tagMgmtMapper.selectTagStatus(params);
-	}
+  @Override
+  public List<EgovMap> getTagMgntStat(Map<String, Object> params) {
+    return tagMgmtMapper.getTagMgntStat(params);
+  }
 
-	@Override
-	public EgovMap getDetailTagStatus(Map<String, Object> params) {
+  @Override
+  public EgovMap getDetailTagStatus(Map<String, Object> params) {
+    return tagMgmtMapper.selectDetailTagStatus(params);
+  }
 
-		return tagMgmtMapper.selectDetailTagStatus(params);
-	}
+  @Override
+  public int addRemarkResult(Map<String, Object> params, SessionVO sessionVO) throws ParseException {
+    int cnt = 0;
 
-	@Override
-	public int addRemarkResult(Map<String, Object> params, SessionVO sessionVO)   throws ParseException{
-		int cnt =0;
+    params.put("userId", sessionVO.getUserId());
 
-		params.put("userId", sessionVO.getUserId());
+    if (params.get("atchFileGrpId") != null) {
+      tagMgmtMapper.insertCcr0015d(params);
+    }
 
+    EgovMap mapCheckCallEntryId = tagMgmtMapper.selectCallEntryId(params);
+    if (mapCheckCallEntryId == null) {
+      cnt = tagMgmtMapper.insertCcr0006d(params);
+    } else {
+      cnt = tagMgmtMapper.updateCcr0006d(params);
+    }
 
+    EgovMap mapCallEntryId = tagMgmtMapper.selectCallEntryId(params);
+    params.put("callEntryId", mapCallEntryId.get("callEntryId"));
 
-		EgovMap mapCheckCallEntryId = tagMgmtMapper.selectCallEntryId(params);
-		if(mapCheckCallEntryId == null){
+    cnt += tagMgmtMapper.insertCcr0007d(params);
 
-    		cnt = tagMgmtMapper.insertCcr0006d(params);
+    return cnt;
+  }
 
-		}
-		else {
+  @Override
+  public List<EgovMap> getTagRemark(Map<String, Object> params) {
+    EgovMap mapCallEntryId = tagMgmtMapper.selectCallEntryId(params);
+    if (mapCallEntryId != null) {
+      params.put("callEntryId", mapCallEntryId.get("callEntryId"));
+    }
+    return tagMgmtMapper.selectTagRemarks(params);
+  }
 
-			cnt = tagMgmtMapper.updateCcr0006d(params);
+  @Override
+  public List<EgovMap> getMainDeptList() {
+    return tagMgmtMapper.selectMainDept();
+  }
 
-		}
+  @Override
+  public List<EgovMap> getSubDeptList(Map<String, Object> params) {
+    return tagMgmtMapper.selectSubDept(params);
+  }
 
-		EgovMap mapCallEntryId= tagMgmtMapper.selectCallEntryId(params);
-		params.put("callEntryId", mapCallEntryId.get("callEntryId") );
+  @Override
+  public List<EgovMap> getMainInquiryList() {
+    return tagMgmtMapper.selectMainInquiryList();
+  }
 
-		cnt += tagMgmtMapper.insertCcr0007d(params);
+  @Override
+  public List<EgovMap> getSubInquiryList(Map<String, Object> params) {
+    return tagMgmtMapper.selectSubInquiryList(params);
+  }
 
-		return cnt;
-	}
+  @Override
+  public EgovMap getOrderInfo(Map<String, Object> params) {
+    return tagMgmtMapper.getOrderInfo(params);
+  }
 
-	@Override
-	public List<EgovMap> getTagRemark(Map<String, Object> params) {
+  @Override
+  public EgovMap getCallerInfo(Map<String, Object> params) {
+    return tagMgmtMapper.getCallerInfo(params);
+  }
 
-		EgovMap mapCallEntryId= tagMgmtMapper.selectCallEntryId(params);
-		if(mapCallEntryId != null){
-			params.put("callEntryId", mapCallEntryId.get("callEntryId") );
-		}
-		return tagMgmtMapper.selectTagRemarks(params);
-	}
+  @Override
+  public EgovMap selectOrderSalesmanViewByOrderID(Map<String, Object> params) {
+    return tagMgmtMapper.selectOrderSalesmanViewByOrderID(params);
+  }
 
-	@Override
-	public List<EgovMap> getMainDeptList() {
-		// TODO Auto-generated method stub
-		return tagMgmtMapper.selectMainDept();
-	}
+  @Override
+  public EgovMap selectOrderServiceMemberViewByOrderID(Map<String, Object> params) {
+    return tagMgmtMapper.selectOrderServiceMemberViewByOrderID(params);
+  }
 
-	@Override
-	public List<EgovMap> getSubDeptList(Map<String, Object> params) {
+  @Override
+  public List<EgovMap> getAttachList(Map<String, Object> params) {
+    return tagMgmtMapper.selectAttachList(params);
+  }
 
-		return tagMgmtMapper.selectSubDept(params);
-	}
-
-	@Override
-	public List<EgovMap> getMainInquiryList() {
-		// TODO Auto-generated method stub
-		return tagMgmtMapper.selectMainInquiryList();
-	}
-
-	@Override
-	public List<EgovMap> getSubInquiryList(Map<String, Object> params) {
-
-		return tagMgmtMapper.selectSubInquiryList(params);
-	}
-
-	@Override
-	public EgovMap getOrderInfo(Map<String, Object> params) {
-
-		return tagMgmtMapper.getOrderInfo(params);
-	}
-
-	@Override
-	public EgovMap getCallerInfo(Map<String, Object> params) {
-
-		return tagMgmtMapper.getCallerInfo(params);
-	}
-
-	@Override
-	public EgovMap selectOrderSalesmanViewByOrderID(Map<String, Object> params) {
-
-		return tagMgmtMapper.selectOrderSalesmanViewByOrderID(params);
-	}
-
-	@Override
-	public EgovMap selectOrderServiceMemberViewByOrderID(Map<String, Object> params) {
-
-		return tagMgmtMapper.selectOrderServiceMemberViewByOrderID(params);
-	}
-
-	@Override
-	public List<EgovMap> getAttachList(Map<String, Object> params) {
-
-		return tagMgmtMapper.selectAttachList(params);
-	}
+  @Override
+  public List<EgovMap> getAttachList2(Map<String, Object> params) {
+    return tagMgmtMapper.selectAttachList2(params);
+  }
 
 }
