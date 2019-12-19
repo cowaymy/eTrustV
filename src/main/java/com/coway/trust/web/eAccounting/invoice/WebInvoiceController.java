c45a7e892b5d8e92775ae19ef7c390d20c6c31ce
Vannie <vannie.koh@coway.com.my>
2019-12-19 07:08:01 +0000

    Petty Cash Module Enhancement 1) To check Custodian ID upon create New Custodian. 2) To be able to return the result by searching the Cost Centre Code in Request Petty Cash screen. 3) To remove unnecessary information from viewing the detail in Cash Request and Approval screens. a) Invoice date b) Payment Due Date c) GL Account d) Budget e) Tax Code f) Description 4) To add following information into the grid view in Cash Request and Approval details screens. a) Remark b) Attachment 5) To add the below info into Interface table after perform approval. a) Posting date b) Sequence No. c) Cos Centre Code d) Remark 6) Add mandatory checking at create New Petty Cash Request for all editable fields.

--- src/main/java/com/coway/trust/web/eAccounting/invoice/WebInvoiceController.java
+++ src/main/java/com/coway/trust/web/eAccounting/invoice/WebInvoiceController.java
@@ -206,39 +206,45 @@ public class WebInvoiceController {
 		
 	}
 
-	@RequestMapping(value = "/webInvoiceRqstViewPop.do")
-	public String webInvoiceRqstViewPop(@RequestParam Map<String, Object> params, ModelMap model) {
-
-		LOGGER.debug("params =====================================>>  " + params);
-
-		List<EgovMap> appvLineInfo = webInvoiceService.selectAppvLineInfo(params);
-		for(int i = 0; i < appvLineInfo.size(); i++) {
-			EgovMap info = appvLineInfo.get(i);
-			if("J".equals(info.get("appvStus"))) {
-				String rejctResn = webInvoiceService.selectRejectOfAppvPrcssNo(info);
-				model.addAttribute("rejctResn", rejctResn);
-			}
-		}
-		List<EgovMap> appvInfoAndItems = webInvoiceService.selectAppvInfoAndItems(params);
-
-		// TODO appvPrcssStus 생성
-		String appvPrcssStus = webInvoiceService.getAppvPrcssStus(appvLineInfo, appvInfoAndItems);
-		
-		// VANNIE ADD TO GET FILE GROUP ID, FILE ID AND FILE COUNT.
-		List<EgovMap> atchFileData = webInvoiceService.selectAtchFileData(params);
-		
-		if(atchFileData.isEmpty()){
-		  model.addAttribute("atchFileCnt", 0);
-		} else {
-		  model.addAttribute("atchFileCnt", atchFileData.get(0).get("fileCnt"));
-		}
-
-		model.addAttribute("appvPrcssStus", appvPrcssStus);
-		model.addAttribute("appvPrcssResult", appvInfoAndItems.get(0).get("appvPrcssStus"));
-		model.addAttribute("appvInfoAndItems", new Gson().toJson(appvInfoAndItems));
-
-		return "eAccounting/webInvoice/webInvoiceRequestViewPop";
-	}
+  @RequestMapping(value = "/webInvoiceRqstViewPop.do")
+  public String webInvoiceRqstViewPop(@RequestParam Map<String, Object> params, ModelMap model) {
+    
+    LOGGER.debug("params =====================================>>  " + params);
+    
+    String clmType = params.get("clmType").toString();
+    
+    List<EgovMap> appvLineInfo = webInvoiceService.selectAppvLineInfo(params);
+    for (int i = 0; i < appvLineInfo.size(); i++) {
+      EgovMap info = appvLineInfo.get(i);
+      if ("J".equals(info.get("appvStus"))) {
+        String rejctResn = webInvoiceService.selectRejectOfAppvPrcssNo(info);
+        model.addAttribute("rejctResn", rejctResn);
+      }
+    }
+    List<EgovMap> appvInfoAndItems = webInvoiceService.selectAppvInfoAndItems(params);
+    
+    // TODO appvPrcssStus 생성
+    String appvPrcssStus = webInvoiceService.getAppvPrcssStus(appvLineInfo, appvInfoAndItems);
+    
+    // VANNIE ADD TO GET FILE GROUP ID, FILE ID AND FILE COUNT.
+    List<EgovMap> atchFileData = webInvoiceService.selectAtchFileData(params);
+    
+    if (atchFileData.isEmpty()) {
+      model.addAttribute("atchFileCnt", 0);
+    } else {
+      model.addAttribute("atchFileCnt", atchFileData.get(0).get("fileCnt"));
+    }
+    
+    model.addAttribute("appvPrcssStus", appvPrcssStus);
+    model.addAttribute("appvPrcssResult", appvInfoAndItems.get(0).get("appvPrcssStus"));
+    model.addAttribute("appvInfoAndItems", new Gson().toJson(appvInfoAndItems));
+    
+    if (clmType.equalsIgnoreCase("R1")) {
+      return "eAccounting/webInvoice/webInvoiceApproveViewR1Pop";
+    } else {
+      return "eAccounting/webInvoice/webInvoiceRequestViewPop";
+    }
+  }
 
 	@RequestMapping(value = "/fileListOfAppvPrcssNoPop.do")
 	public String fileListOfAppvPrcssNoPop(@RequestParam Map<String, Object> params, ModelMap model) {
