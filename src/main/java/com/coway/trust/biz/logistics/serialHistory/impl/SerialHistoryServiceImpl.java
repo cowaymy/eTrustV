6d3929e750473137fcd1b1b06f71680d156d8a59
etc95 <vacant@coway.com.my>
2019-12-23 04:53:59 +0000

    KR_HAN : Order Serial No. History ADD

---
+++ src/main/java/com/coway/trust/biz/logistics/serialHistory/impl/SerialHistoryServiceImpl.java
@@ -0,0 +1,65 @@
+/**
+ *
+ */
+package com.coway.trust.biz.logistics.serialHistory.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.coway.trust.biz.logistics.serialChange.impl.SerialChangeServiceImpl;
+import com.coway.trust.biz.logistics.serialHistory.SerialHistoryService;
+import com.crystaldecisions.jakarta.poi.util.StringUtil;
+
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+/**
+ * @ClassName : SerialHistoryServiceImpl.java
+ * @Description : SerialHistoryServiceImpl
+ *
+ * @History
+ * <pre>
+ * Date            Author       Description
+ * -------------  -----------  -------------
+ * 2019. 12. 21.   KR-HAN        First creation
+ * </pre>
+ */
+@Service("serialHistoryService")
+public class SerialHistoryServiceImpl extends EgovAbstractServiceImpl implements SerialHistoryService {
+
+  private static Logger logger = LoggerFactory.getLogger(SerialChangeServiceImpl.class);
+
+  @Resource(name = "serialHistoryMapper")
+  private SerialHistoryMapper serialHistoryMapper;
+
+
+    /**
+     * 시리얼 이력 조회
+     * @Author KR-HAN
+     * @Date 2019. 12. 21.
+     * @param params
+     * @return
+     * @see com.coway.trust.biz.logistics.serialHistory.SerialHistoryService#selectSerialHistoryList(java.util.Map)
+     */
+    @Override
+	public List<EgovMap> selectSerialHistoryList(Map<String, Object> params) {
+
+    	if( !StringUtils.isEmpty( params.get("serialNo") ) || !StringUtils.isEmpty( params.get("refDocNo") ) || !StringUtils.isEmpty( params.get("#ordNo") )){
+    		System.out.println("++++ 1 ::");
+    	}else{
+    		System.out.println("++++ 2 ::");
+    		params.put("startCrtDt", "");
+    		params.put("endCrtDt", "");
+    	}
+
+		return serialHistoryMapper.selectSerialHistoryList(params);
+	}
+
+}
