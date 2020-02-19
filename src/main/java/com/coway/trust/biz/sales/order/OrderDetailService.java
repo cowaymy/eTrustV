9a2b9de85d81f090a5489d16b3112fa45b724fe2
etc07 <vacant@coway.com.my>
2020-02-19 07:40:16 +0000

    Add getInstAsPSI

--- src/main/java/com/coway/trust/biz/sales/order/OrderDetailService.java
+++ src/main/java/com/coway/trust/biz/sales/order/OrderDetailService.java
@@ -15,39 +15,41 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
  *
  */
 public interface OrderDetailService {
-  
+
   public EgovMap selectOrderBasicInfo(Map<String, Object> params, SessionVO sessionVO) throws Exception;
-  
+
   public List<EgovMap> selectCallLogList(Map<String, Object> params);
-  
+
   public List<EgovMap> selectSameRentalGrpOrderList(Map<String, Object> params);
-  
+
   public List<EgovMap> selectMembershipInfoList(Map<String, Object> params);
-  
+
   public List<EgovMap> selectDocumentList(Map<String, Object> params);
-  
+
   List<EgovMap> selectPaymentMasterList(Map<String, Object> params);
-  
+
   List<EgovMap> selectAutoDebitList(Map<String, Object> params);
-  
+
   List<EgovMap> selectDiscountList(Map<String, Object> params);
-  
+
   List<EgovMap> selectLast6MonthTransList(Map<String, Object> params);
-  
+
   List<EgovMap> selectLast6MonthTransListNew(Map<String, Object> params);
-  
+
   EgovMap selectBasicInfo(Map<String, Object> params) throws Exception;
-  
+
   EgovMap selectGSTCertInfo(Map<String, Object> params);
-  
+
   List<EgovMap> selectEcashList(Map<String, Object> params);
-  
+
   EgovMap selectCurrentBSResultByBSNo(Map<String, Object> params);
-  
+
   List<EgovMap> selectASInfoList(Map<String, Object> params);
-  
+
   List<EgovMap> getInstImg(Map<String, Object> params);
-  
+
   public List<EgovMap> selectGSTRebateList(Map<String, Object> params);
-  
+
+  public List<EgovMap> getInstAsPSIData(Map<String, Object> params);
+
 }
