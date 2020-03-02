fa80446f3e114961bb36f081f6cab2703f1304d7
Tommy, Thun Ping Yi <pingyi.thun@coway.com.my>
2020-03-02 01:27:43 +0000

    Add selectHPDeptCodeListByLv - TPY

--- src/main/java/com/coway/trust/biz/commission/system/CommissionSystemService.java
+++ src/main/java/com/coway/trust/biz/commission/system/CommissionSystemService.java
@@ -19,35 +19,35 @@ public interface CommissionSystemService
      * @return
      */
     List<EgovMap> selectOrgGrList(Map<String, Object> params);
-    
+
     /**
      * search Organization List
      * @param params
      * @return
      */
     List<EgovMap> selectOrgList(Map<String, Object> params);
-    
+
     /**
      * search selectRuleBookMngList List
      * @param params
      * @return
      */
     List<EgovMap> selectRuleBookOrgMngList(Map<String, Object> params);
-    
+
     /**
      * add coommission rule book management Data
      * @param updateList
      * @return
      */
     int addCommissionGrid(List<Object> updateList , String loginId);
-    
+
     /**
      * update coommission rule book management Data
      * @param addList
      * @return
      */
     int udtCommissionGrid(List<Object> addList, String loginId);
-    
+
     /**
      * delete coommission rule book management Data
      * @param removeList
@@ -60,34 +60,34 @@ public interface CommissionSystemService
      * @return
      */
     List<EgovMap> selectOrgGrCdListAll(Map<String, Object> params);
-    
+
     /**
      *  search Organization Code List
      * @param params
      * @return
      */
     List<EgovMap> selectOrgCdListAll(Map<String, Object> params);
-    
+
     /**
      *  search Organization Code Item List
      * @param params
      * @return
      */
-    
+
     /**
      *  search Organization Gruop Code List
      * @param params
      * @return
      */
     List<EgovMap> selectOrgGrCdList(Map<String, Object> params);
-    
+
     /**
      *  search Organization Code List
      * @param params
      * @return
      */
     List<EgovMap> selectOrgCdList(Map<String, Object> params);
-    
+
     /**
      *  search Organization Code Item List
      * @param params
@@ -101,93 +101,95 @@ public interface CommissionSystemService
      * @return
      */
     List<EgovMap> selectRuleBookItemMngList(Map<String, Object> params);
-    
+
     /**
      * add coommission rule book management Data
      * @param updateList
      * @return
      */
     String addCommissionItemGrid(List<Object> updateList , String loginId);
-    
+
     /**
      * update coommission rule book management Data
      * @param addList
      * @return
      */
     String udtCommissionItemGrid(List<Object> addList, String loginId);
-    
+
     /**
      * delete coommission rule book management Data
      * @param removeList
      * @return
      */
-    
+
     /**
      * add coommission rule  management Data
      * @param updateList
      * @return
      */
     int addCommissionRuleData(Map<String, Object> params, String loginId);
-    
+
     /**
      * search selectRuleBookMngList List
      * @param params
      * @return
      */
     List<EgovMap> selectRuleBookMngList(Map<String, Object> params);
-    
+
     /**
      * update coommission rule  management Data
      * @param updateList
      * @return
      */
     int udtCommissionRuleData(Map<String, Object> params, String loginId);
-    
+
     int cntUpdateDate(Map<String, Object> params);
-    
+
     void udtCommissionRuleData(Map<String, Object> params);
-    
+
     List<EgovMap> selectRuleValueType(Map<String, Object> params);
-    
+
     /**
      *  search Weekly List
      * @param params
      * @return
      */
     List<EgovMap> selectWeeklyList(Map<String, Object> params);
-    
+
     /**
      * add coommission rule book management Data
      * @param updateList
      * @return
      */
     int addWeeklyCommissionGrid(List<Object> updateList , String loginId);
-    
+
     /**
      * update coommission rule book management Data
      * @param addList
      * @return
      */
     int udtWeeklyCommissionGrid(List<Object> addList, String loginId);
-    
+
     /**
      * simulation management list
      * @param params
      * @return
      */
     List<EgovMap> selectSimulationMngList(Map<String, Object> params);
-    
+
     /**
      * simulation valid itemSeq search
      * @param itemCd
      * @return
      */
     String varsionVaildSearch(String itemCd);
-    
+
     /**
      * version insert
      * @param map
      * @param list
      */
     void versionItemInsert(Map<String, Object> map,List<Object>list);
+
+    List<EgovMap> selectHPDeptCodeListByLv(Map<String, Object> params);
 }
