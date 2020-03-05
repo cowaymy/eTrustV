62bb0d61d4cd7713d9220d3eb2163e065ffeb611
etc07 <vacant@coway.com.my>
2020-03-05 01:46:11 +0000

    Add HS Report (Customer Sign) feature

--- src/main/java/com/coway/trust/biz/services/report/impl/HSReportMapper.java
+++ src/main/java/com/coway/trust/biz/services/report/impl/HSReportMapper.java
@@ -8,35 +8,37 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 @Mapper("HSReportMapper")
 public interface HSReportMapper {
-  
+
   List<EgovMap> selectHSReportSingle(Map<String, Object> params);
-  
+
   List<EgovMap> selectHSReportGroup(Map<String, Object> params);
-  
+
   List<EgovMap> selectCMGroupList(Map<String, Object> params);
-  
+
   List<EgovMap> selectCodyList(Map<String, Object> params);
-  
+
   List<EgovMap> selectReportBranchCodeList(Map<String, Object> params);
-  
+
   List<EgovMap> selectDeptCodeList(Map<String, Object> params);
-  
+
   List<EgovMap> selectDscCodeList(Map<String, Object> params);
-  
+
   List<EgovMap> selectInsStatusList(Map<String, Object> params);
-  
+
   List<EgovMap> selectCodyCodeList(Map<String, Object> params);
-  
+
   List<EgovMap> selectAreaCodeList(Map<String, Object> params);
-  
+
   List<EgovMap> selectCodyCodeList_1(Map<String, Object> params);
-  
+
   List<EgovMap> safetyLevelList(Map<String, Object> params);
-  
+
   List<EgovMap> getCodyList2(Map<String, Object> params);
-  
+
   List<EgovMap> getCdUpMem(Map<String, Object> params);
-  
+
   List<EgovMap> selectCodyBranch(Map<String, Object> params);
+
+  List<EgovMap> selectHSReportCustSign(Map<String, Object> params);
 
 }
