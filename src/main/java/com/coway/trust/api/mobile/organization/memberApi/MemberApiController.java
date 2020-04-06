2b33ab2a7429efd8016a1ab71d5bc240db4d85a9
etc07 <vacant@coway.com.my>
2020-04-06 10:48:59 +0000

    Restructure Messy Code

--- src/main/java/com/coway/trust/api/mobile/organization/memberApi/MemberApiController.java
+++ src/main/java/com/coway/trust/api/mobile/organization/memberApi/MemberApiController.java
@@ -26,37 +26,33 @@ import io.swagger.annotations.ApiOperation;
  * @Description : TO-DO Class Description
  *
  * @History
- * <pre>
+ *
+ *          <pre>
  * Date             Author          Description
  * -------------    -----------     -------------
  * 2019. 09. 09.    KR-JAEMJAEM:)   First creation
- * </pre>
+ * 2020. 04. 09.    MY-ONGHC         Restructure Messy Code
+ *          </pre>
  */
 @Api(value = "MemberApiController", description = "MemberApiController")
 @RestController(value = "MemberApiController")
 @RequestMapping(AppConstants.MOBILE_API_BASE_URI + "/memberApi")
 public class MemberApiController {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(MemberApiController.class);
 
+  @Resource(name = "MemberApiService")
+  private MemberApiService memberApiService;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(MemberApiController.class);
-
-
-
-	@Resource(name = "MemberApiService")
-	private MemberApiService memberApiService;
-
-
-
-	@ApiOperation(value = "selectMemberList", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@RequestMapping(value = "/selectMemberList", method = RequestMethod.GET)
-	public ResponseEntity<List<MemberApiDto>> selectMemberList(@ModelAttribute MemberApiForm param) throws Exception {
-		List<EgovMap> selectMemberList = memberApiService.selectMemberList(param);
-		if(LOGGER.isDebugEnabled()){
-			for (int i = 0; i < selectMemberList.size(); i++) {
-				LOGGER.debug("selectMemberList    값 : {}", selectMemberList.get(i));
-			}
-		}
-		return ResponseEntity.ok(selectMemberList.stream().map(r -> MemberApiDto.create(r)).collect(Collectors.toList()));
-	}
+  @ApiOperation(value = "selectMemberList", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  @RequestMapping(value = "/selectMemberList", method = RequestMethod.GET)
+  public ResponseEntity<List<MemberApiDto>> selectMemberList(@ModelAttribute MemberApiForm param) throws Exception {
+    List<EgovMap> selectMemberList = memberApiService.selectMemberList(param);
+    if (LOGGER.isDebugEnabled()) {
+      for (int i = 0; i < selectMemberList.size(); i++) {
+        LOGGER.debug("selectMemberList    값 : {}", selectMemberList.get(i));
+      }
+    }
+    return ResponseEntity.ok(selectMemberList.stream().map(r -> MemberApiDto.create(r)).collect(Collectors.toList()));
+  }
 }
