b3830c2734fd387a51059af0a7dd40844c3d7814
etc07 <vacant@coway.com.my>
2020-04-06 10:49:35 +0000

    Add User Name Verification

--- src/main/java/com/coway/trust/biz/organization/memberApi/impl/MemberApiServiceImpl.java
+++ src/main/java/com/coway/trust/biz/organization/memberApi/impl/MemberApiServiceImpl.java
@@ -20,41 +20,42 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
  * @Description : TO-DO Class Description
  *
  * @History
- * <pre>
+ *
+ *          <pre>
  * Date             Author          Description
  * -------------    -----------     -------------
  * 2019. 09. 09.    KR-JAEMJAEM:)   First creation
- * </pre>
+ * 2020. 04. 09.    MY-ONGHC         Add User Name Verification
+ *          </pre>
  */
 @Service("MemberApiService")
-public class MemberApiServiceImpl extends EgovAbstractServiceImpl implements MemberApiService{
+public class MemberApiServiceImpl extends EgovAbstractServiceImpl implements MemberApiService {
 
+  @Resource(name = "MemberApiMapper")
+  private MemberApiMapper memberApiMapper;
 
-
-    @Resource(name = "MemberApiMapper")
-	private MemberApiMapper memberApiMapper;
-
-
-
-	@Override
-	public List<EgovMap> selectMemberList(MemberApiForm param) throws Exception {
-		if( null == param ){
-			throw new ApplicationException(AppConstants.FAIL, "Parameter value does not exist.");
-		}
-		if( CommonUtils.isEmpty(param.getMemType()) ){
-            throw new ApplicationException(AppConstants.FAIL, "Member type value does not exist.");
-        }
-		if( CommonUtils.isEmpty(param.getSelectDivision()) ){
-		    if( CommonUtils.isEmpty(param.getSelectType()) ){
-                throw new ApplicationException(AppConstants.FAIL, "Select type value does not exist.");
-            }
-            if( CommonUtils.isEmpty(param.getSelectKeyword())){
-                throw new ApplicationException(AppConstants.FAIL, "Select keyword value does not exist.");
-            }
-            if( param.getSelectKeyword().length() < 5 ){
-                throw new ApplicationException(AppConstants.FAIL, "Please fill out at least five characters.");
-            }
-        }
-		return memberApiMapper.selectMemberList(MemberApiForm.createMap(param));
-	}
+  @Override
+  public List<EgovMap> selectMemberList(MemberApiForm param) throws Exception {
+    if (null == param) {
+      throw new ApplicationException(AppConstants.FAIL, "Parameter value does not exist.");
+    }
+    if (CommonUtils.isEmpty(param.getMemType())) {
+      throw new ApplicationException(AppConstants.FAIL, "Member type value does not exist.");
+    }
+    if (CommonUtils.isEmpty(param.getSelectDivision())) {
+      if (CommonUtils.isEmpty(param.getSelectType())) {
+        throw new ApplicationException(AppConstants.FAIL, "Select type value does not exist.");
+      }
+      if (CommonUtils.isEmpty(param.getSelectKeyword())) {
+        throw new ApplicationException(AppConstants.FAIL, "Select keyword value does not exist.");
+      }
+      if (CommonUtils.isEmpty(param.getUserName())) {
+        throw new ApplicationException(AppConstants.FAIL, "User Name value does not exist.");
+      }
+      if (param.getSelectKeyword().length() < 5) {
+        throw new ApplicationException(AppConstants.FAIL, "Please fill out at least five characters.");
+      }
+    }
+    return memberApiMapper.selectMemberList(MemberApiForm.createMap(param));
+  }
 }
