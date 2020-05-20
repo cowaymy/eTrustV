00359ec785818783cfd8b478aad639439516d8f8
etc07 <vacant@coway.com.my>
2020-05-20 07:49:10 +0000

    Add selectProductCodeList

--- src/main/java/com/coway/trust/biz/homecare/sales/order/HcOrderListService.java
+++ src/main/java/com/coway/trust/biz/homecare/sales/order/HcOrderListService.java
@@ -10,39 +10,46 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
  * @Description : TO-DO Class Description
  *
  * @History
- * <pre>
+ *
+ *          <pre>
  * Date            Author       Description
  * -------------  -----------  -------------
  * 2019. 10. 18.   KR-SH        First creation
- * </pre>
+ * 2020. 05. 20.   KR-ONGHC  Add selectProductCodeList
+ *          </pre>
  */
 public interface HcOrderListService {
 
-	/**
-	 * Search Homacare OrderList
-	 * @Author KR-SH
-	 * @Date 2019. 10. 24.
-	 * @param params
-	 * @return
-	 */
-	public List<EgovMap> selectHcOrderList(Map<String, Object> params);
+  /**
+   * Search Homacare OrderList
+   *
+   * @Author KR-SH
+   * @Date 2019. 10. 24.
+   * @param params
+   * @return
+   */
+  public List<EgovMap> selectHcOrderList(Map<String, Object> params);
 
-	/**
-	 * Search Homacare OrderInfo
-	 * @Author KR-SH
-	 * @Date 2019. 10. 24.
-	 * @param params
-	 * @return
-	 */
-	public EgovMap selectHcOrderInfo(Map<String, Object> params);
+  /**
+   * Search Homacare OrderInfo
+   *
+   * @Author KR-SH
+   * @Date 2019. 10. 24.
+   * @param params
+   * @return
+   */
+  public EgovMap selectHcOrderInfo(Map<String, Object> params);
 
-	/**
-	 * select Product Info
-	 * @Author KR-SH
-	 * @Date 2020. 1. 14.
-	 * @param salesOrdId
-	 * @return
-	 */
-	public EgovMap selectProductInfo(String salesOrdId);
+  /**
+   * select Product Info
+   *
+   * @Author KR-SH
+   * @Date 2020. 1. 14.
+   * @param salesOrdId
+   * @return
+   */
+  public EgovMap selectProductInfo(String salesOrdId);
+
+  public List<EgovMap> selectProductCodeList();
 
 }
