7e22639e3fccb281ccf1babd508503f2312dc093
etc07 <vacant@coway.com.my>
2020-05-20 07:49:56 +0000

    Add selectProductCodeList

--- src/main/java/com/coway/trust/biz/homecare/sales/order/impl/HcOrderListServiceImpl.java
+++ src/main/java/com/coway/trust/biz/homecare/sales/order/impl/HcOrderListServiceImpl.java
@@ -17,55 +17,64 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
  * @Description : TO-DO Class Description
  *
  * @History
- * <pre>
+ *
+ *          <pre>
  * Date            Author       Description
  * -------------  -----------  -------------
  * 2019. 10. 18.   KR-SH        First creation
- * </pre>
+ * 2020. 05. 20.   KR-ONGHC  Add selectProductCodeList
+ *          </pre>
  */
 @Service("hcOrderListService")
 public class HcOrderListServiceImpl extends EgovAbstractServiceImpl implements HcOrderListService {
 
-  	@Resource(name = "hcOrderListMapper")
-  	private HcOrderListMapper hcOrderListMapper;
+  @Resource(name = "hcOrderListMapper")
+  private HcOrderListMapper hcOrderListMapper;
 
-    /**
-     * Search Homacare OrderList
-     * @Author KR-SH
-     * @Date 2019. 10. 24.
-     * @param params
-     * @return
-     * @see com.coway.trust.biz.homecare.sales.order.HcOrderListService#selectHcOrderList(java.util.Map)
-     */
-	@Override
-	public List<EgovMap> selectHcOrderList(Map<String, Object> params) {
-		return hcOrderListMapper.selectHcOrderList(params);
-	}
+  /**
+   * Search Homacare OrderList
+   *
+   * @Author KR-SH
+   * @Date 2019. 10. 24.
+   * @param params
+   * @return
+   * @see com.coway.trust.biz.homecare.sales.order.HcOrderListService#selectHcOrderList(java.util.Map)
+   */
+  @Override
+  public List<EgovMap> selectHcOrderList(Map<String, Object> params) {
+    return hcOrderListMapper.selectHcOrderList(params);
+  }
 
-	/**
-	 * Search Homacare OrderInfo
-	 * @Author KR-SH
-	 * @Date 2019. 10. 25.
-	 * @param params
-	 * @return
-	 * @see com.coway.trust.biz.homecare.sales.order.HcOrderListService#selectHcOrderInfo(java.util.Map)
-	 */
-	@Override
-	public EgovMap selectHcOrderInfo(Map<String, Object> params) {
-		return hcOrderListMapper.selectHcOrderInfo(params);
-	}
+  /**
+   * Search Homacare OrderInfo
+   *
+   * @Author KR-SH
+   * @Date 2019. 10. 25.
+   * @param params
+   * @return
+   * @see com.coway.trust.biz.homecare.sales.order.HcOrderListService#selectHcOrderInfo(java.util.Map)
+   */
+  @Override
+  public EgovMap selectHcOrderInfo(Map<String, Object> params) {
+    return hcOrderListMapper.selectHcOrderInfo(params);
+  }
 
-	/**
-	 * select Product Info
-	 * @Author KR-SH
-	 * @Date 2020. 1. 14.
-	 * @param salesOrdId
-	 * @return
-	 * @see com.coway.trust.biz.homecare.sales.order.HcOrderListService#selectProductInfo(java.lang.String)
-	 */
-	@Override
-	public EgovMap selectProductInfo(String salesOrdId) {
-		return hcOrderListMapper.selectProductInfo(salesOrdId);
-	}
+  /**
+   * select Product Info
+   *
+   * @Author KR-SH
+   * @Date 2020. 1. 14.
+   * @param salesOrdId
+   * @return
+   * @see com.coway.trust.biz.homecare.sales.order.HcOrderListService#selectProductInfo(java.lang.String)
+   */
+  @Override
+  public EgovMap selectProductInfo(String salesOrdId) {
+    return hcOrderListMapper.selectProductInfo(salesOrdId);
+  }
 
+  @Override
+  public List<EgovMap> selectProductCodeList() {
+    return hcOrderListMapper.selectProductCodeList();
+  }
 }
