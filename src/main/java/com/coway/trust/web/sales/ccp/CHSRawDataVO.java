067839734d3d51053ce601b2092b53d622b5aa31
Tommy, Thun Ping Yi <pingyi.thun@coway.com.my>
2020-06-16 04:18:34 +0000

    Amend VO to CSV Record - TPY

--- src/main/java/com/coway/trust/web/sales/ccp/CHSRawDataVO.java
+++ src/main/java/com/coway/trust/web/sales/ccp/CHSRawDataVO.java
@@ -1,8 +1,10 @@
 package com.coway.trust.web.sales.ccp;
 
-import static com.coway.trust.config.excel.ExcelReadComponent.getValue;
+/*import static com.coway.trust.config.excel.ExcelReadComponent.getValue;*/
+import org.apache.commons.csv.CSVRecord;
 
 import org.apache.poi.ss.usermodel.Row;
+
 
 public class CHSRawDataVO {
 
@@ -13,30 +15,16 @@ public class CHSRawDataVO {
   private String chsStatus;
   private String chsRsn;
 
+  public static CHSRawDataVO create(CSVRecord CSVRecord) {
+    CHSRawDataVO vo = new CHSRawDataVO();
+    vo.setCustId(CSVRecord.get(1));
+    vo.setMonth(CSVRecord.get(2));
+    vo.setYear(CSVRecord.get(3));
+    vo.setChsStatus(CSVRecord.get(4));
+    vo.setChsRsn(CSVRecord.get(5));
 
-
-  public static CHSRawDataVO create(Row row) {
-
-      CHSRawDataVO vo = new CHSRawDataVO();
-      vo.setCustId(getValue(row.getCell(1)));
-      vo.setMonth(getValue(row.getCell(2)));
-      vo.setYear(getValue(row.getCell(3)));
-      vo.setChsStatus(getValue(row.getCell(4)));
-      vo.setChsRsn(getValue(row.getCell(5)));
-
-      return vo;
-  }
-
-
-  @Override
-  public String toString() {
-      return "CHSRawDataVO [custId=" + custId + ", month =" + month
-              + ", year = " + year +  ",  chsStatus = " + chsStatus + ", chsRsn =" + chsRsn + "]";
-  }
-
-
-
-
+    return vo;
+}
 
   public String getCustId() {
       return custId;
