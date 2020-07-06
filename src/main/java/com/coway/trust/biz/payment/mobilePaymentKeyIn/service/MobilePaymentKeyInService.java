0e034b75ea692e1b1577e3b0ee050960eba77fd8
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2020-07-06 07:54:42 +0000

    Add selectMemDetails

--- src/main/java/com/coway/trust/biz/payment/mobilePaymentKeyIn/service/MobilePaymentKeyInService.java
+++ src/main/java/com/coway/trust/biz/payment/mobilePaymentKeyIn/service/MobilePaymentKeyInService.java
@@ -3,6 +3,9 @@ package com.coway.trust.biz.payment.mobilePaymentKeyIn.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import com.coway.trust.cmmn.model.SessionVO;
+
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 /**
@@ -10,50 +13,59 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
  * @Description : MobilePaymentKeyInService
  *
  * @History
- * <pre>
+ *
+ *          <pre>
  * Date            Author       Description
  * -------------  -----------  -------------
  * 2019. 10. 21.   KR-HAN        First creation
- * </pre>
+ * 2020. 07. 06.   ONGHC         Add selectMemDetails
+ *          </pre>
  */
 public interface MobilePaymentKeyInService {
 
-		 /**
-		 * selectMobilePaymentKeyInList
-		 * @Author KR-HAN
-		 * @Date 2019. 11. 21.
-		 * @param params
-		 * @return
-		 */
-		List<EgovMap> selectMobilePaymentKeyInList(Map<String, Object> params);
+  /**
+   * selectMobilePaymentKeyInList
+   *
+   * @Author KR-HAN
+   * @Date 2019. 11. 21.
+   * @param params
+   * @return
+   */
+  List<EgovMap> selectMobilePaymentKeyInList(Map<String, Object> params);
 
-		 /**
-		 * saveMobilePaymentKeyInReject
-		 * @Author KR-HAN
-		 * @Date 2019. 11. 21.
-		 * @param params
-		 * @return
-		 * @throws Exception
-		 */
-		int saveMobilePaymentKeyInReject(Map<String, Object> params) throws Exception;
+  /**
+   * saveMobilePaymentKeyInReject
+   *
+   * @Author KR-HAN
+   * @Date 2019. 11. 21.
+   * @param params
+   * @return
+   * @throws Exception
+   */
+  int saveMobilePaymentKeyInReject(Map<String, Object> params) throws Exception;
 
-	     /**
-	     * saveMobilePaymentKeyInCard
-	     * @Author KR-HAN
-	     * @Date 2019. 11. 21.
-	     * @param params
-	     * @param userId
-	     * @return
-	     */
-	    List<EgovMap> saveMobilePaymentKeyInCard(Map<String, Object> params, String userId );
+  /**
+   * saveMobilePaymentKeyInCard
+   *
+   * @Author KR-HAN
+   * @Date 2019. 11. 21.
+   * @param params
+   * @param userId
+   * @return
+   */
+  List<EgovMap> saveMobilePaymentKeyInCard(Map<String, Object> params, String userId);
 
-	     /**
-	     * saveMobilePaymentKeyInNormalPayment
-	     * @Author KR-HAN
-	     * @Date 2019. 11. 21.
-	     * @param params
-	     * @param userId
-	     * @return
-	     */
-	    Map<String, Object> saveMobilePaymentKeyInNormalPayment(Map<String, Object> params, String userId );
+  /**
+   * saveMobilePaymentKeyInNormalPayment
+   *
+   * @Author KR-HAN
+   * @Date 2019. 11. 21.
+   * @param params
+   * @param userId
+   * @return
+   */
+  Map<String, Object> saveMobilePaymentKeyInNormalPayment(Map<String, Object> params, String userId);
+
+  EgovMap selectMemDetails(SessionVO sessionVO);
+
 }
