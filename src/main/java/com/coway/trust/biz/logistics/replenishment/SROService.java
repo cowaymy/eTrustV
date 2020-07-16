55a88fcbeaa98c7cf22d7485ac08f1bf8f9d1a8f
Leo <leo.ham@coway.com.my>
2020-07-16 05:17:05 +0000

    leo  sro

---
+++ src/main/java/com/coway/trust/biz/logistics/replenishment/SROService.java
@@ -0,0 +1,28 @@
+/**
+ * @author LEO
+ */
+package com.coway.trust.biz.logistics.replenishment;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.coway.trust.cmmn.model.SessionVO;
+
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+public interface SROService {
+
+	List<EgovMap> sroItemMgntList(Map<String, Object> params);
+
+	void  saveSroItemMgnt(Map<String, ArrayList<Object>> params, SessionVO sessionVO);
+
+	List<EgovMap> sroMgmtList(Map<String, Object> params);
+
+	List<EgovMap> sroMgmtDetailList(Map<String, Object> params);
+
+
+	String  saveSroMgmt(Map<String,Object > params, SessionVO sessionVO);
+
+
+}
