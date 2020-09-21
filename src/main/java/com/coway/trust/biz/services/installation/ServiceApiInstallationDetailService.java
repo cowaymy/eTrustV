5f3a61d485d4c6df47249e5d51d4bf02113db757
Leo <leo.ham@coway.com.my>
2020-09-21 03:57:30 +0000

    -Exchange product missing when using mobile

--- src/main/java/com/coway/trust/biz/services/installation/ServiceApiInstallationDetailService.java
+++ src/main/java/com/coway/trust/biz/services/installation/ServiceApiInstallationDetailService.java
@@ -11,15 +11,13 @@ import com.coway.trust.api.mobile.services.installation.InstallationResultDto;
  * @ClassName : ServiceApiInstallationDetailService.java
  * @Description : Mobile Installation Data Save
  *
- * @History
- * Date              Author         Description
- * -------------  -----------  -------------
- * 2019. 09. 17.   Jun             First creation
+ * @History Date Author Description ------------- ----------- ------------- 2019. 09. 17. Jun First creation
  */
 public interface ServiceApiInstallationDetailService {
-	ResponseEntity<InstallationResultDto> installationResultProc(Map<String, Object> insApiresult) throws Exception;
-
-	ResponseEntity<InstallFailJobRequestDto> installFailJobRequestProc(Map<String, Object> params) throws Exception;
-
-	ResponseEntity<InstallationResultDto> installationDtResultProc(Map<String, Object> insApiresult) throws Exception;
+  ResponseEntity<InstallationResultDto> installationResultProc(Map<String, Object> insApiresult) throws Exception;
+  
+  ResponseEntity<InstallFailJobRequestDto> installFailJobRequestProc(Map<String, Object> params) throws Exception;
+  
+  ResponseEntity<InstallationResultDto> installationDtResultProc(Map<String, Object> insApiresult) throws Exception;
+  
 }
