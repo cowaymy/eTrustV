6b581b600af701613b037826f9d787c40df67052
Chang Keat Voon <keatvoon.chang@coway.com.my>
2020-10-26 04:52:43 +0000

    trying solve mobile server - mobile server can't restart back

--- src/main/java/com/coway/trust/biz/sales/order/OrderReqApplication.java
+++ src/main/java/com/coway/trust/biz/sales/order/OrderReqApplication.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import com.coway.trust.biz.common.FileVO;
 import com.coway.trust.biz.common.type.FileType;
 
-public interface OrderReqApplication {
+public interface OrderReqApplication { 
   void insertOrderCancelAttachBiz(List<FileVO> list, FileType type, Map<String, Object> params, List<String> seqs);
 
   void updateOrderCancelAttachBiz(List<FileVO> list, FileType type, Map<String, Object> params,List<String> seqs);
