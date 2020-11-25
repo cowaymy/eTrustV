9001a797802ff8b7e892c9d94f98d1da2d186526
Alex Lau Yik Fei <alex.lau@coway.com.my>
2020-11-25 15:06:23 +0000

    add get file ID and booster used for Fail installation edit

--- src/main/java/com/coway/trust/biz/services/installation/InstallationResultListService.java
+++ src/main/java/com/coway/trust/biz/services/installation/InstallationResultListService.java
@@ -15,123 +15,129 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
  *********************************************************************************************/
 
 public interface InstallationResultListService {
-  
+
   List<EgovMap> selectInstallationType();
-  
+
   List<EgovMap> selectApplicationType();
-  
+
   List<EgovMap> selectInstallStatus();
-  
+
   List<EgovMap> adapterUsed();
-  
+
+  List<EgovMap> boosterUsed();
+
   List<EgovMap> instChkLst();
-  
+
   List<EgovMap> failParent();
-  
+
   List<EgovMap> selectDscCode();
-  
+
   List<EgovMap> installationResultList(Map<String, Object> params);
-  
+
   EgovMap getInstallResultByInstallEntryID(Map<String, Object> params);
-  
+
+  EgovMap getFileID(Map<String, Object> params); ///FileXXX
+
   EgovMap getOrderInfo(Map<String, Object> params);
-  
+
   // EgovMap getcustomerInfo(Object cust_id);
-  
+
   EgovMap getcustomerInfo(Map<String, Object> params);
-  
+
   EgovMap getCustomerAddressInfo(Map<String, Object> params);
-  
+
   EgovMap getCustomerContractInfo(Map<String, Object> params);
-  
+
   EgovMap getInstallationBySalesOrderID(Map<String, Object> params);
-  
+
   EgovMap getInstallContactByContactID(Map<String, Object> params);
-  
+
   EgovMap getSalesOrderMBySalesOrderID(Map<String, Object> params);
-  
+
   EgovMap getMemberFullDetailsByMemberIDCode(Map<String, Object> params);
-  
+
   List<EgovMap> selectViewInstallation(Map<String, Object> params);
-  
+
   EgovMap selectCallType(Map<String, Object> params);
-  
+
   EgovMap getOrderExchangeTypeByInstallEntryID(Map<String, Object> params);
-  
+
   List<EgovMap> selectFailReason(Map<String, Object> params);
-  
+
   EgovMap getStockInCTIDByInstallEntryIDForInstallationView(Map<String, Object> params);
-  
+
   EgovMap getSirimLocByInstallEntryID(Map<String, Object> params);
-  
+
   List<EgovMap> checkCurrentPromoIsSwapPromoIDByPromoID(int promotionId);
-  
+
   List<EgovMap> selectSalesPromoMs(int promotionId);
-  
+
   // EgovMap getPromoPriceAndPV(int promotionId, int productId);
-  
+
   EgovMap getAssignPromoIDByCurrentPromoIDAndProductID(int promotionId, int productId, boolean flag);
-  
+
   EgovMap selectViewDetail(Map<String, Object> params);
-  
+
   boolean insertInstallationProductExchange(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
-  
+
   Map<String, Object> insertInstallationResult(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
-  
+
   Map<String, Object> insertInstallationResult_2(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
-  
+
   Map<String, Object> runInstSp(Map<String, Object> params, SessionVO sessionVO, String no) throws ParseException;
-  
+
   Map<String, Object> updateAssignCT(Map<String, Object> params);
-  
+
   List<EgovMap> assignCtOrderList(Map<String, Object> params);
-  
+
   List<EgovMap> assignCtList(Map<String, Object> params);
-  
+
   List<EgovMap> selectInstallationNoteListing(Map<String, Object> params) throws ParseException;
-  
+
   EgovMap selectInstallInfo(Map<String, Object> params);
-  
+
   int editInstallationResult(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
-  
+
+  int updateInstallFileKey(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
+
   int failInstallationResult(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
 
   int isInstallAlreadyResult(Map<String, Object> params);
-  
+
   int insResultSync(Map<String, Object> params);
-  
+
   EgovMap validationInstallationResult(Map<String, Object> params);
-  
+
   EgovMap getLocInfo(Map<String, Object> params);
-  
+
   EgovMap getInstallationResultInfo(Map<String, Object> params);
-  
+
   List<EgovMap> viewInstallationResult(Map<String, Object> params);
-  
+
   EgovMap checkMonthInstallDate(Map<String, Object> params);
-  
+
   List<EgovMap> getProductList(Map<String, Object> params);
-  
+
   int chkRcdTms(Map<String, Object> params);
-  
+
   int selRcdTms(Map<String, Object> params);
-  
+
   String getSalStat(Map<String, Object> params);
-  
+
   int chkExgRsnCde(Map<String, Object> params);
-  
+
   List<EgovMap> selectCtSerialNoList(Map<String, Object> params);
-  
+
   List<EgovMap> selectFailChild(Map<String, Object> params);
-  
+
   ReturnMessage insertInstallationResultSerial(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
-  
+
   int editInstallationResultSerial(Map<String, Object> params, SessionVO sessionVO) throws ParseException;
-  
+
   Map<String, Object> updateAssignCTSerial(Map<String, Object> params);
-  
+
   List<EgovMap> waterEnvironmentList(Map<String, Object> params);
-  
+
   List<EgovMap> getProductListwithCategory(Map<String, Object> params);
-  
+
 }
