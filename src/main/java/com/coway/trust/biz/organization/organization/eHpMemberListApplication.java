56e03c21a9153a66237c7afc787652421cd09b6a
Chang Keat Voon <keatvoon.chang@coway.com.my>
2021-05-10 10:32:56 +0000

    redeploy since mobile server cant start back

--- src/main/java/com/coway/trust/biz/organization/organization/eHpMemberListApplication.java
+++ src/main/java/com/coway/trust/biz/organization/organization/eHpMemberListApplication.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import com.coway.trust.biz.common.FileVO;
 import com.coway.trust.biz.common.type.FileType;
 
-
+//
 public interface eHpMemberListApplication {
 
   void insertEHPAttachBiz(List<FileVO> list, FileType type, Map<String, Object> params, List<String> seqs);
