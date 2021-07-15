e58a55962967559f51511b85dc9510e5def3c829
Chang Keat Voon <keatvoon.chang@coway.com.my>
2021-07-15 10:44:20 +0000

    redeploy - mobile server can't capture

--- src/main/java/com/coway/trust/biz/sales/ownershipTransfer/impl/OwnershipTransferApplicationImpl.java
+++ src/main/java/com/coway/trust/biz/sales/ownershipTransfer/impl/OwnershipTransferApplicationImpl.java
@@ -18,63 +18,65 @@ import com.coway.trust.biz.common.type.FileType;
 import com.coway.trust.biz.sales.ownershipTransfer.OwnershipTransferApplication;
 
 @Service("ownershipTransferApplication")
-public class OwnershipTransferApplicationImpl implements OwnershipTransferApplication{
+public class OwnershipTransferApplicationImpl implements OwnershipTransferApplication {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FileApplicationImpl.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileApplicationImpl.class);
 
-    @Autowired
-    private FileService fileService;
+	@Autowired
+	private FileService fileService;
 
-    @Autowired
-    private FileMapper fileMapper;
+	@Autowired
+	private FileMapper fileMapper;
 
-    @Override
-    public void insertOwnershipTransferAttach(List<FileVO> list, FileType type, Map<String, Object> params) {
-        LOGGER.debug("ownershipTransferApplication :: insertOwnershipTransferAttach");
+	@Override
+	public void insertOwnershipTransferAttach(List<FileVO> list, FileType type, Map<String, Object> params) {
+		LOGGER.debug("ownershipTransferApplication :: insertOwnershipTransferAttach");
 
-        int fileGroupKey = fileService.insertFiles(list, type, (Integer) params.get("userId"));
-        params.put("fileGroupKey", fileGroupKey);
-    }
+		int fileGroupKey = fileService.insertFiles(list, type, (Integer) params.get("userId"));
+		params.put("fileGroupKey", fileGroupKey);
+	}
 
-    @Override
-    public void updateOwnershipTransferAttach(List<FileVO> list, FileType type, Map<String, Object> params) {
-        LOGGER.debug("ownershipTransferApplication :: updateOwnershipTransferAttach");
+	@Override
+	public void updateOwnershipTransferAttach(List<FileVO> list, FileType type, Map<String, Object> params) {
+		LOGGER.debug("ownershipTransferApplication :: updateOwnershipTransferAttach");
 
-        String update = (String) params.get("update");
-        String[] updateList = null;
-        if(!StringUtils.isEmpty(update)) {
-            updateList = params.get("update").toString().split(",");
-            LOGGER.debug("updateList.length : {}", updateList.length);
-        }
+		String update = (String) params.get("update");
+		String[] updateList = null;
+		if (!StringUtils.isEmpty(update)) {
+			updateList = params.get("update").toString().split(",");
+			LOGGER.debug("updateList.length : {}", updateList.length);
+		}
 
-        if(list.size() > 0) {
-            for(int i = 0; i < list.size(); i++) {
-                if(updateList != null && i < updateList.length) {
-                    String atchFileId = updateList[i];
+		if (list.size() > 0) {
+			for (int i = 0; i < list.size(); i++) {
+				if (updateList != null && i < updateList.length) {
+					String atchFileId = updateList[i];
 
-                    LOGGER.debug("updateAttach(i) :: atchFileGrpId :: " + params.get("atchFileGrpId"));
-                    LOGGER.debug("updateAttach(i) :: atchFileId :: " + atchFileId);
-                    LOGGER.debug("updateAttach(i) :: list.get(i) :: " + list.get(i));
-                    LOGGER.debug("updateAttach(i) :: userId :: " + params.get("userId"));
+					LOGGER.debug("updateAttach(i) :: atchFileGrpId :: " + params.get("atchFileGrpId"));
+					LOGGER.debug("updateAttach(i) :: atchFileId :: " + atchFileId);
+					LOGGER.debug("updateAttach(i) :: list.get(i) :: " + list.get(i));
+					LOGGER.debug("updateAttach(i) :: userId :: " + params.get("userId"));
 
-                    fileService.changeFile(Integer.parseInt(String.valueOf(params.get("atchFileGrpId"))), Integer.parseInt(atchFileId), list.get(i), type, Integer.parseInt(String.valueOf(params.get("userId"))));
-                } else {
-                    FileGroupVO fileGroupVO = new FileGroupVO();
+					fileService.changeFile(Integer.parseInt(String.valueOf(params.get("atchFileGrpId"))),
+							Integer.parseInt(atchFileId), list.get(i), type,
+							Integer.parseInt(String.valueOf(params.get("userId"))));
+				} else {
+					FileGroupVO fileGroupVO = new FileGroupVO();
 
-                    LOGGER.debug("updateAttach(e) :: list.get(i) :: " + list.get(i));
-                    fileMapper.insertFileDetail(list.get(i));
+					LOGGER.debug("updateAttach(e) :: list.get(i) :: " + list.get(i));
+					fileMapper.insertFileDetail(list.get(i));
 
-                    fileGroupVO.setAtchFileGrpId(Integer.parseInt(params.get("atchFileGrpId").toString()));
-                    fileGroupVO.setAtchFileId(list.get(i).getAtchFileId());
-                    fileGroupVO.setChenalType(type.getCode());
-                    fileGroupVO.setCrtUserId(Integer.parseInt(String.valueOf(params.get("userId"))));
-                    fileGroupVO.setUpdUserId(Integer.parseInt(String.valueOf(params.get("userId"))));
+					fileGroupVO.setAtchFileGrpId(Integer.parseInt(params.get("atchFileGrpId").toString()));
+					fileGroupVO.setAtchFileId(list.get(i).getAtchFileId());
+					fileGroupVO.setChenalType(type.getCode());
+					fileGroupVO.setCrtUserId(Integer.parseInt(String.valueOf(params.get("userId"))));
+					fileGroupVO.setUpdUserId(Integer.parseInt(String.valueOf(params.get("userId"))));
 
-                    LOGGER.debug("updateAttach(e) :: fileGroupVO :: " + fileGroupVO);
-                    fileMapper.insertFileGroup(fileGroupVO);
-                }
-            }
-        }
-    }
+					LOGGER.debug("updateAttach(e) :: fileGroupVO :: " + fileGroupVO);
+					fileMapper.insertFileGroup(fileGroupVO);
+				}
+			}
+		}
+	}
 
 }
