ce80f30a9a7df825598d3b904a12b8aa5adf3729
Tang Hui Ling <huiling.tang@coway.com.my>
2021-10-07 07:39:30 +0000

    Coway University (LMS) API

--- src/main/java/com/coway/trust/api/callcenter/common/CommonConstants.java
+++ src/main/java/com/coway/trust/api/callcenter/common/CommonConstants.java
@@ -7,7 +7,7 @@ public class CommonConstants {
 
 	public static final String USER_ID = "userId";
 
-	public static final String lmsApiDevUrl = "https://cu.modernlms.net/";
+	public static final String lmsApiDevUrl = "http://cu.modernlms.net/";
 	//http://cu-dev.modernlms.net/modernlms-api/api/
 
 	public static final String lmsSecretKeyDev = "qwer12345qergxcv3456789cvgbhnjmk34678cvtbyunim4f5gt6yhu5f6g7h8j";
