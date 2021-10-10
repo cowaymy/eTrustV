be19582e8516eef5baf3160ff55de98fcd70ada0
Teoh Hui Ding <huiding.teoh@coway.com.my>
2021-10-10 11:58:13 +0000

    Hui Ding - LMS API pre-production deployment. temp remove API key and password at properties.

--- src/main/java/com/coway/trust/api/callcenter/common/CommonConstants.java
+++ src/main/java/com/coway/trust/api/callcenter/common/CommonConstants.java
@@ -11,5 +11,6 @@ public class CommonConstants {
 	public static final String lmsApiDevUrl = "https://api.cowayuniv.com/"; //PROD
 	//http://cu-dev.modernlms.net/modernlms-api/api/
 
-	public static final String lmsSecretKeyDev = "qwer12345qergxcv3456789cvgbhnjmk34678cvtbyunim4f5gt6yhu5f6g7h8j";
+	//public static final String lmsSecretKeyDev = "qwer12345qergxcv3456789cvgbhnjmk34678cvtbyunim4f5gt6yhu5f6g7h8j";
+	public static final String lmsSecretKeyDev = "19961A8A7F66515F1E85B2B113A01824D6AC9F19";
 }
