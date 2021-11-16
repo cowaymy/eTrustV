d82725666761f5cbfaf16b453e837d9737814abb
Celeste Chan Mei Kei <celeste.chan@coway.com.my>
2021-11-16 08:27:35 +0000

    Amend the wrong product category for eCommerce

--- src/main/java/com/coway/trust/biz/api/impl/EcommApiServiceImpl.java
+++ src/main/java/com/coway/trust/biz/api/impl/EcommApiServiceImpl.java
@@ -266,7 +266,7 @@ public class EcommApiServiceImpl extends EgovAbstractServiceImpl implements Ecom
 
         // use product code to find product category. when product category is 5706/5707, insert into HMC0110D and setBundleID in SAL0001D with HMC0110D ord_seq_no
         String prdCat = hcOrderRegisterMapper.getProductCategory(reqPrm.get("product").toString());
-        if(prdCat != null && (prdCat.equals("5706") || prdCat.equals("5607")))
+        if(prdCat != null && (prdCat.equals("5706") || prdCat.equals("5707")))
 		{
         	hcOrderRegisterService.hcRegisterOrder(orderVO, sessionVO);
             HcOrderVO hcOrderVO = orderVO.getHcOrderVO();
