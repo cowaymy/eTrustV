50a3d371f1e2b7f8a74af6e77ab87c3dbf4491bc
Alex Lau Yik Fei <alex.lau@coway.com.my>
2022-01-28 09:27:10 +0000

    Test05 SMS triggering AS

--- src/main/java/com/coway/trust/biz/services/as/impl/ASManagementListServiceImpl.java
+++ src/main/java/com/coway/trust/biz/services/as/impl/ASManagementListServiceImpl.java
@@ -4202,8 +4202,9 @@ public List<EgovMap> selectDefectEntry(Map<String, Object> params) {
 
   @Override
   public void sendSms(Map<String, Object> smsList){
-	int userId = (int) smsList.get("userId");
-    SmsVO sms = new SmsVO(userId, 975);
+	//int userId = (int) smsList.get("userId");
+    //SmsVO sms = new SmsVO(userId, 975);
+	SmsVO sms = new SmsVO(Integer.parseInt(smsList.get("userId").toString()), 975); //TEMP ADDED BY ALEX to Test SMS Function
 
     sms.setMessage(smsList.get("smsMessage").toString());
     sms.setMobiles(smsList.get("smsMobileNo").toString());
