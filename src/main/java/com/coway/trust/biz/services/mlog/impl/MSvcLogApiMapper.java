0cb93c33c76d375a7e7ea980a4a6a424ad1b0a1b
Alex Lau Yik Fei <alex.lau@coway.com.my>
2022-02-11 06:32:50 +0000

    PR - sms triggering

--- src/main/java/com/coway/trust/biz/services/mlog/impl/MSvcLogApiMapper.java
+++ src/main/java/com/coway/trust/biz/services/mlog/impl/MSvcLogApiMapper.java
@@ -180,6 +180,8 @@ public interface MSvcLogApiMapper {
 
   int updateFailed_LOG0038D(Map<String, Object> params);
 
+  EgovMap selectPRFailReason(Map<String, Object> params);
+
   int isPrdRtnAlreadyResult(Map<String, Object> params);
 
   int updFctExch(Map<String, Object> params);
