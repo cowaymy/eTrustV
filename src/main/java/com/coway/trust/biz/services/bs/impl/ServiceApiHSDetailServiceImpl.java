39ffbb8685444829c064106e0f5b29cfc1737e8e
Alex Lau Yik Fei <alex.lau@coway.com.my>
2022-03-02 03:17:14 +0000

    Swicth Filter for Neo and Harry -- SWITCH_CHK_LST

--- src/main/java/com/coway/trust/biz/services/bs/impl/ServiceApiHSDetailServiceImpl.java
+++ src/main/java/com/coway/trust/biz/services/bs/impl/ServiceApiHSDetailServiceImpl.java
@@ -149,6 +149,7 @@ public class ServiceApiHSDetailServiceImpl extends EgovAbstractServiceImpl imple
 
           params.put("hidSerialRequireChkYn", String.valueOf(insApiresult.get("serialRequireChkYn")));
           params.put("instChklstCheckBox", String.valueOf(insApiresult.get("hsChkLst")));/////////////////////////////////////////////hsChklst
+          params.put("switchChkLst", String.valueOf(insApiresult.get("switchChkLst")));
           params.put("instruction", String.valueOf(insApiresult.get("instruction")));///////////////////////////////////////////// job instruction
           params.put("codeFailRemark", String.valueOf(insApiresult.get("codeFailRemark"))); // Barcode Reason Enhancement
           params.put("voucherRedemption", String.valueOf(insApiresult.get("voucherRedemption")));
