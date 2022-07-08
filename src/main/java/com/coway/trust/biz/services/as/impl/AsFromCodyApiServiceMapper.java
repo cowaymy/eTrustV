773ba6b1363687e58b14d0ff6615632f81f6ce90
Alex Lau Yik Fei <alex.lau@coway.com.my>
2022-07-08 06:25:59 +0000

    AsFromCody - Alex

---
+++ src/main/java/com/coway/trust/biz/services/as/impl/AsFromCodyApiServiceMapper.java
@@ -0,0 +1,20 @@
+package com.coway.trust.biz.services.as.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+
+ *********************************************************************************************/
+
+@Mapper("asFromCodyApiServiceMapper")
+public interface AsFromCodyApiServiceMapper {
+
+	int insertAsFromCodyRequest(Map<String, Object> params);
+
+}
