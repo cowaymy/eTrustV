9835302c9d901c37207b565b10f6be89b73165d9
Alex Lau Yik Fei <alex.lau@coway.com.my>
2022-07-08 06:24:29 +0000

    AsFromCody

---
+++ src/main/java/com/coway/trust/biz/services/as/AsFromCodyApiService.java
@@ -0,0 +1,29 @@
+package com.coway.trust.biz.services.as;
+
+import java.util.List;
+
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 01/04/2019    ONGHC      1.0.1       - Restructure File
+ *********************************************************************************************/
+import java.util.Map;
+
+
+import com.coway.trust.api.mobile.sales.royaltyCustomerApi.RoyaltyCustomerListApiForm;
+import com.coway.trust.api.mobile.services.asFromCody.AsFromCodyForm;
+import com.coway.trust.cmmn.model.ReturnMessage;
+import com.coway.trust.cmmn.model.SessionVO;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 07/07/2022    ALEX      1.0.1
+ *********************************************************************************************/
+
+public interface AsFromCodyApiService {
+
+	int insertAsFromCodyRequest(AsFromCodyForm AsFromCodyForm) throws Exception;
+
+}
