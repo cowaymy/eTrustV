8785fc57b00bb49a708a5e7ef6fb3837b90826e9
Alex Lau Yik Fei <alex.lau@coway.com.my>
2022-07-28 03:33:01 +0000

    UnmatchAS Reason

--- src/main/java/com/coway/trust/biz/services/as/impl/ServiceApiASDetailServiceImpl.java
+++ src/main/java/com/coway/trust/biz/services/as/impl/ServiceApiASDetailServiceImpl.java
@@ -436,6 +436,7 @@ public class ServiceApiASDetailServiceImpl extends EgovAbstractServiceImpl imple
         		params.put("AS_RESULT_REM", insApiresult.get("resultRemark"));
                 params.put("AS_PSI", insApiresult.get("psiRcd"));
                 params.put("AS_LPM", insApiresult.get("lpmRcd"));
+                params.put("AS_UNMATCH_REASON", insApiresult.get("asUnmatchReason"));
 
         		if (insApiresult.get("inHouseRepairRemark") != null) {
         			params.put("IN_HUSE_REPAIR_REM", String.valueOf(insApiresult.get("inHouseRepairRemark")));
