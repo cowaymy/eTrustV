b2eb19c94fd31641fb9bd499e17068aba4d55b91
etc22 <vacant@coway.com.my>
2022-08-18 05:58:17 +0000

    Kim Ching - Mobile Care Service Dshboard View

--- src/main/java/com/coway/trust/api/mobile/services/serviceDashboardApi/ServiceDashboardApiForm.java
+++ src/main/java/com/coway/trust/api/mobile/services/serviceDashboardApi/ServiceDashboardApiForm.java
@@ -22,32 +22,18 @@ import io.swagger.annotations.ApiModel;
 @ApiModel(value = "ServiceDashboardApiForm", description = "ServiceDashboardApiForm")
 public class ServiceDashboardApiForm {
 
-
+	private String regId;
 
     public static ServiceDashboardApiForm create(Map<String, Object> customerMap) {
         return BeanConverter.toBean(customerMap, ServiceDashboardApiForm.class);
     }
 
-
 	public static Map<String, Object> createMap(ServiceDashboardApiForm vo){
 		Map<String, Object> params = new HashMap<>();
-		params.put("memId", vo.getMemId());
 		params.put("regId", vo.getRegId());
 		return params;
 	}
 
-
-
-	private int memId;
-	private String regId;
-
-    public int getMemId() {
-        return memId;
-    }
-
-    public void setMemId(int memId) {
-        this.memId = memId;
-    }
 
     public String getRegId() {
         return regId;
