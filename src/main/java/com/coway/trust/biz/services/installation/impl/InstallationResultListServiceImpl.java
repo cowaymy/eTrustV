6f8e6bed0cc740c935bf7a254c5c97166341ec29
etc21 <vacant@coway.com.my>
2022-09-21 01:31:46 +0000

    inst notes

--- src/main/java/com/coway/trust/biz/services/installation/impl/InstallationResultListServiceImpl.java
+++ src/main/java/com/coway/trust/biz/services/installation/impl/InstallationResultListServiceImpl.java
@@ -3886,7 +3886,7 @@ private boolean insertInstallation(int statusId, String ApptypeID, Map<String, O
 
 		params.put(REPORT_FILE_NAME, "/services/InstallationNoteDigitalization.rpt");// visualcut
 	    params.put(REPORT_VIEW_TYPE, "MAIL_PDF"); // viewType
-	    params.put("V_WHERESQL", " AND Extent1.INSTALL_ENTRY_ID = " + params.get("installEntryId").toString());// parameter
+	    params.put("V_WHERESQL", params.get("installEntryId").toString());// parameter
 	    params.put(AppConstants.REPORT_DOWN_FILE_NAME, "InstallationNotes_" + CommonUtils.getNowDate());
 
 	    String emailSubject = "COWAY: Congratulation For New Coway Product";
