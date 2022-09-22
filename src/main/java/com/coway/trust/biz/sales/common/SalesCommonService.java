48d1558d7c0453220769c9437f2d17a1b88c6b90
etc31 <vacant@coway.com.my>
2022-09-22 13:43:07 +0000

    20220922 - Nicholas Ong - prevent SO and HP view CD, HT sales

--- src/main/java/com/coway/trust/biz/sales/common/SalesCommonService.java
+++ src/main/java/com/coway/trust/biz/sales/common/SalesCommonService.java
@@ -7,6 +7,7 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 public interface SalesCommonService {
 
 	EgovMap getUserInfo(Map<String, Object> params);
-	
-	
+
+	EgovMap getUserBranchType(Map<String, Object> params);
+
 }
