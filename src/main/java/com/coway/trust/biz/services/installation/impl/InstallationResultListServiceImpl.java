2ece3927b549d4e7b6a5d4bc1ae2ad613d3bce2c
etc21 <vacant@coway.com.my>
2022-09-27 09:29:19 +0000

    ins note

--- src/main/java/com/coway/trust/biz/services/installation/impl/InstallationResultListServiceImpl.java
+++ src/main/java/com/coway/trust/biz/services/installation/impl/InstallationResultListServiceImpl.java
@@ -3954,7 +3954,7 @@ private boolean insertInstallation(int statusId, String ApptypeID, Map<String, O
 		logger.debug("params1111 : {}", params.toString());
 		logger.debug("installEntryId1111 : {}", params.get("installEntryId").toString());
 
-		params.put(REPORT_FILE_NAME, "/services/InstallationNoteDigitalization.rpt");// visualcut
+		params.put(REPORT_FILE_NAME, "/services/InstallationNoteDigitalization_Mobile.rpt");// visualcut
 	    params.put(REPORT_VIEW_TYPE, "MAIL_PDF"); // viewType
 	    params.put("V_WHERE", params.get("installEntryId").toString());// parameter
 	    params.put(AppConstants.REPORT_DOWN_FILE_NAME,  "InstallationNoteDigitalization_" + CommonUtils.getNowDate() + ".pdf");
