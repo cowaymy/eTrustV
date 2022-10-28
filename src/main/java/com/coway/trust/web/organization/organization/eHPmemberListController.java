b829f17d6db04827d39a665d6e245fef17feaafa
Boi Poh Leng <pohleng.boi@coway.com.my>
2022-10-28 01:51:52 +0000

    Open back member rejoin module

--- src/main/java/com/coway/trust/web/organization/organization/eHPmemberListController.java
+++ src/main/java/com/coway/trust/web/organization/organization/eHPmemberListController.java
@@ -439,13 +439,12 @@ public class eHPmemberListController {
        String memCode = "";
        ReturnMessage message = new ReturnMessage();
 
-//       //Check whether member is rejoin or not
-//       Boolean isRejoinMem = Boolean.parseBoolean(params.get("isRejoinMem").toString());
-//       String rejoinMemId = params.get("memId").toString();
+       //Check whether member is rejoin or not
+       Boolean isRejoinMem = Boolean.parseBoolean(params.get("isRejoinMem").toString());
+       String rejoinMemId = params.get("memId").toString();
 
        // To check email address uniqueness - LMS could only receive unique email address. Hui Ding, 2021-10-20
-      // if (params.get("eHPemail") != null && !isRejoinMem && (rejoinMemId.equals(null) || rejoinMemId.equals(""))){
-       if (params.get("eHPemail") != null){
+       if (params.get("eHPemail") != null && !isRejoinMem && (rejoinMemId.equals(null) || rejoinMemId.equals(""))){
     	   List<EgovMap> HpExist = eHPmemberListService.selectHpApplByEmail(params);
     	   if(HpExist.size() > 0){
     		   message.setMessage("Email has been used");
@@ -466,11 +465,11 @@ public class eHPmemberListController {
         		   + "<br><br>Kindly Proceed to complete eHP application within 7 days from application date to avoid auto cancel <br><br>Thank You");
        }
 
-//   		//If is rejoin member
-//		if(isRejoinMem && !(rejoinMemId.equals(null) || rejoinMemId.equals(""))){
-//			params.put("memId", params.get("memId").toString());
-//			memberListService.updateMemberStatus(params);
-//		}
+   		//If is rejoin member
+		if(isRejoinMem && !(rejoinMemId.equals(null) || rejoinMemId.equals(""))){
+			params.put("memId", params.get("memId").toString());
+			memberListService.updateMemberStatus(params);
+		}
 
        logger.debug("message : {}", message);
 
