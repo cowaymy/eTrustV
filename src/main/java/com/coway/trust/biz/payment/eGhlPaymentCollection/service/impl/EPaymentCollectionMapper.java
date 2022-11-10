f299bfe45a1a98162dfbf12aab9bbb738612305f
Frango Liew Yao Jun <frango.liew@coway.com.my>
2022-11-10 07:13:44 +0000

    test commit

--- src/main/java/com/coway/trust/biz/payment/eGhlPaymentCollection/service/impl/EPaymentCollectionMapper.java
+++
@@ -1,8 +0,0 @@
-package com.coway.trust.biz.payment.ePaymentCollection.service.impl;
-
-import egovframework.rte.psl.dataaccess.mapper.Mapper;
-
-@Mapper("ePaymentCollectionMapper")
-public interface EPaymentCollectionMapper {
-
-}
