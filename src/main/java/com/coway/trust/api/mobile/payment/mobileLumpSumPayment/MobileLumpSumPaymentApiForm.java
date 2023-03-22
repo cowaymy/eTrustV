25587329a072656664b2e1478d2014af09f9da3f
Frango Liew Yao Jun <frango.liew@coway.com.my>
2023-03-22 02:45:29 +0000

    Mobile Lump Sum Development

--- src/main/java/com/coway/trust/api/mobile/payment/mobileLumpSumPayment/MobileLumpSumPaymentApiForm.java
+++ src/main/java/com/coway/trust/api/mobile/payment/mobileLumpSumPayment/MobileLumpSumPaymentApiForm.java
@@ -1,7 +1,10 @@
 package com.coway.trust.api.mobile.payment.mobileLumpSumPayment;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.codec.binary.Base64;
 
 import io.swagger.annotations.ApiModel;
 
@@ -15,6 +18,18 @@ public class MobileLumpSumPaymentApiForm {
 	//Order Search
 	private int custId;
 
+	//Submit Data
+	private String userName;
+	private String signData;
+	private String sms1;
+    private String sms2;
+    private String email1;
+    private String email2;
+    private String paymentMethod;
+    private Double totalOriginalOutstandingAmount;
+    private Double totalPayableAmount;
+    private List<MobileLumpSumPaymentOrderDetailsForm> orderDetailList;
+
 	public static Map<String, Object> createMap(MobileLumpSumPaymentApiForm vo){
     	Map<String, Object> params = new HashMap<>();
     	//Customer Search
@@ -24,6 +39,23 @@ public class MobileLumpSumPaymentApiForm {
     	//Order Search
     	params.put("custId", vo.getCustId());
     	params.put("ordNoList", vo.getOrdNoList());
+
+    	//Submission Save
+    	params.put("userName", vo.getUserName());
+    	if(vo.getSignData() != null && vo.getSignData().length() > 0){
+			params.put("signData", Base64.decodeBase64(vo.getSignData()));
+		}
+		else{
+			params.put("signData", vo.getSignData());
+		}
+		params.put("sms1", vo.getSms1());
+		params.put("sms2", vo.getSms2());
+		params.put("email1", vo.getEmail1());
+		params.put("email2", vo.getEmail2());
+		params.put("paymentMethod", vo.getPaymentMethod());
+		params.put("totalOriginalOutstandingAmount", vo.getTotalOriginalOutstandingAmount());
+		params.put("totalPayableAmount", vo.getTotalPayableAmount());
+		params.put("orderDetailList", vo.getOrderDetailList());
 
     	return params;
 	}
@@ -54,4 +86,85 @@ public class MobileLumpSumPaymentApiForm {
 	public void setOrdNoList(String ordNoList) {
 		this.ordNoList = ordNoList;
 	}
+
+	public String getSignData() {
+		return signData;
+	}
+
+	public void setSignData(String signData) {
+		this.signData = signData;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getSms1() {
+		return sms1;
+	}
+
+	public void setSms1(String sms1) {
+		this.sms1 = sms1;
+	}
+
+	public String getSms2() {
+		return sms2;
+	}
+
+	public void setSms2(String sms2) {
+		this.sms2 = sms2;
+	}
+
+	public String getEmail1() {
+		return email1;
+	}
+
+	public void setEmail1(String email1) {
+		this.email1 = email1;
+	}
+
+	public String getEmail2() {
+		return email2;
+	}
+
+	public void setEmail2(String email2) {
+		this.email2 = email2;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public Double getTotalOriginalOutstandingAmount() {
+		return totalOriginalOutstandingAmount;
+	}
+
+	public void setTotalOriginalOutstandingAmount(Double totalOriginalOutstandingAmount) {
+		this.totalOriginalOutstandingAmount = totalOriginalOutstandingAmount;
+	}
+
+	public Double getTotalPayableAmount() {
+		return totalPayableAmount;
+	}
+
+	public void setTotalPayableAmount(Double totalPayableAmount) {
+		this.totalPayableAmount = totalPayableAmount;
+	}
+
+	public List<MobileLumpSumPaymentOrderDetailsForm> getOrderDetailList() {
+		return orderDetailList;
+	}
+
+	public void setOrderDetailList(List<MobileLumpSumPaymentOrderDetailsForm> orderDetailList) {
+		this.orderDetailList = orderDetailList;
+	}
+
 }
---
+++ src/main/java/com/coway/trust/api/mobile/payment/mobileLumpSumPayment/MobileLumpSumPaymentOrderDetailsForm.java
@@ -0,0 +1,14 @@
+package com.coway.trust.api.mobile.payment.mobileLumpSumPayment;
+
+public class MobileLumpSumPaymentOrderDetailsForm {
+	public int ordId;
+	public String ordNo;
+	public int ordPaymentTypeId;
+	public String ordPaymentTypeName;
+	public int payType;
+	public Double otstndAmt;
+	public int custId;
+	public String nric;
+	public int ordTypeId;
+	public String ordTypeName;
+}
