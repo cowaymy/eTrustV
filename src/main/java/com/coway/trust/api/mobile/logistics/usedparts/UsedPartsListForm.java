dc9277b9e1babeb26a9be8119847784ceaab1fb3
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2023-04-07 07:24:09 +0000

    Add Search Criteria

--- src/main/java/com/coway/trust/api/mobile/logistics/usedparts/UsedPartsListForm.java
+++ src/main/java/com/coway/trust/api/mobile/logistics/usedparts/UsedPartsListForm.java
@@ -9,66 +9,68 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(value = "UsedPartsListForm", description = "공통코드 Form")
 public class UsedPartsListForm {
 
-	@ApiModelProperty(value = "userId [default : '' 전체] 예) CD101707", example = "CD101707")
-	private String userId;
+  @ApiModelProperty(value = "userId [default : '' 전체] 예) CD101707", example = "CD101707")
+  private String userId;
 
-	@ApiModelProperty(value = "조회시작날짜 (YYYYMMDD) [default : '' 전체] 예) 20160307", example = "20170707")
-	private String searchFromDate;
+  @ApiModelProperty(value = "조회시작날짜 (YYYYMMDD) [default : '' 전체] 예) 20160307", example = "20170707")
+  private String searchFromDate;
 
-	@ApiModelProperty(value = "조회종료날짜 (YYYYMMDD [default : '' 전체] 예) 20170714", example = "20170714")
-	private String searchToDate;
+  @ApiModelProperty(value = "조회종료날짜 (YYYYMMDD [default : '' 전체] 예) 20170714", example = "20170714")
+  private String searchToDate;
 
-	@ApiModelProperty(value = "searchType [default : '' 전체] 예) return=1, not return=2 ", example = " 1,2")
-	private int searchType;
+  @ApiModelProperty(value = "searchType [default : '' 전체] 예) return=1, not return=2 ", example = " 1,2")
+  private int searchType;
 
-	
-	public static Map<String, Object> createMap(UsedPartsListForm usedPartsListForm) {
-		Map<String, Object> params = new HashMap<>();
-		params.put("userId", usedPartsListForm.getUserId());
-		params.put("searchFromDate", usedPartsListForm.getSearchFromDate());
-		params.put("searchToDate", usedPartsListForm.getSearchToDate());
-		params.put("searchType", usedPartsListForm.getSearchType());
-		return params;
-	}
+  @ApiModelProperty(value = "search Serial no", example = "13908ERI21A1504061")
+  private int searchSerialNo;
 
+  public static Map<String, Object> createMap(UsedPartsListForm usedPartsListForm) {
+    Map<String, Object> params = new HashMap<>();
+    params.put("userId", usedPartsListForm.getUserId());
+    params.put("searchFromDate", usedPartsListForm.getSearchFromDate());
+    params.put("searchToDate", usedPartsListForm.getSearchToDate());
+    params.put("searchType", usedPartsListForm.getSearchType());
+    params.put("searchSerialNo", usedPartsListForm.getSearchSerialNo());
+    return params;
+  }
 
-	public String getUserId() {
-		return userId;
-	}
+  public String getUserId() {
+    return userId;
+  }
 
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+  public String getSearchFromDate() {
+    return searchFromDate;
+  }
 
+  public void setSearchFromDate(String searchFromDate) {
+    this.searchFromDate = searchFromDate;
+  }
 
-	public String getSearchFromDate() {
-		return searchFromDate;
-	}
+  public String getSearchToDate() {
+    return searchToDate;
+  }
 
+  public void setSearchToDate(String searchToDate) {
+    this.searchToDate = searchToDate;
+  }
 
-	public void setSearchFromDate(String searchFromDate) {
-		this.searchFromDate = searchFromDate;
-	}
+  public int getSearchType() {
+    return searchType;
+  }
 
+  public void setSearchType(int searchType) {
+    this.searchType = searchType;
+  }
 
-	public String getSearchToDate() {
-		return searchToDate;
-	}
+  public int getSearchSerialNo() {
+    return searchSerialNo;
+  }
 
-
-	public void setSearchToDate(String searchToDate) {
-		this.searchToDate = searchToDate;
-	}
-
-
-	public int getSearchType() {
-		return searchType;
-	}
-
-
-	public void setSearchType(int searchType) {
-		this.searchType = searchType;
-	}
-
+  public void setSearchSerialNo(int searchSerialNo) {
+    this.searchSerialNo = searchSerialNo;
+  }
 }
