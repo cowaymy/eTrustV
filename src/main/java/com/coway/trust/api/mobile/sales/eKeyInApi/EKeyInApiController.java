49d2f1935427d8d9edc1b53db6ddbd79e131fb01
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2023-06-27 08:22:01 +0000

    Add checkTNA for Validate Card Validity bt crcID

--- src/main/java/com/coway/trust/api/mobile/sales/eKeyInApi/EKeyInApiController.java
+++ src/main/java/com/coway/trust/api/mobile/sales/eKeyInApi/EKeyInApiController.java
@@ -430,7 +430,7 @@ public class EKeyInApiController {
 
   @ApiOperation(value = "CHECK CARD TNA", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
   @RequestMapping(value = "/checkTNA", method = RequestMethod.GET)
-  public ResponseEntity<Integer> checkTNA (@RequestParam String param) throws Exception {
+  public ResponseEntity<EgovMap> checkTNA (@RequestParam String param) throws Exception {
     LOGGER.debug("checkTNA    값 : {}", param);
     return ResponseEntity.ok(eKeyInApiService.checkTNA(param));
   }
