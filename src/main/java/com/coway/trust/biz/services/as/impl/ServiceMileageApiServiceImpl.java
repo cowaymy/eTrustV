843d6417d615dc0abf1ff43aeb5c1a7b40eb8ec3
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2023-07-25 03:21:04 +0000

    Add for Service Mileage

---
+++ src/main/java/com/coway/trust/biz/services/as/impl/ServiceMileageApiServiceImpl.java
@@ -0,0 +1,93 @@
+package com.coway.trust.biz.services.as.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.collections4.MapUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.coway.trust.AppConstants;
+import com.coway.trust.api.mobile.common.userProfileApi.UserProfileApiDto;
+import com.coway.trust.api.mobile.common.userProfileApi.UserProfileApiForm;
+import com.coway.trust.api.mobile.payment.payment.PaymentForm;
+import com.coway.trust.api.mobile.sales.royaltyCustomerApi.RoyaltyCustomerListApiForm;
+import com.coway.trust.api.mobile.services.as.SyncIhrApiForm;
+import com.coway.trust.api.mobile.services.asFromCody.AsFromCodyDto;
+import com.coway.trust.api.mobile.services.asFromCody.AsFromCodyForm;
+import com.coway.trust.biz.login.impl.LoginMapper;
+import com.coway.trust.biz.payment.payment.service.impl.PaymentApiMapper;
+import com.coway.trust.biz.services.as.AsFromCodyApiService;
+import com.coway.trust.biz.services.as.IhrApiService;
+import com.coway.trust.biz.services.as.ServiceApiASDetailService;
+import com.coway.trust.biz.services.as.ServiceApiASService;
+import com.coway.trust.biz.services.as.ServiceMileageApiService;
+import com.coway.trust.biz.services.mlog.MSvcLogApiService;
+import com.coway.trust.cmmn.exception.ApplicationException;
+import com.coway.trust.cmmn.model.LoginVO;
+import com.coway.trust.util.CommonUtils;
+
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+@Service("serviceMileageApiService")
+public class ServiceMileageApiServiceImpl extends EgovAbstractServiceImpl implements ServiceMileageApiService {
+
+  @Resource(name = "serviceMileageApiServiceMapper")
+  private ServiceMileageApiServiceMapper serviceMileageApiServiceMapper;
+
+  @Autowired
+  private LoginMapper loginMapper;
+
+  private static final Logger logger = LoggerFactory.getLogger(AsFromCodyApiServiceImpl.class);
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public List<EgovMap> checkInMileage(Map<String, Object> params) {
+    // STEP 1 : VERIFY MASTER TABLE TO GET MASTER MILEAGE CLAIM NO
+    EgovMap mileageClaimNo = serviceMileageApiServiceMapper.getMileageClaimNo(params);
+
+    Map<String, Object> memInfo = new HashMap<>();
+    memInfo.put("_USER_ID", params.get("userName"));
+    memInfo.put("MemCode", params.get("userName"));
+    LoginVO loginVO = loginMapper.selectLoginInfoById(memInfo);
+    params.put("userID_SYS47", loginVO.getUserId());
+    loginVO = loginMapper.getVaccineDeclarationAplcntInfo(memInfo);
+    params.put("userID_ORG01", loginVO.getUserId());
+
+    if (CommonUtils.nvl(mileageClaimNo.get("MIL_CLM_NO")).equals("")) {
+      String masterMileageClaimNo = CommonUtils.nvl(serviceMileageApiServiceMapper.getMasterMileageClaimNo());
+      params.put("MIL_CLM_NO", masterMileageClaimNo);
+
+      // CREATE MASTER RECORD
+      int insertMasterClaimRecord = serviceMileageApiServiceMapper.insertMasterClaimRecord(params);
+      logger.debug(" = checkInMileage = MASTER RECORD INSERTED : " + insertMasterClaimRecord);
+    } else {
+      params.put("MIL_CLM_NO", CommonUtils.nvl(mileageClaimNo.get("MIL_CLM_NO")));
+    }
+
+    // STEP 2 : INSERT SUB TABLE
+    int updateExistingSubMileageClaim = serviceMileageApiServiceMapper.updateExistingSubMileageClaim(params);
+    logger.debug(" = checkInMileage = SUB RECORD UPDATED : " + updateExistingSubMileageClaim);
+
+    int insertSubMileageClaim = serviceMileageApiServiceMapper.insertSubMileageChaim(params);
+    logger.debug(" = checkInMileage = SUB RECORD INSERTED : " + insertSubMileageClaim);
+
+    Map<String, Object> returnParam = new HashMap<>();
+    if (insertSubMileageClaim > 0) {
+      returnParam.put("status", true);
+    } else {
+      returnParam.put("status", false);
+    }
+
+    return (List<EgovMap>) returnParam;
+  }
+
+}
