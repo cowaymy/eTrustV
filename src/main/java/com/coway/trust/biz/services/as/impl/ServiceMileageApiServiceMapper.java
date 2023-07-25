8c82e7330d85abd4f09d0400aca8e6e8591f1cd7
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2023-07-25 03:21:18 +0000

    Add for Service Mileage

---
+++ src/main/java/com/coway/trust/biz/services/as/impl/ServiceMileageApiServiceMapper.java
@@ -0,0 +1,24 @@
+package com.coway.trust.biz.services.as.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import egovframework.rte.psl.dataaccess.mapper.Mapper;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+@Mapper("serviceMileageApiServiceMapper")
+public interface ServiceMileageApiServiceMapper {
+
+  EgovMap getMileageClaimNo(Map<String, Object> params);
+
+  int getMasterMileageClaimNo();
+
+  int insertMasterClaimRecord(Map<String, Object> params);
+
+  int updateExistingSubMileageClaim(Map<String, Object> params);
+
+  int insertSubMileageChaim(Map<String, Object> params);
+
+  List<EgovMap> checkInMileage(Map<String, Object> params);
+
+}
