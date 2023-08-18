d50cbdae8f4ed42c3bc810513a1a1226437f1507
Tommy, Thun Ping Yi <pingyi.thun@coway.com.my>
2023-08-18 08:10:07 +0000

    [Ad Hoc] Eco Lite Material Prefix Conversion - add checking for serial prefix conversion configuration - TPY 20230818

--- src/main/java/com/coway/trust/biz/logistics/serialmgmt/impl/SerialMgmtNewServiceImpl.java
+++ src/main/java/com/coway/trust/biz/logistics/serialmgmt/impl/SerialMgmtNewServiceImpl.java
@@ -209,9 +209,14 @@ public class SerialMgmtNewServiceImpl implements SerialMgmtNewService{
 
 		    mainMap.put("docNoItm", serialMgmtNewMapper.selectHPDeliveryGRInfo(mainMap));
 		    if(StringUtils.isBlank((String)mainMap.get("docNoItm"))){
+		    	// Check serial prefix conversion configuration setting - LOG0205M
+		    	String serialPrefixChk = serialMgmtNewMapper.selectSerialPrefixConversion(mainMap);
+		    	logger.info("serialPrefixChk: " + serialPrefixChk);
+		    	if(StringUtils.isEmpty(serialPrefixChk)){
 		    	mainMap.put("stockName", "Serial No. (Invalid stock)");
 				mainMap.put("status", 0);
 				continue;
+		    	}
 		    }
 
 		    mainMap.put("stkGrad", "A");
