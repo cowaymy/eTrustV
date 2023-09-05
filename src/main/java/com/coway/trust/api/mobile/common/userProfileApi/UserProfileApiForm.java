7659fd7cd38ee40dc9bd1858a7c8f1c1a94f6f16
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2023-09-05 02:23:48 +0000

    ADD E-TAG FEATURE

--- src/main/java/com/coway/trust/api/mobile/common/userProfileApi/UserProfileApiForm.java
+++ src/main/java/com/coway/trust/api/mobile/common/userProfileApi/UserProfileApiForm.java
@@ -15,31 +15,35 @@ import io.swagger.annotations.ApiModel;
  * Date            Author          Description
  * -------------   -----------     -------------
  * 2019. 11. 01.   KR-JAEMJAEM:)   First creation
+ * 2023. 09. 05    MY-ONGHC         ADD E-TAG FEATURE
  * </pre>
  */
 @ApiModel(value = "UserProfileApiForm", description = "UserProfileApiForm")
 public class UserProfileApiForm {
+  public static Map<String, Object> createMap(UserProfileApiForm vo){
+    Map<String, Object> params = new HashMap<>();
+    params.put("userName", vo.getUserName());
+    params.put("imgGrpId", vo.getImgGrpId());
+    return params;
+  }
 
+  private String userName;
+  private String imgGrpId;
 
+  public String getUserName() {
+    return userName;
+  }
 
-	public static Map<String, Object> createMap(UserProfileApiForm vo){
-		Map<String, Object> params = new HashMap<>();
-        params.put("userName", vo.getUserName());
-		return params;
-	}
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
 
+  public String getImgGrpId() {
+    return imgGrpId;
+  }
 
-
-	private String userName;
-
-
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+  public void setImgGrpId(String imgGrpId) {
+    this.imgGrpId = imgGrpId;
+  }
 
 }
