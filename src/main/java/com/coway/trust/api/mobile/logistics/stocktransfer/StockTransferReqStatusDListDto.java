0279e67d05e7431b126ed0dd7335fd9a88053e2c
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2023-10-16 07:43:51 +0000

    Add getBarcodeChkYN

--- src/main/java/com/coway/trust/api/mobile/logistics/stocktransfer/StockTransferReqStatusDListDto.java
+++ src/main/java/com/coway/trust/api/mobile/logistics/stocktransfer/StockTransferReqStatusDListDto.java
@@ -9,95 +9,95 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(value = "StockTransferReqStatusDListDto", description = "StockTransferReqStatusDListDto")
 public class StockTransferReqStatusDListDto {
 
-	@ApiModelProperty(value = "SMO item no")
-	private int smoNoItem;
+  @ApiModelProperty(value = "SMO item no")
+  private int smoNoItem;
 
-	@ApiModelProperty(value = "부품코드")
-	private String partsCode;
+  @ApiModelProperty(value = "부품코드")
+  private String partsCode;
 
-	@ApiModelProperty(value = "부품명")
-	private String partsName;
+  @ApiModelProperty(value = "부품명")
+  private String partsName;
 
-	@ApiModelProperty(value = "부품 id")
-	private int partsId;
+  @ApiModelProperty(value = "부품 id")
+  private int partsId;
 
-	@ApiModelProperty(value = "부품 타입(필터(62) / 부품(63) / MISC(64))")
-	private int partsType;
+  @ApiModelProperty(value = "부품 타입(필터(62) / 부품(63) / MISC(64))")
+  private int partsType;
 
-	@ApiModelProperty(value = "요청수량")
-	private int requestQty;
+  @ApiModelProperty(value = "요청수량")
+  private int requestQty;
 
-	@ApiModelProperty(value = "부품 sn")
-	private String serialNo;
+  @ApiModelProperty(value = "부품 sn")
+  private String serialNo;
 
-	@ApiModelProperty(value = "바코드 대상 여부(Y 또는 공백)")
-	private String barcodeChkYN;
+  @ApiModelProperty(value = "바코드 대상 여부(Y 또는 공백)")
+  private String barcodeChkYN;
 
-	public String getBarcodeChkYN() {
-		return barcodeChkYN;
-	}
+  public static StockTransferReqStatusDListDto create(EgovMap egvoMap) {
+    return BeanConverter.toBean(egvoMap, StockTransferReqStatusDListDto.class);
+  }
 
-	public void setBarcodeChkYN(String barcodeChkYN) {
-		this.barcodeChkYN = barcodeChkYN;
-	}
+  public int getSmoNoItem() {
+    return smoNoItem;
+  }
 
-	public static StockTransferReqStatusDListDto create(EgovMap egvoMap) {
-		return BeanConverter.toBean(egvoMap, StockTransferReqStatusDListDto.class);
-	}
+  public void setSmoNoItem(int smoNoItem) {
+    this.smoNoItem = smoNoItem;
+  }
 
-	public int getSmoNoItem() {
-		return smoNoItem;
-	}
+  public int getRequestQty() {
+    return requestQty;
+  }
 
-	public void setSmoNoItem(int smoNoItem) {
-		this.smoNoItem = smoNoItem;
-	}
+  public void setRequestQty(int requestQty) {
+    this.requestQty = requestQty;
+  }
 
-	public int getRequestQty() {
-		return requestQty;
-	}
+  public String getPartsCode() {
+    return partsCode;
+  }
 
-	public void setRequestQty(int requestQty) {
-		this.requestQty = requestQty;
-	}
+  public void setPartsCode(String partsCode) {
+    this.partsCode = partsCode;
+  }
 
-	public String getPartsCode() {
-		return partsCode;
-	}
+  public String getPartsName() {
+    return partsName;
+  }
 
-	public void setPartsCode(String partsCode) {
-		this.partsCode = partsCode;
-	}
+  public void setPartsName(String partsName) {
+    this.partsName = partsName;
+  }
 
-	public String getPartsName() {
-		return partsName;
-	}
+  public String getSerialNo() {
+    return serialNo;
+  }
 
-	public void setPartsName(String partsName) {
-		this.partsName = partsName;
-	}
+  public void setSerialNo(String serialNo) {
+    this.serialNo = serialNo;
+  }
 
-	public String getSerialNo() {
-		return serialNo;
-	}
+  public int getPartsId() {
+    return partsId;
+  }
 
-	public void setSerialNo(String serialNo) {
-		this.serialNo = serialNo;
-	}
+  public void setPartsId(int partsId) {
+    this.partsId = partsId;
+  }
 
-	public int getPartsId() {
-		return partsId;
-	}
+  public int getPartsType() {
+    return partsType;
+  }
 
-	public void setPartsId(int partsId) {
-		this.partsId = partsId;
-	}
+  public void setPartsType(int partsType) {
+    this.partsType = partsType;
+  }
 
-	public int getPartsType() {
-		return partsType;
-	}
+  public String getBarcodeChkYN() {
+    return barcodeChkYN;
+  }
 
-	public void setPartsType(int partsType) {
-		this.partsType = partsType;
-	}
+  public void setBarcodeChkYN(String barcodeChkYN) {
+    this.barcodeChkYN = barcodeChkYN;
+  }
 }
