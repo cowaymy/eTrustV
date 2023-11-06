101019ee6ccc5b4d628d800f892f8532540dc3d0
Tang Hui Ling <huiling.tang@coway.com.my>
2023-11-06 03:23:11 +0000

    [Ad Hoc] H2H e-Mall Payment Receipt Automation - S3 Bucket

--- src/main/java/com/coway/trust/config/aws/AWSS3Config.java
+++ src/main/java/com/coway/trust/config/aws/AWSS3Config.java
@@ -11,13 +11,13 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
- 
+
 //@Configuration
 //@ComponentScan("com.coway.trust")
 @Component
 public class AWSS3Config {
 	private static Logger logger = LoggerFactory.getLogger(AWSS3Config.class);
-	
+
     // Access key id will be read from the application.properties file during the application intialization.
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
@@ -27,20 +27,20 @@ public class AWSS3Config {
     // Region will be read from the application.properties file  during the application intialization.
     @Value("${cloud.aws.region}")
     private String region;
- 
+
 //    @Bean
 //    public BasicAWSCredentials basicAWSCredentials() {
 //        return new BasicAWSCredentials(accessKey, secretKey);
 //    }
-    
+
 //    @Bean
 //    public static AmazonS3Client amazonS3Client() {
 //        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
 //                .withCredentials(new BasicAWSCredentials(accessKey, secretKey))
 //                .build();
 //    }
-    
-    @Primary
+
+    /*@Primary
     @Bean(name = "amazonS3Client")
 	public AmazonS3 amazonS3Client() {
     	logger.info("Start loading AmazonS3 Client with {}:{}",accessKey,secretKey);
@@ -50,5 +50,5 @@ public class AWSS3Config {
                 .withCredentials(new AWSStaticCredentialsProvider(
                         new BasicAWSCredentials(accessKey, secretKey)))
                 .build();
-    }
+    }*/
 }
