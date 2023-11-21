c6edf4dec88bd263f47ea1aa0deefa6d0d00d87b
Frango Liew Yao Jun <frango.liew@coway.com.my>
2023-11-21 12:44:19 +0000

    Voucher Management revert for Production

--- src/main/java/com/coway/trust/biz/sales/order/impl/OrderDetailServiceImpl.java
+++ src/main/java/com/coway/trust/biz/sales/order/impl/OrderDetailServiceImpl.java
@@ -21,7 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.coway.trust.biz.misc.voucher.impl.VoucherMapper;
+//import com.coway.trust.biz.misc.voucher.impl.VoucherMapper;
 import com.coway.trust.biz.sales.ccp.impl.CcpCalculateMapper;
 import com.coway.trust.biz.sales.customer.impl.CustomerMapper;
 import com.coway.trust.biz.sales.order.OrderDetailService;
@@ -57,8 +57,8 @@ public class OrderDetailServiceImpl extends EgovAbstractServiceImpl implements O
   @Resource(name = "ccpCalculateMapper")
   private CcpCalculateMapper ccpCalculateMapper;
 
-  @Resource(name = "voucherMapper")
-  private VoucherMapper voucherMapper;
+//  @Resource(name = "voucherMapper")
+//  private VoucherMapper voucherMapper;
 
   // @Autowired
   // private MessageSourceAccessor messageSourceAccessor;
@@ -156,16 +156,16 @@ public class OrderDetailServiceImpl extends EgovAbstractServiceImpl implements O
       this.loadConfigInfo(orderCfgInfo);
     // if(gstCertInfo != null) this.loadGstCertInfo(gstCertInfo);
 
-    if(basicInfo.get("voucherCode") != null
-    		&& basicInfo.get("voucherCode").toString().length() > 0){
-        Map<String,Object> voucherParam = new HashMap();
-        voucherParam.put("voucherCode", basicInfo.get("voucherCode").toString());
-        EgovMap voucherInfo = voucherMapper.getVoucherInfo(voucherParam);
-
-        if(voucherInfo != null){
-        	basicInfo.put("voucherInfo", voucherInfo);
-        }
-    }
+//    if(basicInfo.get("voucherCode") != null
+//    		&& basicInfo.get("voucherCode").toString().length() > 0){
+//        Map<String,Object> voucherParam = new HashMap();
+//        voucherParam.put("voucherCode", basicInfo.get("voucherCode").toString());
+//        EgovMap voucherInfo = voucherMapper.getVoucherInfo(voucherParam);
+//
+//        if(voucherInfo != null){
+//        	basicInfo.put("voucherInfo", voucherInfo);
+//        }
+//    }
 
     orderDetail.put("basicInfo", basicInfo);
     orderDetail.put("logView", logView);
