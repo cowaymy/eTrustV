2c8861145bf1585166190ba75f9cf5498848ee86
Abang Muhammad Adib Bin Abang Sapani <adib.sapani@coway.com.my>
2024-01-15 02:59:46 +0000

    Replace QR code api generator, google chart shut down

--- src/main/java/com/coway/trust/web/login/LoginController.java
+++ src/main/java/com/coway/trust/web/login/LoginController.java
@@ -128,7 +128,9 @@ public class LoginController {
 	//Barcode creation function
 	public static String getQRBarcodeURL(String user, String host, String secret) {
 		// QR코드 주소 생성
-		String format2 = "https://chart.apis.google.com/chart?cht=qr&chs=200x200&chl=otpauth://totp/%s@%s%%3Fsecret%%3D%s&chld=H|0";
+/*		String format2 = "http://chart.apis.google.com/chart?cht=qr&chs=200x200&chl=otpauth://totp/%s@%s%%3Fsecret%%3D%s&chld=H|0";
+*/
+		String format2 = "https://qrcode.tec-it.com/API/QRCode?size=small&dpi=150&data=otpauth://totp/%s@%s%%3Fsecret%%3D%s&chld=H|0";
 
 		return String.format(format2, user, host, secret);
 	}
