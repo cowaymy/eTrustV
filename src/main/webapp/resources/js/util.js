ced4b74a9f8bf1d86b37178e7d75b4250f8a2b15
Teoh Hui Ding <huiding.teoh@coway.com.my>
2024-03-19 10:02:22 +0000

    Hui Ding - SST 8%

--- src/main/webapp/resources/js/util.js
+++ src/main/webapp/resources/js/util.js
@@ -478,5 +478,19 @@ var FormUtil = {
 			flag = true;
 			}
 		return flag;
-	}
+	},
+
+	// 20240319 - Hui Ding - return rounding numbers with decimal places
+    roundNumber : function(num, scale) {
+    	  if(!("" + num).includes("e")) {
+    	    return +(Math.round(num + "e+" + scale)  + "e-" + scale);
+    	  } else {
+    	    var arr = ("" + num).split("e");
+    	    var sig = ""
+    	    if(+arr[1] + scale > 0) {
+    	      sig = "+";
+    	    }
+    	    return + (Math.round(+arr[0] + "e" + sig + (+arr[1] + scale)) + "e-" + scale);
+    	  }
+    	}
 };
