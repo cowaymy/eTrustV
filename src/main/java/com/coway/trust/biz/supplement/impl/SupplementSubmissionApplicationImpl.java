c077e840cf94d165f4e765e0bfb8dc8e59fef83c
Tommy, Thun Ping Yi <pingyi.thun@coway.com.my>
2024-05-29 07:10:48 +0000

    [Project Id :6600577937][New Development] eTrust Supplement Expansion - Add New Supplement Submission module - TPY

---
+++ src/main/java/com/coway/trust/biz/supplement/impl/SupplementSubmissionApplicationImpl.java
@@ -0,0 +1,120 @@
+package com.coway.trust.biz.supplement.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.coway.trust.biz.application.impl.FileApplicationImpl;
+import com.coway.trust.biz.common.FileGroupVO;
+import com.coway.trust.biz.common.FileService;
+import com.coway.trust.biz.common.FileVO;
+import com.coway.trust.biz.common.impl.FileMapper;
+import com.coway.trust.biz.common.type.FileType;
+import com.coway.trust.biz.supplement.SupplementSubmissionApplication;
+
+@Service("SupplementSubmissionApplication")
+public class SupplementSubmissionApplicationImpl implements SupplementSubmissionApplication {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FileApplicationImpl.class);
+
+  @Value("${app.name}")
+  private String appName;
+
+  @Autowired
+    private FileService fileService;
+
+  @Autowired
+    private FileMapper fileMapper;
+
+    @Resource(name = "supplementSubmissionMapper")
+    private SupplementSubmissionMapper supplementSubmissionMapper;
+
+  @Override
+  public void insertSupplementSubmissionAttachBiz(List<FileVO> list, FileType type, Map<String, Object> params, List<String> seqs) {
+    // TODO Auto-generated method stub
+    int fileGroupKey = fileMapper.selectFileGroupKey();
+    AtomicInteger i = new AtomicInteger(0); // get seq key.
+
+    list.forEach(r -> {this.insertFile(fileGroupKey, r, type, params, seqs.get(i.getAndIncrement()));});
+    params.put("fileGroupKey", fileGroupKey);
+  }
+
+  public void insertFile(int fileGroupKey, FileVO flVO, FileType flType, Map<String, Object> params,String seq) {
+        LOGGER.debug("insertFile :: Start");
+
+        int atchFlId = supplementSubmissionMapper.selectNextFileId();
+
+        FileGroupVO fileGroupVO = new FileGroupVO();
+
+        Map<String, Object> flInfo = new HashMap<String, Object>();
+        flInfo.put("atchFileId", atchFlId);
+        flInfo.put("atchFileName", flVO.getAtchFileName());
+        flInfo.put("fileSubPath", flVO.getFileSubPath());
+        flInfo.put("physiclFileName", flVO.getPhysiclFileName());
+        flInfo.put("fileExtsn", flVO.getFileExtsn());
+        flInfo.put("fileSize", flVO.getFileSize());
+        flInfo.put("filePassword", flVO.getFilePassword());
+        flInfo.put("fileUnqKey", params.get("claimUn"));
+        flInfo.put("fileKeySeq", seq);
+
+        supplementSubmissionMapper.insertFileDetail(flInfo);
+
+        fileGroupVO.setAtchFileGrpId(fileGroupKey);
+        fileGroupVO.setAtchFileId(atchFlId);
+        fileGroupVO.setChenalType(flType.getCode());
+        fileGroupVO.setCrtUserId(Integer.parseInt(params.get("userId").toString()));
+        fileGroupVO.setUpdUserId(Integer.parseInt(params.get("userId").toString()));
+
+        fileMapper.insertFileGroup(fileGroupVO);
+
+        LOGGER.debug("insertFile :: End");
+    }
+
+  @Override
+  public void updateSupplementSubmissionAttachBiz(List<FileVO> list, FileType type, Map<String, Object> params,List<String> seqs) {
+    // TODO Auto-generated method stub
+    LOGGER.debug("params =====================================>>  " + params.toString());
+    LOGGER.debug("list.size : {}", list.size());
+    String update = (String) params.get("update");
+    String remove = (String) params.get("remove");
+    String[] updateList = null;
+    String[] removeList = null;
+    if(!StringUtils.isEmpty(update)) {
+      updateList = params.get("update").toString().split(",");
+      LOGGER.debug("updateList.length : {}", updateList.length);
+    }
+    if(!StringUtils.isEmpty(remove)) {
+      removeList = params.get("remove").toString().split(",");
+      LOGGER.debug("removeList.length : {}", removeList.length);
+    }
+    // serivce 에서 파일정보를 가지고, DB 처리.
+    if (list.size() > 0) {
+      for(int i = 0; i < list.size(); i++) {
+        if(updateList != null && i < updateList.length) {
+          String atchFileId = updateList[i];
+          fileService.changeFileUpdate(Integer.parseInt(String.valueOf(params.get("atchFileGrpId"))), Integer.parseInt(atchFileId), list.get(i), type, Integer.parseInt(String.valueOf(params.get("userId"))));
+        }
+        else {
+          int fileGroupId = (Integer.parseInt(params.get("atchFileGrpId").toString()));
+          this.insertFile(fileGroupId, list.get(i), type,params, seqs.get(i));
+        }
+      }
+    }
+    if(removeList != null && removeList.length > 0){
+      for(String id : removeList){
+        LOGGER.info(id);
+        String atchFileId = id;
+        fileService.removeFileByFileId(type, Integer.parseInt(atchFileId));
+      }
+    }
+  }
+}
