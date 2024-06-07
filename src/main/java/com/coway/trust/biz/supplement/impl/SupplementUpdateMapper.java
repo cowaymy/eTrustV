7413501ae49e3091da96588809ef8ee76f042bb1
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-06-07 09:25:13 +0000

    Add updOrdDelStat

--- src/main/java/com/coway/trust/biz/supplement/impl/SupplementUpdateMapper.java
+++ src/main/java/com/coway/trust/biz/supplement/impl/SupplementUpdateMapper.java
@@ -31,6 +31,10 @@ public interface SupplementUpdateMapper {
 
   int updOrdDelStat( Map<String, Object> params );
 
+  void updateDelLstDtl( Map<String, Object> params );
+
+  void insertDelLstDtl( Map<String, Object> params );
+
   EgovMap getStoSup( Map<String, Object> params );
 
   Map<String, Object> SP_STO_PRE_SUPP( Map<String, Object> param );
