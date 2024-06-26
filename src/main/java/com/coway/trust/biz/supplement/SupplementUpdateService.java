0595c9a2af06f29d1e3e0b6af00fdbae723b675f
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-06-26 02:17:30 +0000

    Add supplementCustDelInfo

--- src/main/java/com/coway/trust/biz/supplement/SupplementUpdateService.java
+++ src/main/java/com/coway/trust/biz/supplement/SupplementUpdateService.java
@@ -8,7 +8,8 @@ import org.codehaus.jettison.json.JSONException;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface SupplementUpdateService {
-  List<EgovMap> selectSupplementList( Map<String, Object> params ) throws Exception;
+  List<EgovMap> selectSupplementList( Map<String, Object> params )
+    throws Exception;
 
   List<EgovMap> selectSupRefStus();
 
@@ -18,25 +19,19 @@ public interface SupplementUpdateService {
 
   List<EgovMap> selectSubmBrch();
 
-  List<EgovMap> selectWhBrnchList()
-    throws Exception;
+  List<EgovMap> selectWhBrnchList() throws Exception;
 
-  List<EgovMap> getSupplementDetailList( Map<String, Object> params )
-    throws Exception;
+  List<EgovMap> getSupplementDetailList( Map<String, Object> params ) throws Exception;
 
-  List<EgovMap> getDelRcdLst( Map<String, Object> params )
-    throws Exception;
+  List<EgovMap> getDelRcdLst( Map<String, Object> params ) throws Exception;
 
-  List<EgovMap> selectPosJsonList( Map<String, Object> params )
-    throws Exception;
+  List<EgovMap> selectPosJsonList( Map<String, Object> params ) throws Exception;
 
   List<EgovMap> checkDuplicatedTrackNo( Map<String, Object> params );
 
-  EgovMap selectOrderBasicInfo( Map<String, Object> params )
-    throws Exception;
+  EgovMap selectOrderBasicInfo( Map<String, Object> params ) throws Exception;
 
-  Map<String, Object> updateRefStgStatus( Map<String, Object> params )
-    throws Exception;
+  Map<String, Object> updateRefStgStatus( Map<String, Object> params ) throws Exception;
 
   EgovMap updOrdDelStatGdex( Map<String, Object> params ) throws IOException, JSONException, ParseException;
 
@@ -46,16 +41,17 @@ public interface SupplementUpdateService {
 
   Map<String, Object> SP_STO_PRE_SUPP( Map<String, Object> param );
 
-  void sendEmail(Map<String, Object> params);
+  void sendEmail( Map<String, Object> params );
 
-  public List<EgovMap> selectDocumentList(Map<String, Object> params);
+  public List<EgovMap> selectDocumentList( Map<String, Object> params );
 
-  List<EgovMap> selectPaymentMasterList(Map<String, Object> params);
+  List<EgovMap> selectPaymentMasterList( Map<String, Object> params );
 
-  EgovMap selectOrderBasicLedgerInfo(Map<String, Object> params);
+  EgovMap selectOrderBasicLedgerInfo( Map<String, Object> params );
 
-  List<EgovMap> getOderLdgr(Map<String, Object> params);
+  List<EgovMap> getOderLdgr( Map<String, Object> params );
 
-  List<EgovMap> getOderOutsInfo(Map<String, Object> params);
+  List<EgovMap> getOderOutsInfo( Map<String, Object> params );
 
+  List<EgovMap> getCustOrdDelInfo( Map<String, Object> params );
 }
