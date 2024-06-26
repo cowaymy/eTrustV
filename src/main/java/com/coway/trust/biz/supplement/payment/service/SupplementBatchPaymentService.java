24049fc45853543f5faadcc360d610e503cd777d
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-06-26 09:05:08 +0000

    Change Structure

--- src/main/java/com/coway/trust/biz/supplement/payment/service/SupplementBatchPaymentService.java
+++ src/main/java/com/coway/trust/biz/supplement/payment/service/SupplementBatchPaymentService.java
@@ -5,83 +5,124 @@ import java.util.Map;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
-public interface SupplementBatchPaymentService
-{
+public interface SupplementBatchPaymentService {
+  /**
+   * Batch Payment List(Master Grid) 조회
+   *
+   * @param params
+   * @return
+   */
+  List<EgovMap> selectBatchList( Map<String, Object> params );
 
+  /**
+   * selectBatchPaymentView 조회
+   *
+   * @param params
+   * @return
+   */
+  EgovMap selectBatchPaymentView( Map<String, Object> params );
 
-	/**
-	 * Batch Payment List(Master Grid) 조회
-	 * @param params
-	 * @return
-	 */
-    List<EgovMap> selectBatchList(Map<String, Object> params);
+  /**
+   * selectBatchPaymentDetList 조회
+   *
+   * @param params
+   * @return
+   */
+  List<EgovMap> selectBatchPaymentDetList( Map<String, Object> params );
 
-    /**
-	 * selectBatchPaymentView 조회
-	 * @param params
-	 * @return
-	 */
-    EgovMap selectBatchPaymentView(Map<String, Object> params);
+  /**
+   * selectTotalValidAmt 조회
+   *
+   * @param params
+   * @return
+   */
+  EgovMap selectTotalValidAmt( Map<String, Object> params );
 
-    /**
-	 * selectBatchPaymentDetList 조회
-	 * @param params
-	 * @return
-	 */
-    List<EgovMap> selectBatchPaymentDetList(Map<String, Object> params);
+  /**
+   * updRemoveItem
+   *
+   * @param params
+   * @return
+   */
+  int updRemoveItem( Map<String, Object> params );
 
-    /**
-	 * selectTotalValidAmt 조회
-	 * @param params
-	 * @return
-	 */
-    EgovMap selectTotalValidAmt(Map<String, Object> params);
+  /**
+   * saveItemConfirm
+   *
+   * @param params
+   * @return
+   */
+  int saveConfirmBatch( Map<String, Object> params );
 
-    /**
-	 * updRemoveItem
-	 * @param params
-	 * @return
-	 */
-    int updRemoveItem(Map<String, Object> params);
+  /**
+   * selectBatchPaymentDs
+   *
+   * @param params
+   * @return
+   */
+  EgovMap selectBatchPaymentDs( Map<String, Object> params );
 
-    /**
-	 * saveItemConfirm
-	 * @param params
-	 * @return
-	 */
-    int saveConfirmBatch(Map<String, Object> params);
+  /**
+   * saveDeactivateBatch
+   *
+   * @param params
+   * @return
+   */
+  int saveDeactivateBatch( Map<String, Object> params );
 
+  /**
+   * selectBatchPaymentMs
+   *
+   * @param params
+   * @return
+   */
+  EgovMap selectBatchPaymentMs( Map<String, Object> params );
 
-    /**
-	 * selectBatchPaymentDs
-	 * @param params
-	 * @return
-	 */
-    EgovMap selectBatchPaymentDs(Map<String, Object> params);
+  /**
+   * saveBatchPaymentUpload
+   *
+   * @param params
+   * @return
+   */
+  int saveBatchPaymentUpload( Map<String, Object> master, List<Map<String, Object>> detailList );
 
+  /**
+   * selectBatchPayCardModeId
+   *
+   * @param params
+   * @return
+   */
+  String selectBatchPayCardModeId( String cardModeCode );
 
-    /**
-	 * saveDeactivateBatch
-	 * @param params
-	 * @return
-	 */
-    int saveDeactivateBatch(Map<String, Object> params);
+  /**
+   * removeEGHLBatchOrderRecord
+   *
+   * @param params
+   * @return
+   */
+  int removeEGHLBatchOrderRecord( Map<String, Object> params );
 
-    /**
-	 * selectBatchPaymentMs
-	 * @param params
-	 * @return
-	 */
-    EgovMap selectBatchPaymentMs(Map<String, Object> params);
+  /**
+   * getPaymentMode
+   *
+   * @param params
+   * @return
+   */
+  List<EgovMap> getPaymentMode();
 
-    /**
-	 * saveBatchPaymentUpload
-	 * @param params
-	 * @return
-	 */
-    int saveBatchPaymentUpload(Map<String, Object> master, List<Map<String, Object>> detailList);
+  /**
+   * getPaymentBatStatus
+   *
+   * @param params
+   * @return
+   */
+  List<EgovMap> getPaymentBatStatus();
 
-  	String selectBatchPayCardModeId(String cardModeCode);
-
-  	int removeEGHLBatchOrderRecord(Map<String, Object> params);
+  /**
+   * getPaymentBatConfirmtStatus
+   *
+   * @param params
+   * @return
+   */
+  List<EgovMap> getPaymentBatConfirmtStatus();
 }
