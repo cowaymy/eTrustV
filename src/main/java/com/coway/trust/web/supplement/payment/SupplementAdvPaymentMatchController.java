d9e4820beb5b256ae426aad106da00326550d782
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-07-05 07:18:28 +0000

    Commit

--- src/main/java/com/coway/trust/web/supplement/payment/SupplementAdvPaymentMatchController.java
+++ src/main/java/com/coway/trust/web/supplement/payment/SupplementAdvPaymentMatchController.java
@@ -1,6 +1,5 @@
 package com.coway.trust.web.supplement.payment;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,151 +20,142 @@ import com.coway.trust.biz.supplement.payment.service.SupplementAdvPaymentMatchS
 import com.coway.trust.biz.payment.reconciliation.service.ReconciliationSearchVO;
 import com.coway.trust.cmmn.model.ReturnMessage;
 import com.coway.trust.cmmn.model.SessionVO;
+import com.coway.trust.util.CommonUtils;
+import com.coway.trust.web.sales.SalesConstants;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 @Controller
 @RequestMapping(value = "/supplement/payment")
 public class SupplementAdvPaymentMatchController {
+  @SuppressWarnings("unused")
+  private static final Logger LOGGER = LoggerFactory.getLogger( SupplementAdvPaymentMatchController.class );
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SupplementAdvPaymentMatchController.class);
+  @Resource(name = "supplementAdvPaymentMatchService")
+  private SupplementAdvPaymentMatchService supplementAdvPaymentMatchService;
 
-	@Resource(name = "supplementAdvPaymentMatchService")
-	private SupplementAdvPaymentMatchService supplementAdvPaymentMatchService;
+  /******************************************************
+   * Advance Payment Matching
+   *****************************************************/
+  /**
+   * Advance Payment Matching
+   *
+   * @param params
+   * @param model
+   * @return
+   */
+  @RequestMapping(value = "/initAdvPaymentMatch.do")
+  public String initAdvPaymentMatch( @RequestParam Map<String, Object> params, ModelMap model ) {
+    String bfDay = CommonUtils.changeFormat( CommonUtils.getCalDate( -30 ), SalesConstants.DEFAULT_DATE_FORMAT3,
+                                             SalesConstants.DEFAULT_DATE_FORMAT1 );
+    String toDay = CommonUtils.getFormattedString( SalesConstants.DEFAULT_DATE_FORMAT1 );
+    model.put( "bfDay", bfDay );
+    model.put( "toDay", toDay );
+    return "/supplement/payment/supplementAdvPaymentMatch";
+  }
 
-	/******************************************************
-	 *  Advance Payment Matching
-	 *****************************************************/
-	/**
-	 * Advance Payment Matching
-	 * @param params
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = "/initAdvPaymentMatch.do")
-	public String initAdvPaymentMatch(@RequestParam Map<String, Object> params, ModelMap model) {
-		return "/supplement/payment/supplementAdvPaymentMatch";
-	}
+  /**
+   * Advance Payment Matching
+   *
+   * @param
+   * @param params
+   * @param model
+   * @return
+   */
+  @RequestMapping(value = "/selectPaymentMatchList.do", method = RequestMethod.POST)
+  public ResponseEntity<Map<String, Object>> selectPaymentMatchList( @ModelAttribute("searchVO") ReconciliationSearchVO searchVO,
+                                                                     @RequestBody Map<String, Object> params,
+                                                                     ModelMap model ) {
+    Map<String, Object> resultMap = new HashMap<String, Object>();
+    List<EgovMap> keyInList = supplementAdvPaymentMatchService.selectAdvKeyInList( params );
+    List<EgovMap> stateList = supplementAdvPaymentMatchService.selectBankStateMatchList( params );
+    resultMap.put( "keyInList", keyInList );
+    resultMap.put( "stateList", stateList );
+    return ResponseEntity.ok( resultMap );
+  }
 
-	/**
-	 *  Advance Payment Matching
-	 * @param
-	 * @param params
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = "/selectPaymentMatchList.do", method = RequestMethod.POST)
-	public ResponseEntity<Map<String, Object>> selectPaymentMatchList(@ModelAttribute("searchVO")ReconciliationSearchVO searchVO, @RequestBody Map<String, Object> params, ModelMap model) {
-  	  LOGGER.info("[SupplementAdvPaymentMatchController-  selectPaymentMatchList] params:: {}" + params);
-  		Map<String, Object> resultMap = new HashMap<String, Object>();
+  /**
+   * Advance Payment Matching - Key In
+   *
+   * @param params
+   * @param model
+   * @return
+   */
+  @RequestMapping(value = "/initDetailGrpPaymentPop.do")
+  public String initRequestDCFPop( @RequestParam Map<String, Object> params, ModelMap model ) {
+    model.put( "groupSeq", params.get( "groupSeq" ) );
+    return "supplement/payment/supplementViewGroupPaymentKeyListPop";
+  }
 
-  		List<EgovMap> keyInList = supplementAdvPaymentMatchService.selectAdvKeyInList(params);
-  		List<EgovMap> stateList = supplementAdvPaymentMatchService.selectBankStateMatchList(params);
+  @RequestMapping(value = "/selectPaymentListByGroupSeq.do")
+  public ResponseEntity<List<EgovMap>> selectPaymentListByGroupSeq( @RequestBody Map<String, Object> params,
+                                                                    ModelMap model ) {
+    String[] groupSeqList = params.get( "groupSeq" ).toString().replace( "\"", "" ).split( "," );
+    params.put( "groupSeq", groupSeqList );
+    //조회.
+    List<EgovMap> resultList = supplementAdvPaymentMatchService.selectPaymentListByGroupSeq( params );
+    // 조회 결과 리턴.
+    return ResponseEntity.ok( resultList );
+  }
 
-  		resultMap.put("keyInList", keyInList);
-  		resultMap.put("stateList", stateList);
+  /**
+   * Advance Payment Matching - Mapping
+   *
+   * @param params
+   * @param model
+   * @return
+   */
+  @RequestMapping(value = "/saveAdvPaymentMapping.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> saveAdvPaymentMapping( @RequestBody Map<String, Object> params, ModelMap model,
+                                                              SessionVO sessionVO ) {
+    String groupSeq = params.get( "groupSeq" ).toString();
+    String groupSeqArr[];
+    if ( groupSeq.contains( "," ) ) {
+      groupSeqArr = groupSeq.split( "," );
+      params.put( "groupSeqArr", groupSeqArr );
+    }
 
-      return ResponseEntity.ok(resultMap);
-	}
+    params.put( "userId", sessionVO.getUserId() );
+    supplementAdvPaymentMatchService.saveAdvPaymentMapping( params );
 
-	/**
-	 *  Advance Payment Matching - Key In
-	 * @param params
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = "/initDetailGrpPaymentPop.do")
-	public String initRequestDCFPop(@RequestParam Map<String, Object> params, ModelMap model) {
-		model.put("groupSeq", params.get("groupSeq"));
-		LOGGER.debug("payment List params : {} ", params);
+    ReturnMessage message = new ReturnMessage();
+    message.setCode( AppConstants.SUCCESS );
+    message.setMessage( "Saved Successfully" );
+    return ResponseEntity.ok( message );
+  }
 
-		return "supplement/payment/supplementViewGroupPaymentKeyListPop";
-	}
+  /**
+   * Advance Payment Matching - Debtor 처리
+   *
+   * @param params
+   * @param model
+   * @return
+   */
+  @RequestMapping(value = "/saveAdvPaymentDebtor.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> saveAdvPaymentDebtor( @RequestBody Map<String, Object> params, ModelMap model,
+                                                             SessionVO sessionVO ) {
 
-	 @RequestMapping(value = "/selectPaymentListByGroupSeq.do")
-	  public ResponseEntity<List<EgovMap>> selectPaymentListByGroupSeq(@RequestBody Map<String, Object> params, ModelMap model) {
-	    LOGGER.debug("params : {} ", params);
+    params.put( "userId", sessionVO.getUserId() );
+    params.put( "groupSeq", params.get( "debtorGroupSeq" ) );
+    params.put( "fTrnscId", 0 );
+    params.put( "accCode", 0 );
+    supplementAdvPaymentMatchService.saveAdvPaymentDebtor( params );
 
-	    String[] groupSeqList = params.get("groupSeq").toString().replace("\"","").split(",");
-	    params.put("groupSeq", groupSeqList);
+    ReturnMessage message = new ReturnMessage();
+    message.setCode( AppConstants.SUCCESS );
+    message.setMessage( "Saved Successfully" );
+    return ResponseEntity.ok( message );
+  }
 
-	    //조회.
-	    List<EgovMap> resultList = supplementAdvPaymentMatchService.selectPaymentListByGroupSeq(params);
-
-	    // 조회 결과 리턴.
-	    return ResponseEntity.ok(resultList);
-	  }
-
-	/**
-	 * Advance Payment Matching - Mapping
-	 * @param params
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = "/saveAdvPaymentMapping.do", method = RequestMethod.POST)
-	public ResponseEntity<ReturnMessage> saveAdvPaymentMapping(
-			@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
-
-		LOGGER.debug("params : {} ", params);
-		String groupSeq = params.get("groupSeq").toString();
-		String groupSeqArr[];
-		if(groupSeq.contains(",")) {
-		    groupSeqArr = groupSeq.split(",");
-		    params.put("groupSeqArr", groupSeqArr);
-		}
-
-		// 저장
-		params.put("userId", sessionVO.getUserId());
-		supplementAdvPaymentMatchService.saveAdvPaymentMapping(params);
-
-		// 결과 만들기.
-		ReturnMessage message = new ReturnMessage();
-		message.setCode(AppConstants.SUCCESS);
-		message.setMessage("Saved Successfully");
-
-		return ResponseEntity.ok(message);
-
-	}
-
-	/**
-	 * Advance Payment Matching - Debtor 처리
-	 * @param params
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = "/saveAdvPaymentDebtor.do", method = RequestMethod.POST)
-	public ResponseEntity<ReturnMessage> saveAdvPaymentDebtor(
-			@RequestBody Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
-
-		LOGGER.debug("params : {} ", params);
-
-		// 저장
-		params.put("userId", sessionVO.getUserId());
-		params.put("groupSeq", params.get("debtorGroupSeq"));
-		params.put("fTrnscId", 0);
-		params.put("accCode", 0);
-
-		supplementAdvPaymentMatchService.saveAdvPaymentDebtor(params);
-
-		// 결과 만들기.
-		ReturnMessage message = new ReturnMessage();
-		message.setCode(AppConstants.SUCCESS);
-		message.setMessage("Saved Successfully");
-
-		return ResponseEntity.ok(message);
-	}
-
-	@RequestMapping(value = "/getAccountList.do", method = RequestMethod.GET)
+  @RequestMapping(value = "/getAccountList.do", method = RequestMethod.GET)
   public ResponseEntity<List<EgovMap>> getAccountList( @RequestParam Map<String, Object> params ) {
-    List<EgovMap> resultList = supplementAdvPaymentMatchService.getAccountList(params);
-    return ResponseEntity.ok(resultList);
+    List<EgovMap> resultList = supplementAdvPaymentMatchService.getAccountList( params );
+    return ResponseEntity.ok( resultList );
   }
 
   @RequestMapping(value = "/selectAdvKeyInReport.do")
-  public ResponseEntity<List<EgovMap>> selectAdvKeyInReport(@RequestBody Map<String, Object> params, ModelMap model) {
-      LOGGER.debug("[SupplementAdvPaymentMatchController - selectAdvKeyInReport] params : {} ", params);
-      List<EgovMap> resultList = supplementAdvPaymentMatchService.selectAdvKeyInReport(params);
-      return ResponseEntity.ok(resultList);
+  public ResponseEntity<List<EgovMap>> selectAdvKeyInReport( @RequestBody Map<String, Object> params, ModelMap model ) {
+    List<EgovMap> resultList = supplementAdvPaymentMatchService.selectAdvKeyInReport( params );
+    return ResponseEntity.ok( resultList );
   }
-
 }
