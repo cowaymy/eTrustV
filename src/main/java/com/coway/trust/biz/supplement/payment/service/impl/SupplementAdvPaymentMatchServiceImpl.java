8f1e13cd5b41bbcec4cbe190c40545d0673783cd
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-07-05 07:23:06 +0000

    Commit

--- src/main/java/com/coway/trust/biz/supplement/payment/service/impl/SupplementAdvPaymentMatchServiceImpl.java
+++ src/main/java/com/coway/trust/biz/supplement/payment/service/impl/SupplementAdvPaymentMatchServiceImpl.java
@@ -13,103 +13,70 @@ import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 @Service("supplementAdvPaymentMatchService")
-public class SupplementAdvPaymentMatchServiceImpl extends EgovAbstractServiceImpl implements SupplementAdvPaymentMatchService {
+public class SupplementAdvPaymentMatchServiceImpl
+  extends EgovAbstractServiceImpl
+  implements SupplementAdvPaymentMatchService {
+  @Resource(name = "supplementAdvPaymentMatchMapper")
+  private SupplementAdvPaymentMatchMapper supplementAdvPaymentMatchMapper;
 
-	@Resource(name = "supplementAdvPaymentMatchMapper")
-	private SupplementAdvPaymentMatchMapper supplementAdvPaymentMatchMapper;
-
-	//@Resource(name = "paymentListMapper")
-	//private PaymentListMapper paymentListMapper;
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(SupplementAdvPaymentMatchServiceImpl.class);
-
-	@Override
-	public List<EgovMap> selectAdvKeyInList(Map<String, Object> params) {
-		return supplementAdvPaymentMatchMapper.selectAdvKeyInList(params);
-	}
-
-	@Override
-	public List<EgovMap> selectBankStateMatchList(Map<String, Object> params) {
-		return supplementAdvPaymentMatchMapper.selectBankStateMatchList(params);
-	}
-
-	/**
-	 * Advance Payment Matching - Mapping 처리
-	 * @param params
-	 * @param model
-	 * @return
-	 */
-	@Override
-	@Transactional
-	public void saveAdvPaymentMapping(Map<String, Object> params) {
-
-		//Group Payment Mapping처리
-	  supplementAdvPaymentMatchMapper.mappingAdvGroupPayment(params);
-
-		//Bank Statement Mapping 처리
-	  supplementAdvPaymentMatchMapper.mappingBankStatementAdv(params);
-
-		//Interface 테이블 처리 - Bank Statement Bank Charge
-		List<EgovMap> returnList = supplementAdvPaymentMatchMapper.selectMappedData(params);
-
-
-		if(returnList != null && returnList.size() > 0){
-			for(int i = 0 ; i < returnList.size(); i++){
-
-				EgovMap ifMap  = (EgovMap) returnList.get(i);
-
-
-				//variance
-				ifMap.put("variance", params.get("variance"));
-				ifMap.put("userId", params.get("userId"));
-				supplementAdvPaymentMatchMapper.insertAdvPaymentMatchIF(ifMap);
-				supplementAdvPaymentMatchMapper.updateDiffTypeDiffAmt(ifMap);
-			}
-		}
-	}
-
-	 /**
-	 * Advance Payment Matching - Debtor 처리
-	 * @param params
-	 * @param model
-	 * @return
-	 */
-
-	@Transactional
-	public void saveAdvPaymentDebtor(Map<String, Object> params) {
-
-		//Group Payment Mapping처리
-		params.put("remark", params.get("debtorRemark"));
-		supplementAdvPaymentMatchMapper.mappingAdvGroupPayment(params);
-
-		//Interface 테이블 처리 - Bank Statement Bank Charge
-		List<EgovMap> returnList = supplementAdvPaymentMatchMapper.selectMappedData(params);
-
-		if(returnList != null && returnList.size() > 0){
-			for(int i = 0 ; i < returnList.size(); i++){
-
-				EgovMap ifMap  = (EgovMap) returnList.get(i);
-
-				//variance
-				ifMap.put("variance", params.get("variance"));
-				ifMap.put("userId", params.get("userId"));
-				supplementAdvPaymentMatchMapper.insertAdvPaymentDebtorIF(ifMap);
-			}
-		}
-	}
+  private static final Logger LOGGER = LoggerFactory.getLogger( SupplementAdvPaymentMatchServiceImpl.class );
 
   @Override
-  public List<EgovMap> getAccountList(Map<String, Object> params) {
-    return supplementAdvPaymentMatchMapper.getAccountList(params);
+  public List<EgovMap> selectAdvKeyInList( Map<String, Object> params ) {
+    return supplementAdvPaymentMatchMapper.selectAdvKeyInList( params );
   }
 
   @Override
-  public List<EgovMap> selectPaymentListByGroupSeq(Map<String, Object> params) {
-    return supplementAdvPaymentMatchMapper.selectPaymentListByGroupSeq(params);
+  public List<EgovMap> selectBankStateMatchList( Map<String, Object> params ) {
+    return supplementAdvPaymentMatchMapper.selectBankStateMatchList( params );
   }
 
   @Override
-  public List<EgovMap> selectAdvKeyInReport(Map<String, Object> params) {
-    return supplementAdvPaymentMatchMapper.selectAdvKeyInReport(params);
+  @Transactional
+  public void saveAdvPaymentMapping( Map<String, Object> params ) {
+    supplementAdvPaymentMatchMapper.mappingAdvGroupPayment( params );
+    supplementAdvPaymentMatchMapper.mappingBankStatementAdv( params );
+    List<EgovMap> returnList = supplementAdvPaymentMatchMapper.selectMappedData( params );
+
+    if ( returnList != null && returnList.size() > 0 ) {
+      for ( int i = 0; i < returnList.size(); i++ ) {
+        EgovMap ifMap = (EgovMap) returnList.get( i );
+        ifMap.put( "variance", params.get( "variance" ) );
+        ifMap.put( "userId", params.get( "userId" ) );
+        supplementAdvPaymentMatchMapper.insertAdvPaymentMatchIF( ifMap );
+        supplementAdvPaymentMatchMapper.updateDiffTypeDiffAmt( ifMap );
+      }
+    }
+  }
+
+  @Transactional
+  public void saveAdvPaymentDebtor( Map<String, Object> params ) {
+    params.put( "remark", params.get( "debtorRemark" ) );
+    supplementAdvPaymentMatchMapper.mappingAdvGroupPayment( params );
+    List<EgovMap> returnList = supplementAdvPaymentMatchMapper.selectMappedData( params );
+
+    if ( returnList != null && returnList.size() > 0 ) {
+      for ( int i = 0; i < returnList.size(); i++ ) {
+        EgovMap ifMap = (EgovMap) returnList.get( i );
+        ifMap.put( "variance", params.get( "variance" ) );
+        ifMap.put( "userId", params.get( "userId" ) );
+        supplementAdvPaymentMatchMapper.insertAdvPaymentDebtorIF( ifMap );
+      }
+    }
+  }
+
+  @Override
+  public List<EgovMap> getAccountList( Map<String, Object> params ) {
+    return supplementAdvPaymentMatchMapper.getAccountList( params );
+  }
+
+  @Override
+  public List<EgovMap> selectPaymentListByGroupSeq( Map<String, Object> params ) {
+    return supplementAdvPaymentMatchMapper.selectPaymentListByGroupSeq( params );
+  }
+
+  @Override
+  public List<EgovMap> selectAdvKeyInReport( Map<String, Object> params ) {
+    return supplementAdvPaymentMatchMapper.selectAdvKeyInReport( params );
   }
 }
