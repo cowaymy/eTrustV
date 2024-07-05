deef4907cb7e7401094f316a1ba2311c87df6119
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-07-05 07:22:17 +0000

    Commit

--- src/main/java/com/coway/trust/biz/supplement/payment/service/SupplementAdvPaymentMatchService.java
+++ src/main/java/com/coway/trust/biz/supplement/payment/service/SupplementAdvPaymentMatchService.java
@@ -5,35 +5,18 @@ import java.util.Map;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
-public interface SupplementAdvPaymentMatchService
-{
+public interface SupplementAdvPaymentMatchService {
+  List<EgovMap> selectAdvKeyInList( Map<String, Object> paramMap );
 
-	/**
-   	 * Advance Payment Match - Advance KeyIn List
-   	 */
-	List<EgovMap> selectAdvKeyInList(Map<String, Object> paramMap);
+  List<EgovMap> selectBankStateMatchList( Map<String, Object> paramMap );
 
-	/**
-   	 * Advance Payment Match - Bank Statement List
-   	 */
-	List<EgovMap> selectBankStateMatchList(Map<String, Object> paramMap);
+  void saveAdvPaymentMapping( Map<String, Object> params );
 
-	/**
-	 * Advance Payment Matching - Mapping
-	 */
-    void saveAdvPaymentMapping(Map<String, Object> params);
+  void saveAdvPaymentDebtor( Map<String, Object> params );
 
-    /**
-	 * Advance Payment Matching - Debtor
-	 * @param params
-	 * @param model
-	 * @return
-	 */
-    void saveAdvPaymentDebtor(Map<String, Object> params);
+  List<EgovMap> getAccountList( Map<String, Object> params );
 
-    List<EgovMap> getAccountList(Map<String, Object> params);
+  List<EgovMap> selectPaymentListByGroupSeq( Map<String, Object> params );
 
-    List<EgovMap> selectPaymentListByGroupSeq(Map<String, Object> params);
-
-    List<EgovMap> selectAdvKeyInReport(Map<String, Object> params);
+  List<EgovMap> selectAdvKeyInReport( Map<String, Object> params );
 }
