b8c1f51a7765e62c4d1df4333083eb50a6cd0fcc
Fannie <mingshan.tay@coway.com.my>
2024-07-09 08:45:24 +0000

    Add the new method for update the reference stage from '2' to '3' of supplement order when success mapping the each transaction

--- src/main/java/com/coway/trust/biz/supplement/payment/service/SupplementAdvPaymentMatchService.java
+++ src/main/java/com/coway/trust/biz/supplement/payment/service/SupplementAdvPaymentMatchService.java
@@ -6,17 +6,17 @@ import java.util.Map;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface SupplementAdvPaymentMatchService {
-  List<EgovMap> selectAdvKeyInList( Map<String, Object> paramMap );
+  List<EgovMap> selectAdvKeyInList(Map<String, Object> paramMap);
 
-  List<EgovMap> selectBankStateMatchList( Map<String, Object> paramMap );
+  List<EgovMap> selectBankStateMatchList(Map<String, Object> paramMap);
 
-  void saveAdvPaymentMapping( Map<String, Object> params );
+  void saveAdvPaymentMapping(Map<String, Object> params);
 
-  void saveAdvPaymentDebtor( Map<String, Object> params );
+  void saveAdvPaymentDebtor(Map<String, Object> params);
 
-  List<EgovMap> getAccountList( Map<String, Object> params );
+  List<EgovMap> getAccountList(Map<String, Object> params);
 
-  List<EgovMap> selectPaymentListByGroupSeq( Map<String, Object> params );
+  List<EgovMap> selectPaymentListByGroupSeq(Map<String, Object> params);
 
-  List<EgovMap> selectAdvKeyInReport( Map<String, Object> params );
+  List<EgovMap> selectAdvKeyInReport(Map<String, Object> params);
 }
--- src/main/java/com/coway/trust/biz/supplement/payment/service/impl/SupplementAdvPaymentMatchMapper.java
+++ src/main/java/com/coway/trust/biz/supplement/payment/service/impl/SupplementAdvPaymentMatchMapper.java
@@ -7,25 +7,27 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 @Mapper("supplementAdvPaymentMatchMapper")
 public interface SupplementAdvPaymentMatchMapper {
-  List<EgovMap> selectAdvKeyInList( Map<String, Object> paramMap );
+  List<EgovMap> selectAdvKeyInList(Map<String, Object> paramMap);
 
-  List<EgovMap> selectBankStateMatchList( Map<String, Object> paramMap );
+  List<EgovMap> selectBankStateMatchList(Map<String, Object> paramMap);
 
-  void mappingAdvGroupPayment( Map<String, Object> params );
+  void mappingAdvGroupPayment(Map<String, Object> params);
 
-  void mappingBankStatementAdv( Map<String, Object> params );
+  void mappingBankStatementAdv(Map<String, Object> params);
 
-  List<EgovMap> selectMappedData( Map<String, Object> paramMap );
+  List<EgovMap> selectMappedData(Map<String, Object> paramMap);
 
-  void insertAdvPaymentMatchIF( EgovMap params );
+  void insertAdvPaymentMatchIF(EgovMap params);
 
-  void insertAdvPaymentDebtorIF( EgovMap params );
+  void insertAdvPaymentDebtorIF(EgovMap params);
 
-  void updateDiffTypeDiffAmt( Map<String, Object> params );
+  void updateDiffTypeDiffAmt(Map<String, Object> params);
 
-  List<EgovMap> getAccountList( Map<String, Object> params );
+  List<EgovMap> getAccountList(Map<String, Object> params);
 
-  List<EgovMap> selectPaymentListByGroupSeq( Map<String, Object> params );
+  List<EgovMap> selectPaymentListByGroupSeq(Map<String, Object> params);
 
-  List<EgovMap> selectAdvKeyInReport( Map<String, Object> params );
+  List<EgovMap> selectAdvKeyInReport(Map<String, Object> params);
+
+  void updSupplementOrdStage(Map<String, Object> params);
 }
