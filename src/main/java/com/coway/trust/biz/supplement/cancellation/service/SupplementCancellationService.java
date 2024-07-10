d2c6c50492713f52d16e8128f5001c02b319fd36
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-07-10 07:56:28 +0000

    Add Cancellation Service

---
+++ src/main/java/com/coway/trust/biz/supplement/cancellation/service/SupplementCancellationService.java
@@ -0,0 +1,30 @@
+package com.coway.trust.biz.supplement.cancellation.service;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+import org.codehaus.jettison.json.JSONException;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+public interface SupplementCancellationService {
+
+  List<EgovMap> selectSupRefStus();
+
+  List<EgovMap> selectSupRefStg();
+
+  List<EgovMap> selectSupRtnStus();
+
+  List<EgovMap> selectSupDelStus();
+
+  List<EgovMap> selectSupplementCancellationJsonList( Map<String, Object> params ) throws Exception;
+
+  List<EgovMap> selectSupplementItmList( Map<String, Object> params ) throws Exception;
+
+  EgovMap selectOrderBasicInfo( Map<String, Object> params ) throws Exception;
+
+  List<EgovMap> checkDuplicatedTrackNo( Map<String, Object> params );
+
+  Map<String, Object> updateRefStgStatus( Map<String, Object> params ) throws Exception;
+
+}
