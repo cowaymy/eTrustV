cc81e3aa8dafea04c7b00f3ea4ce9ef8550af5da
An-nur Annisha Binti Abdul Ahmad <annisha.ahmad@coway.com.my>
2024-09-24 01:18:19 +0000

    [Bug Fix #24041967] Error during submit consent Pre CCP v2 -sha

--- src/main/java/com/coway/trust/config/ctos/client/xml/proxy/ws/ResRequestVO.java
+++ src/main/java/com/coway/trust/config/ctos/client/xml/proxy/ws/ResRequestVO.java
@@ -6,63 +6,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Data;
 
-
+@Setter
+@Getter
+@Builder
 @AllArgsConstructor
 public class ResRequestVO {
-    private String custIc;
-    private String resultRaw;
-    private String ficoScore;
-    private String batchNo;
-    private Date ctosDate;
-    private boolean bankRupt;
-    private String confirmEntity;
 
-
-	public String getCustIc() {
-		return custIc;
-	}
-	public void setCustIc(String custIc) {
-		this.custIc = custIc;
-	}
-	public String getResultRaw() {
-		return resultRaw;
-	}
-	public void setResultRaw(String resultRaw) {
-		this.resultRaw = resultRaw;
-	}
-	public String getFicoScore() {
-		return ficoScore;
-	}
-	public void setFicoScore(String ficoScore) {
-		this.ficoScore = ficoScore;
-	}
-	public String getBatchNo() {
-		return batchNo;
-	}
-	public void setBatchNo(String batchNo) {
-		this.batchNo = batchNo;
-	}
-	public Date getCtosDate() {
-		return ctosDate;
-	}
-	public void setCtosDate(Date ctosDate) {
-		this.ctosDate = ctosDate;
-	}
-	public boolean isBankRupt() {
-		return bankRupt;
-	}
-	public void setBankRupt(boolean bankRupt) {
-		this.bankRupt = bankRupt;
-	}
-	public String getConfirmEntity() {
-		return confirmEntity;
-	}
-	public void setConfirmEntity(String confirmEntity) {
-		this.confirmEntity = confirmEntity;
-	}
-
-
+	private String custIc;
+	private String batchNo;
+	private int ficoScore;
+	private String resultRaw;
+	private Date ctosDate;
+	private String bankRupt;
+	private String confirmEntity;
 
 }
