33f4bb17f4fee3342a905eb1e0df5c67702a33b3
An-nur Annisha Binti Abdul Ahmad <annisha.ahmad@coway.com.my>
2024-09-24 02:56:08 +0000

    [Bug Fix #24041967] Error during submit consent Pre CCP v3 -sha

--- src/main/java/com/coway/trust/config/ctos/client/xml/proxy/ws/ResRequestVO.java
+++ src/main/java/com/coway/trust/config/ctos/client/xml/proxy/ws/ResRequestVO.java
@@ -21,4 +21,48 @@ public class ResRequestVO {
 	private String bankRupt;
 	private String confirmEntity;
 
+
+	public String getCustIc() {
+		return custIc;
+	}
+	public void setCustIc(String custIc) {
+		this.custIc = custIc;
+	}
+	public String getBatchNo() {
+		return batchNo;
+	}
+	public void setBatchNo(String batchNo) {
+		this.batchNo = batchNo;
+	}
+	public int getFicoScore() {
+		return ficoScore;
+	}
+	public void setFicoScore(int ficoScore) {
+		this.ficoScore = ficoScore;
+	}
+	public String getResultRaw() {
+		return resultRaw;
+	}
+	public void setResultRaw(String resultRaw) {
+		this.resultRaw = resultRaw;
+	}
+	public Date getCtosDate() {
+		return ctosDate;
+	}
+	public void setCtosDate(Date ctosDate) {
+		this.ctosDate = ctosDate;
+	}
+	public String getBankRupt() {
+		return bankRupt;
+	}
+	public void setBankRupt(String bankRupt) {
+		this.bankRupt = bankRupt;
+	}
+	public String getConfirmEntity() {
+		return confirmEntity;
+	}
+	public void setConfirmEntity(String confirmEntity) {
+		this.confirmEntity = confirmEntity;
+	}
+
 }
