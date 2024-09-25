087cfbc74373a67ce81a2a789403c13e87cfd70c
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-09-25 02:51:48 +0000

    Commit Latest Changes

--- src/main/java/com/coway/trust/biz/supplement/colorGrid/service/SupplementColorGridService.java
+++ src/main/java/com/coway/trust/biz/supplement/colorGrid/service/SupplementColorGridService.java
@@ -6,10 +6,15 @@ import java.util.Map;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface SupplementColorGridService {
-	List<EgovMap> colorGridList(Map<String, Object> params);
-	List<EgovMap> selectProductCategoryList();
-	List<EgovMap> colorGridCmbProduct();
-	String getMemID(Map<String, Object> params);
-	List<EgovMap> selectCodeList();
-	List<EgovMap> getSupplementDetailList(Map<String, Object> params) throws Exception;
+  List<EgovMap> colorGridList( Map<String, Object> params );
+
+  List<EgovMap> selectProductCategoryList();
+
+  List<EgovMap> colorGridCmbProduct();
+
+  String getMemID( Map<String, Object> params );
+
+  List<EgovMap> selectCodeList();
+
+  List<EgovMap> getSupplementDetailList( Map<String, Object> params ) throws Exception;
 }
