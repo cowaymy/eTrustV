08a2267d51e228ed76ee5e01ab542e152b75cae2
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-11-04 06:24:57 +0000

    Create getPropUsrSessionKey

--- src/main/java/com/coway/trust/biz/login/LoginService.java
+++ src/main/java/com/coway/trust/biz/login/LoginService.java
@@ -1,5 +1,6 @@
 package com.coway.trust.biz.login;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Map;
 
@@ -11,62 +12,70 @@ import com.coway.trust.cmmn.model.SessionVO;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface LoginService {
+  LoginVO getLoginInfo( Map<String, Object> params );
 
-	LoginVO getLoginInfo(Map<String, Object> params);
+  LoginVO selectFindUserIdPop( Map<String, Object> params );
 
-	LoginVO selectFindUserIdPop(Map<String, Object> params);
+  void logout( Map<String, Object> params );
 
-	void logout(Map<String, Object> params);
+  LoginVO loginByMobile( Map<String, Object> params );
 
-	LoginVO loginByMobile(Map<String, Object> params);
+  LoginVO loginByCallcenter( Map<String, Object> params );
 
-	LoginVO loginByCallcenter(Map<String, Object> params);
+  void logoutByMobile( Map<String, Object> params );
 
-	void logoutByMobile(Map<String, Object> params);
+  List<EgovMap> getLanguages();
 
-	List<EgovMap> getLanguages();
+  int updatePassWord( Map<String, Object> params, Integer crtUserId );
 
-	int updatePassWord(Map<String, Object> params, Integer crtUserId);
+  int updateUserSetting( Map<String, Object> params, Integer crtUserId );
 
-	int updateUserSetting(Map<String, Object> params, Integer crtUserId);
+  void saveLoginHistory( LoginHistory loginHistory );
 
-	void saveLoginHistory(LoginHistory loginHistory);
+  List<EgovMap> selectSecureResnList( Map<String, Object> params );
 
-	List<EgovMap> selectSecureResnList(Map<String, Object> params);
+  EgovMap checkByPass( Map<String, Object> params );
 
-	EgovMap checkByPass(Map<String, Object> params);
+  LoginVO getAplcntInfo( Map<String, Object> params );
 
-	LoginVO getAplcntInfo(Map<String, Object> params);
+  EgovMap getDtls( Map<String, Object> params );
 
-	EgovMap getDtls(Map<String, Object> params);
+  EgovMap getPopDtls( Map<String, Object> params );
 
-	EgovMap getPopDtls(Map<String, Object> params);
+  EgovMap getCowayNoticePopDtls( Map<String, Object> params );
 
-	EgovMap getCowayNoticePopDtls(Map<String, Object> params);
+  int checkNotice();
 
-	int checkNotice();
+  //	int passwordRequest(Map<String, Object> params);
+  Map<String, Object> tempPwProcess( Map<String, Object> params );
 
-//	int passwordRequest(Map<String, Object> params);
+  int checkConsent();
 
-	Map<String, Object> tempPwProcess(Map<String, Object> params);
+  EgovMap getConsentDtls( Map<String, Object> params );
 
-	int checkConsent();
-	EgovMap getConsentDtls(Map<String, Object> params);
-	int loginPopAccept(Map<String, Object> params);
+  int loginPopAccept( Map<String, Object> params );
 
-	public void insertAttachDoc(List<FileVO> list, FileType type, Map<String, Object> params, List<String> seqs);
-	public int insertVaccineInfo (Map<String, Object> params, int userId, String memId);
-	public EgovMap getVaccineInfo (String memId);
+  public void insertAttachDoc( List<FileVO> list, FileType type, Map<String, Object> params, List<String> seqs );
 
-	LoginVO getVaccineDeclarationAplcntInfo(Map<String, Object> params);
+  public int insertVaccineInfo( Map<String, Object> params, int userId, String memId );
 
-	public int checkNewPassword (Map<String, Object> params);
-	public void updateLoginFailAttempt(Map<String, Object> params);
-	public void resetLoginFailAttempt(int userId);
-	public int getLoginFailedMaxAttempt();
-	public EgovMap selectUserByUserName(String username);
+  public EgovMap getVaccineInfo( String memId );
 
-	public int checkSecurityAnswer (Map<String, Object> params);
+  LoginVO getVaccineDeclarationAplcntInfo( Map<String, Object> params );
 
-	public void updateCheckMfaFlag(int userId);
+  public int checkNewPassword( Map<String, Object> params );
+
+  public void updateLoginFailAttempt( Map<String, Object> params );
+
+  public void resetLoginFailAttempt( int userId );
+
+  public int getLoginFailedMaxAttempt();
+
+  public EgovMap selectUserByUserName( String username );
+
+  public int checkSecurityAnswer( Map<String, Object> params );
+
+  public void updateCheckMfaFlag( int userId );
+
+  String getPropUsrSessionKey(Map<String, Object> params) throws NoSuchAlgorithmException;
 }
