22e65bfff119ee45bb69f203fb0685ec04cf5608
Tan Kai Yang <kaiyang.tan@coway.com.my>
2024-11-12 08:08:43 +0000

    [7782632216] [Enhancement] PCI DSS ASV Scan Issues Fix Enhancement

--- src/main/java/com/coway/trust/util/SecurityHeadersFilter.java
+++ src/main/java/com/coway/trust/util/SecurityHeadersFilter.java
@@ -23,13 +23,13 @@ public class SecurityHeadersFilter implements Filter {
 
 		// Set Content-Security-Policy header
 		httpResponse.setHeader("Content-Security-Policy",
-				"default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline'; \r\n"
-						+ "script-src * data: blob: 'unsafe-inline' 'unsafe-eval'; \r\n"
-						+ "script-src-elem * data: blob: 'unsafe-inline' 'unsafe-eval';\r\n"
-						+ "connect-src * data: blob: 'unsafe-inline'; \r\n"
-						+ "img-src * data: blob: 'unsafe-inline'; \r\n"
-						+ "media-src * data: blob: 'unsafe-inline'; \r\n" + "frame-src * data: blob: ; \r\n"
-						+ "style-src * data: blob: 'unsafe-inline';\r\n" + "font-src * data: blob: 'unsafe-inline';\r\n"
+				"default-src * data: mediastream: blob: filesystem: about: ws: wss: 'unsafe-eval' 'wasm-unsafe-eval' 'unsafe-inline';"
+						+ "script-src * data: blob: 'unsafe-inline' 'unsafe-eval';"
+						+ "script-src-elem * data: blob: 'unsafe-inline' 'unsafe-eval';"
+						+ "connect-src * data: blob: 'unsafe-inline';"
+						+ "img-src * data: blob: 'unsafe-inline';"
+						+ "media-src * data: blob: 'unsafe-inline';" + "frame-src * data: blob: ;"
+						+ "style-src * data: blob: 'unsafe-inline';" + "font-src * data: blob: 'unsafe-inline';"
 						+ "frame-ancestors * data: blob:;");
 
 		// Set X-Frame-Options header
