52f5a793f186d5a13097a07a52dae8d2c70e04a7
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-12-03 03:55:13 +0000

    AMEND installFailJobRequest

--- src/main/java/com/coway/trust/biz/services/installation/ServiceApiInstallationService.java
+++ src/main/java/com/coway/trust/biz/services/installation/ServiceApiInstallationService.java
@@ -13,19 +13,17 @@ import com.coway.trust.api.mobile.services.installation.InstallationResultForm;
  * @ClassName : ServiceApiInstallationService.java
  * @Description : Mobile Installation Data Save
  *
- * @History Date Author Description ------------- ----------- ------------- 2019. 09. 17. Jun First creation
- * 
+ * @History Date Author Description
+ * ------------- ----------- -------------
+ * 2019. 09. 17. Jun First creation
+ *
  */
 public interface ServiceApiInstallationService {
-  ResponseEntity<InstallationResultDto> installationResult(List<InstallationResultForm> installationResultForms)
-      throws Exception;
-  
-  ResponseEntity<InstallFailJobRequestDto> installFailJobRequest(InstallFailJobRequestForm installFailJobRequestForm)
-      throws Exception;
-  
-  ResponseEntity<InstallationResultDto> installationDtResult(List<InstallationResultForm> installationResultForms)
-      throws Exception;
-  
-  ResponseEntity<InstallFailJobRequestDto> installDtFailJobRequest(InstallFailJobRequestForm installFailJobRequestForm)
-      throws Exception;
+  ResponseEntity<InstallationResultDto> installationResult(List<InstallationResultForm> installationResultForms) throws Exception;
+
+  ResponseEntity<InstallFailJobRequestDto> installFailJobRequest(InstallFailJobRequestForm installFailJobRequestForm) throws Exception;
+
+  ResponseEntity<InstallationResultDto> installationDtResult(List<InstallationResultForm> installationResultForms) throws Exception;
+
+  ResponseEntity<InstallFailJobRequestDto> installDtFailJobRequest(InstallFailJobRequestForm installFailJobRequestForm) throws Exception;
 }
