cd730877a34e28dee57af7b616bf127487b30cce
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-12-04 02:16:25 +0000

    aMEND

--- src/main/java/com/coway/trust/api/mobile/services/installation/InstallFailJobRequestDto.java
+++ src/main/java/com/coway/trust/api/mobile/services/installation/InstallFailJobRequestDto.java
@@ -1,31 +1,44 @@
 package com.coway.trust.api.mobile.services.installation;
 
+import java.util.Map;
+
+import com.coway.trust.util.CommonUtils;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "InstallFailJobRequestDto", description = "InstallFailJobRequestDto")
 public class InstallFailJobRequestDto {
-	
-	@ApiModelProperty(value = "결과값")
-	private String result;
-	
-	public String getResult() {
-		return result;
-	}
+  @ApiModelProperty(value = "결과값")
+  private String result;
+  private String status;
 
+  public String getResult() {
+    return result;
+  }
 
-	public void setResult(String result) {
-		this.result = result;
-	}
-	
-	public static InstallFailJobRequestDto create(String result) {
-		InstallFailJobRequestDto dto = new InstallFailJobRequestDto();
-		dto.setResult(result);
-		
-		return dto;
-	}
+  public void setResult( String result ) {
+    this.result = result;
+  }
 
+  public String getStatus() {
+    return status;
+  }
 
+  public void setStatus( String status ) {
+    this.status = status;
+  }
 
-	
+  public static InstallFailJobRequestDto create( String result ) {
+    InstallFailJobRequestDto dto = new InstallFailJobRequestDto();
+    dto.setResult( result );
+    return dto;
+  }
+
+  public static InstallFailJobRequestDto create( Map<String, Object> rtnResultMap ) {
+    InstallFailJobRequestDto dto = new InstallFailJobRequestDto();
+    dto.setResult( CommonUtils.nvl(rtnResultMap.get( "result" )) );
+    dto.setStatus( CommonUtils.nvl(rtnResultMap.get( "status" )) );
+    return dto;
+  }
 }
