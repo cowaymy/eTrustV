1d09b0e1776e5c68cf1e45b488052c8db3c1e928
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-12-04 01:56:40 +0000

    AMEND installFailJobRequestProc

--- src/main/java/com/coway/trust/biz/services/installation/ServiceApiInstallationDetailService.java
+++ src/main/java/com/coway/trust/biz/services/installation/ServiceApiInstallationDetailService.java
@@ -7,17 +7,22 @@ import org.springframework.http.ResponseEntity;
 import com.coway.trust.api.mobile.services.installation.InstallFailJobRequestDto;
 import com.coway.trust.api.mobile.services.installation.InstallationResultDto;
 
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
 /**
  * @ClassName : ServiceApiInstallationDetailService.java
  * @Description : Mobile Installation Data Save
  *
  *
- * @History Date Author Description ------------- ----------- ------------- 2019. 09. 17. Jun First creation
+ * @History
+ * Date Author Description
+ * ------------- ----------- -------------
+ * 2019. 09. 17. Jun First creation
  */
 public interface ServiceApiInstallationDetailService {
   ResponseEntity<InstallationResultDto> installationResultProc(Map<String, Object> insApiresult) throws Exception;
 
-  ResponseEntity<InstallFailJobRequestDto> installFailJobRequestProc(Map<String, Object> params) throws Exception;
+  EgovMap installFailJobRequestProc(Map<String, Object> params) throws Exception;
 
   ResponseEntity<InstallationResultDto> installationDtResultProc(Map<String, Object> insApiresult) throws Exception;
 
