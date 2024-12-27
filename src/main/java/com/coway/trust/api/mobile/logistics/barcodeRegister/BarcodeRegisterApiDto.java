a64c3ddd5add5c4874eb50125200ad2a9e43cf39
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2024-12-27 03:57:23 +0000

    Add itmDesc

--- src/main/java/com/coway/trust/api/mobile/logistics/barcodeRegister/BarcodeRegisterApiDto.java
+++ src/main/java/com/coway/trust/api/mobile/logistics/barcodeRegister/BarcodeRegisterApiDto.java
@@ -8,43 +8,53 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "BarcodeRegisterApiDto", description = "공통코드 Dto")
 public class BarcodeRegisterApiDto {
+  @ApiModelProperty(value = "")
+  private String scanNo;
 
-	@ApiModelProperty(value = "")
-	private String scanNo;
+  @ApiModelProperty(value = "")
+  private String serialNo;
 
-	@ApiModelProperty(value = "")
-	private String serialNo;
+  @ApiModelProperty(value = "")
+  private String itmCode;
 
-	@ApiModelProperty(value = "")
-	private String itmCode;
+  @ApiModelProperty(value = "")
+  private String itmDesc;
 
-	@SuppressWarnings("unchecked")
-    public static BarcodeRegisterApiDto create(EgovMap egvoMap) {
-		return BeanConverter.toBean(egvoMap, BarcodeRegisterApiDto.class);
-	}
+  @SuppressWarnings("unchecked")
+  public static BarcodeRegisterApiDto create( EgovMap egvoMap ) {
+    return BeanConverter.toBean( egvoMap, BarcodeRegisterApiDto.class );
+  }
 
-	public String getScanNo() {
-		return scanNo;
-	}
+  public String getScanNo() {
+    return scanNo;
+  }
 
-	public void setScanNo(String scanNo) {
-		this.scanNo = scanNo;
-	}
+  public void setScanNo( String scanNo ) {
+    this.scanNo = scanNo;
+  }
 
-	public String getSerialNo() {
-		return serialNo;
-	}
+  public String getSerialNo() {
+    return serialNo;
+  }
 
-	public void setSerialNo(String serialNo) {
-		this.serialNo = serialNo;
-	}
+  public void setSerialNo( String serialNo ) {
+    this.serialNo = serialNo;
+  }
 
-	public String getItmCode() {
-		return itmCode;
-	}
+  public String getItmCode() {
+    return itmCode;
+  }
 
-	public void setItmCode(String itmCode) {
-		this.itmCode = itmCode;
-	}
+  public void setItmCode( String itmCode ) {
+    this.itmCode = itmCode;
+  }
+
+  public String getItmDesc() {
+    return itmDesc;
+  }
+
+  public void setItmDesc( String itmDesc ) {
+    this.itmDesc = itmDesc;
+  }
 
 }
