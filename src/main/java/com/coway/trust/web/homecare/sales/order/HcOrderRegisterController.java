5992dcdf0964e6eb7b0c97608fd01fc2afc10ed6
Tang Hui Ling <huiling.tang@coway.com.my>
2025-01-20 01:37:37 +0000

    MY-1232 & MY-1233 HC Module Ex-Trade Enhancement - phase 1.1 && PHASE 1.2_Extarde (HC Trade in HC) without Product Return (61m onward)

--- src/main/java/com/coway/trust/web/homecare/sales/order/HcOrderRegisterController.java
+++ src/main/java/com/coway/trust/web/homecare/sales/order/HcOrderRegisterController.java
@@ -491,8 +491,10 @@ public class HcOrderRegisterController {
 		String anoOrderNo = "";
 		if(isHc){
 			rMap = hcOrderListService.selectHcOrderInfo(cParam);
-			bundleId = rMap.get("bndlNo") == null ? "" : rMap.get("bndlNo").toString();
-			anoOrderNo = rMap.get("anoOrdNo") == null ? "" : rMap.get("anoOrdNo").toString();
+			if(rMap != null){
+				bundleId = rMap.get("bndlNo") == null ? "" : rMap.get("bndlNo").toString();
+				anoOrderNo = rMap.get("anoOrdNo") == null ? "" : rMap.get("anoOrdNo").toString();
+			}
 		}
 
 		model.put("bundleId", bundleId);
