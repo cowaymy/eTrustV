66d98def760880bf87fbc97524a14fdf75b84540
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2025-01-22 06:58:16 +0000

    Amend Code

--- src/main/java/com/coway/trust/biz/services/svcCodeConfig/svcCodeConfigService.java
+++ src/main/java/com/coway/trust/biz/services/svcCodeConfig/svcCodeConfigService.java
@@ -1,28 +1,22 @@
 package com.coway.trust.biz.services.svcCodeConfig;
 
-import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 
-import com.coway.trust.cmmn.model.ReturnMessage;
 import com.coway.trust.cmmn.model.SessionVO;
 
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 public interface svcCodeConfigService {
+  List<EgovMap> selectSvcCodeConfigList( Map<String, Object> params );
 
-	List<EgovMap> selectSvcCodeConfigList(Map<String, Object> params);
+  List<EgovMap> selectProductCategoryList();
 
-	//Product Category
-	List<EgovMap> selectProductCategoryList();
+  List<EgovMap> selectStatusCategoryCodeList();
 
-	// StatusCategory Code
-    List<EgovMap> selectStatusCategoryCodeList();
+  EgovMap selectCodeConfigList( Map<String, Object> params );
 
-	EgovMap selectCodeConfigList(Map<String, Object> params);
+  void saveNewCode( Map<String, Object> params, SessionVO sessionVO );
 
-	void saveNewCode(Map<String, Object> params, SessionVO sessionVO);
-
-	void updateSvcCode(Map<String, Object> params, SessionVO sessionVO);
-
+  void updateSvcCode( Map<String, Object> params, SessionVO sessionVO );
 }
