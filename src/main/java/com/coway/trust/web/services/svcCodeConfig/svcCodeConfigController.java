cb650f1e391276154750a27c22c65f235ee0c40b
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2025-01-22 06:57:55 +0000

    Amend Code

--- src/main/java/com/coway/trust/web/services/svcCodeConfig/svcCodeConfigController.java
+++ src/main/java/com/coway/trust/web/services/svcCodeConfig/svcCodeConfigController.java
@@ -35,127 +35,90 @@ import egovframework.rte.psl.dataaccess.util.EgovMap;
 @Controller
 @RequestMapping(value = "/services/svcCodeConfig")
 public class svcCodeConfigController {
+  private static final Logger logger = LoggerFactory.getLogger( svcCodeConfigController.class );
 
-	private static final Logger logger = LoggerFactory.getLogger(svcCodeConfigController.class);
+  @Value("${app.name}")
+  private String appName;
 
-	@Value("${app.name}")
-	private String appName;
+  @Resource(name = "svcCodeConfigService")
+  private svcCodeConfigService svcCodeConfigService;
 
-	@Resource(name = "svcCodeConfigService")
-	private svcCodeConfigService svcCodeConfigService;
+  @Resource(name = "commonService")
+  private CommonService commonService;
 
-	@Resource(name = "commonService")
-	private CommonService commonService;
+  @Resource(name = "promotionService")
+  private PromotionService promotionService;
 
-	@Resource(name = "promotionService")
-	private PromotionService promotionService;
+  @RequestMapping(value = "/svcCodeConfigList.do")
+  public String svcCodeConfigList( @RequestParam Map<String, Object> params, ModelMap model, SessionVO sessionVO ) {
+    List<EgovMap> codeStatus = svcCodeConfigService.selectStatusCategoryCodeList();
+    List<EgovMap> prodCatList = svcCodeConfigService.selectProductCategoryList();
+    model.addAttribute( "codeStatus", codeStatus );
+    model.addAttribute( "prodCatList", prodCatList );
+    return "services/svcCodeConfig/svcCodeConfigList";
+  }
 
-	@RequestMapping(value = "/svcCodeConfigList.do")
-	public String svcCodeConfigList(@RequestParam Map<String, Object> params, ModelMap model, SessionVO sessionVO) {
-		logger.debug("[svcCodeConfigController - svcCodeConfigList] params : {}", params);
+  @RequestMapping(value = "/selectSvcCodeConfigList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selectCodeMgmtList( @RequestParam Map<String, Object> params,
+                                                           HttpServletRequest request, ModelMap model,
+                                                           SessionVO sessionVO ) {
+    String[] arrProdCat = request.getParameterValues( "productCtgry" ); //Product Category
+    String[] arrCodeStus = request.getParameterValues( "codeStatusBrw" ); //Code Status
 
-		List<EgovMap> codeStatus = svcCodeConfigService.selectStatusCategoryCodeList();
-		List<EgovMap> prodCatList = promotionService.selectProductCategoryList();
-		logger.debug("[svcCodeConfigController - svcCodeConfigList] codeStatus : {}", codeStatus);
-		logger.debug("[svcCodeConfigController - svcCodeConfigList] prodCatList : {}", prodCatList);
+    if ( arrProdCat != null && !CommonUtils.containsEmpty( arrProdCat ) )
+      params.put( "arrProdCat", arrProdCat );
+    if ( arrCodeStus != null && !CommonUtils.containsEmpty( arrCodeStus ) )
+      params.put( "arrCodeStus", arrCodeStus );
 
-		model.addAttribute("codeStatus", codeStatus);
-		model.addAttribute("prodCatList", prodCatList);
+    List<EgovMap> codeConfigList = null;
+    codeConfigList = svcCodeConfigService.selectSvcCodeConfigList( params );
+    return ResponseEntity.ok( codeConfigList );
+  }
 
-		return "services/svcCodeConfig/svcCodeConfigList";
-	}
+  @RequestMapping(value = "/selectProductCategoryList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selectProductCategoryList( @RequestParam Map<String, Object> params ) {
+    List<EgovMap> resultList = svcCodeConfigService.selectProductCategoryList();
+    return ResponseEntity.ok( resultList );
+  }
 
-	@RequestMapping(value = "/selectSvcCodeConfigList.do" ,method = RequestMethod.GET)
-	public ResponseEntity<List<EgovMap>> selectCodeMgmtList(@RequestParam Map<String, Object>params, HttpServletRequest request, ModelMap model, SessionVO sessionVO) {
-		String[] arrProdCat = request.getParameterValues("productCtgry"); //Product Category
-		String[] arrCodeStus = request.getParameterValues("codeStatus"); //Code Status
+  @RequestMapping(value = "/selectStatusCategoryCodeList.do", method = RequestMethod.GET)
+  public ResponseEntity<List<EgovMap>> selectStatusCategoryCodeList( @RequestParam Map<String, Object> params ) {
+    List<EgovMap> codeList = svcCodeConfigService.selectStatusCategoryCodeList();
+    return ResponseEntity.ok( codeList );
+  }
 
-		if(arrProdCat != null && !CommonUtils.containsEmpty(arrProdCat))
-			params.put("arrProdCat", arrProdCat);
-		if(arrCodeStus != null && !CommonUtils.containsEmpty(arrCodeStus))
-			params.put("arrCodeStus", arrCodeStus);
-
-		logger.debug("[svcCodeConfigController - selectSvcCodeConfigList] params :: {}", params);
-		List<EgovMap> codeConfigList =null;
-		codeConfigList = svcCodeConfigService.selectSvcCodeConfigList(params);
-
-		logger.debug("[svcCodeConfigController - selectSvcCodeConfigList] codeConfigList :: {}", codeConfigList);
-		return ResponseEntity.ok(codeConfigList);
-	}
-
-	//Product Category
-	@RequestMapping(value = "/selectProductCategoryList.do", method = RequestMethod.GET)
-    public ResponseEntity<List<EgovMap>> selectProductCategoryList(@RequestParam Map<String, Object> params) {
-    	List<EgovMap> resultList = svcCodeConfigService.selectProductCategoryList();
-    	return ResponseEntity.ok(resultList);
+  @RequestMapping(value = "/addEditSvcCodeConfigPop.do")
+  public String addEditSvcCodeConfigPop( @RequestParam Map<String, Object> params, ModelMap model, SessionVO sessionVO )
+    throws Exception {
+    model.put( "viewType", (String) params.get( "viewType" ) );
+    if ( params.get( "viewType" ).equals( "2" ) || params.get( "viewType" ).equals( "3" ) ) {
+      EgovMap codeMgmtMap = svcCodeConfigService.selectCodeConfigList( params );
+      List<EgovMap> codeList = svcCodeConfigService.selectStatusCategoryCodeList();
+      model.put( "defectId", (String) params.get( "defectId" ) );
+      model.put( "hidTypeId", (String) params.get( "hidTypeId" ) );
+      model.addAttribute( "codeMgmtMap", codeMgmtMap );
     }
+    return "services/svcCodeConfig/addEditSvcCodeConfigPop";
+  }
 
-	//Status Category Code
-   @RequestMapping(value = "/selectStatusCategoryCodeList.do", method = RequestMethod.GET)
-    public ResponseEntity<List<EgovMap>> selectStatusCategoryCodeList(@RequestParam Map<String, Object> params) {
-	   logger.debug( "[svcCodeConfigController - selectStatusCategoryCodeList] params :: {}", params );
-       List<EgovMap> codeList = svcCodeConfigService.selectStatusCategoryCodeList();
-       return ResponseEntity.ok(codeList);
+  @RequestMapping(value = "/saveNewCode.do", method = RequestMethod.POST)
+  public ResponseEntity<ReturnMessage> saveNewCode( @RequestBody Map<String, Object> params, SessionVO sessionVO )
+    throws ParseException {
+    ReturnMessage message = new ReturnMessage();
+    Map<String, Object> newCodeMap = (Map<String, Object>) params.get( "newDpCodeConfig" );
+    newCodeMap.put( "dftType", "DP" );
+    newCodeMap.put( "creator", sessionVO.getUserId() );
+    newCodeMap.put( "updator", sessionVO.getUserId() );
+    if ( newCodeMap.get( "viewType" ).equals( "1" ) ) {
+      newCodeMap.put( "stus", "1" );
+      svcCodeConfigService.saveNewCode( newCodeMap, sessionVO );
+      message.setCode( AppConstants.SUCCESS );
+      message.setMessage( "Successfully configured code " + newCodeMap.get( "dftPrtCde" ) + "-" + newCodeMap.get( "dftPrtDesc" ) );
+    } else  {
+      svcCodeConfigService.updateSvcCode( newCodeMap, sessionVO );
+      message.setCode( AppConstants.SUCCESS );
+      message.setMessage( "Successfully update code " + newCodeMap.get( "dftPrtCde" ) + "-" + newCodeMap.get( "dftPrtDesc" ) );
     }
-
-	@RequestMapping(value = "/addEditSvcCodeConfigPop.do")
-	  public String addEditSvcCodeConfigPop(@RequestParam Map<String, Object> params, ModelMap model, SessionVO sessionVO)
-	      throws Exception {
-
-	    logger.debug("=====================/addEditSvcCodeConfigPop.do=========================");
-	    logger.debug("[svcCodeConfigController - addEditSvcCodeConfigPop] params : {}", params);
-	    model.put("viewType", (String) params.get("viewType"));
-	    logger.debug("=====================/addEditSvcCodeConfigPop.do=========================");
-
-	    if(params.get("viewType").equals("2") || params.get("viewType").equals("3")) //Edit or View
-	    {
-	    	EgovMap codeMgmtMap = svcCodeConfigService.selectCodeConfigList(params);
-	    	logger.debug("[svcCodeConfigController - addEditSvcCodeConfigPop] codeMgmtMap :: {}", codeMgmtMap);
-
-	    	List<EgovMap> codeList = svcCodeConfigService.selectStatusCategoryCodeList();
-		    model.put("defectId", (String) params.get("defectId"));
-		    model.put("hidTypeId", (String) params.get("hidTypeId"));
-		    model.addAttribute("codeMgmtMap", codeMgmtMap);
-	    }
-
-	    return "services/svcCodeConfig/addEditSvcCodeConfigPop";
-	  }
-
-	  @RequestMapping(value = "/saveNewCode.do", method = RequestMethod.POST)
-	  public ResponseEntity<ReturnMessage> saveNewCode(@RequestBody Map<String, Object> params, SessionVO sessionVO) throws ParseException {
-
-	    ReturnMessage message = new ReturnMessage();
-
-	    logger.debug("==========================/saveNewCode.do=================================");
-	    logger.debug("[svcCodeConfigController - saveNewCode] params :: {}", params);
-
-	    Map<String, Object> newCodeMap = (Map<String, Object>) params.get("newDpCodeConfig");
-	    logger.debug("[svcCodeConfigController - saveNewCode] newCodeMap ::{}" + newCodeMap.toString());
-
-	    newCodeMap.put("dftType", "DP");
-	    newCodeMap.put("creator", sessionVO.getUserId());
-	    newCodeMap.put("updator", sessionVO.getUserId());
-
-	    if(newCodeMap.get("viewType").equals("1"))//NEW
-	    {
-	    	newCodeMap.put("stus","1");
-	    	//message = svcCodeConfigService.saveNewCode(newCodeMap, sessionVO);
-	    	svcCodeConfigService.saveNewCode(newCodeMap, sessionVO);
-	    	message.setCode(AppConstants.SUCCESS);
-	        message.setMessage("Successfully configured code " + newCodeMap.get("dftPrtCde") + "-" + newCodeMap.get("dftPrtDesc"));
-
-	    }
-	    else //viewtype ==2 Edit, ==3 View
-	    {
-		    logger.debug("[svcCodeConfigController - saveNewCode] edit params ::{}" + newCodeMap);
-
-	    	svcCodeConfigService.updateSvcCode(newCodeMap, sessionVO);
-	    	message.setCode(AppConstants.SUCCESS);
-	        message.setMessage("Successfully update code " + newCodeMap.get("dftPrtCde") + "-" + newCodeMap.get("dftPrtDesc"));
-	    }
-
-	    logger.debug("==========================/saveNewCode.do=================================");
-
-	    return ResponseEntity.ok(message);
-	  }
+    return ResponseEntity.ok( message );
+  }
 }
