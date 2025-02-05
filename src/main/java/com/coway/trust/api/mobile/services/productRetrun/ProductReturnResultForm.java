9e9f3a95d2004e014dfcae3fbbbbcf77c9f12755
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2025-02-05 06:25:37 +0000

    Add HC product return partner ID and Code

--- src/main/java/com/coway/trust/api/mobile/services/productRetrun/ProductReturnResultForm.java
+++ src/main/java/com/coway/trust/api/mobile/services/productRetrun/ProductReturnResultForm.java
@@ -13,279 +13,287 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "ProductReturnResultForm", description = "공통코드 Form")
 public class ProductReturnResultForm {
+  @ApiModelProperty(value = "사용자 ID (예_CT123456)")
+  private String userId;
 
+  @ApiModelProperty(value = "주문번호")
+  private String salesOrderNo;
 
-	@ApiModelProperty(value = "사용자 ID (예_CT123456)")
-	private String userId;
+  @ApiModelProperty(value = "EX_BS00000 / AS00000")
+  private String serviceNo;
 
-	@ApiModelProperty(value = "주문번호")
-	private String salesOrderNo;
+  @ApiModelProperty(value = "결과 등록 메모")
+  private String resultRemark;
 
-	@ApiModelProperty(value = "EX_BS00000 / AS00000")
-	private String serviceNo;
+  @ApiModelProperty(value = "결과 등록시, Owner Code")
+  private String ownerCode;
 
-	@ApiModelProperty(value = "결과 등록 메모")
-	private String resultRemark;
+  @ApiModelProperty(value = "결과 등록시, Cust Name")
+  private String resultCustName;
 
-	@ApiModelProperty(value = "결과 등록시, Owner Code")
-	private String ownerCode;
+  @ApiModelProperty(value = "결과 등록시, NrIc 또는 Mobile No")
+  private String resultIcMobileNo;
 
-	@ApiModelProperty(value = "결과 등록시, Cust Name")
-	private String resultCustName;
+  @ApiModelProperty(value = "결과 등록시, Email_No")
+  private String resultReportEmailNo;
 
-	@ApiModelProperty(value = "결과 등록시, NrIc 또는 Mobile No")
-	private String resultIcMobileNo;
+  @ApiModelProperty(value = "결과 등록시, Acceptance Name")
+  private String resultAcceptanceName;
 
-	@ApiModelProperty(value = "결과 등록시, Email_No")
-	private String resultReportEmailNo;
+  @ApiModelProperty(value = "base64 Data")
+  private String signData;
 
-	@ApiModelProperty(value = "결과 등록시, Acceptance Name")
-	private String resultAcceptanceName;
+  @ApiModelProperty(value = "Transaction ID 값(체계 : USER_ID + SALES_ORDER_NO + SERVICE_NO + 현재시간_YYYYMMDDHHMMSS)")
+  private String transactionId;
 
-	@ApiModelProperty(value = "base64 Data")
-	private String signData;
+  private String checkInDate;
 
-	@ApiModelProperty(value = "Transaction ID 값(체계 : USER_ID + SALES_ORDER_NO + SERVICE_NO + 현재시간_YYYYMMDDHHMMSS)")
-	private String transactionId;
+  private String checkInTime;
 
-	private String checkInDate;
-	private String checkInTime;
-	private String checkInGps;
+  private String checkInGps;
 
+  @ApiModelProperty(value = "signRegDate Data")
+  private String signRegDate;
 
-	@ApiModelProperty(value = "signRegDate Data")
-	private String signRegDate;
+  @ApiModelProperty(value = "signRegTime Data")
+  private String signRegTime;
 
-	@ApiModelProperty(value = "signRegTime Data")
-	private String signRegTime;
+  private String ccCode;
 
-	private String ccCode;
-	private String resultCode;
+  private String resultCode;
 
-	private String serialRequireChkYn;
+  private String serialRequireChkYn;
 
-	private String retnCodeFailRemark;
+  private String retnCodeFailRemark;
 
-	public String getSerialRequireChkYn() {
-		return serialRequireChkYn;
-	}
+  private String partnerCode;
 
-	public void setSerialRequireChkYn(String serialRequireChkYn) {
-		this.serialRequireChkYn = serialRequireChkYn;
-	}
+  private String partnerCodeName;
 
-	public String getCcCode() {
-		return ccCode;
-	}
+  public String getSerialRequireChkYn() {
+    return serialRequireChkYn;
+  }
 
-	public void setCcCode(String ccCode) {
-		this.ccCode = ccCode;
-	}
+  public void setSerialRequireChkYn( String serialRequireChkYn ) {
+    this.serialRequireChkYn = serialRequireChkYn;
+  }
 
-	public String getResultCode() {
-		return resultCode;
-	}
+  public String getCcCode() {
+    return ccCode;
+  }
 
-	public void setResultCode(String resultCode) {
-		this.resultCode = resultCode;
-	}
+  public void setCcCode( String ccCode ) {
+    this.ccCode = ccCode;
+  }
 
-	public String getSignRegDate() {
-		return signRegDate;
-	}
+  public String getResultCode() {
+    return resultCode;
+  }
 
-	public void setSignRegDate(String signRegDate) {
-		this.signRegDate = signRegDate;
-	}
+  public void setResultCode( String resultCode ) {
+    this.resultCode = resultCode;
+  }
 
-	public String getSignRegTime() {
-		return signRegTime;
-	}
+  public String getSignRegDate() {
+    return signRegDate;
+  }
 
-	public void setSignRegTime(String signRegTime) {
-		this.signRegTime = signRegTime;
-	}
+  public void setSignRegDate( String signRegDate ) {
+    this.signRegDate = signRegDate;
+  }
 
+  public String getSignRegTime() {
+    return signRegTime;
+  }
 
+  public void setSignRegTime( String signRegTime ) {
+    this.signRegTime = signRegTime;
+  }
 
-	public String getCheckInDate() {
-		return checkInDate;
-	}
+  public String getCheckInDate() {
+    return checkInDate;
+  }
 
-	public void setCheckInDate(String checkInDate) {
-		this.checkInDate = checkInDate;
-	}
+  public void setCheckInDate( String checkInDate ) {
+    this.checkInDate = checkInDate;
+  }
 
-	public String getCheckInTime() {
-		return checkInTime;
-	}
+  public String getCheckInTime() {
+    return checkInTime;
+  }
 
-	public void setCheckInTime(String checkInTime) {
-		this.checkInTime = checkInTime;
-	}
+  public void setCheckInTime( String checkInTime ) {
+    this.checkInTime = checkInTime;
+  }
 
-	public String getCheckInGps() {
-		return checkInGps;
-	}
+  public String getCheckInGps() {
+    return checkInGps;
+  }
 
-	public void setCheckInGps(String checkInGps) {
-		this.checkInGps = checkInGps;
-	}
+  public void setCheckInGps( String checkInGps ) {
+    this.checkInGps = checkInGps;
+  }
 
+  public String getUserId() {
+    return userId;
+  }
 
+  public void setUserId( String userId ) {
+    this.userId = userId;
+  }
 
-	public String getUserId() {
-		return userId;
-	}
+  public String getSalesOrderNo() {
+    return salesOrderNo;
+  }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+  public void setSalesOrderNo( String salesOrderNo ) {
+    this.salesOrderNo = salesOrderNo;
+  }
 
-	public String getSalesOrderNo() {
-		return salesOrderNo;
-	}
+  public String getServiceNo() {
+    return serviceNo;
+  }
 
-	public void setSalesOrderNo(String salesOrderNo) {
-		this.salesOrderNo = salesOrderNo;
-	}
+  public void setServiceNo( String serviceNo ) {
+    this.serviceNo = serviceNo;
+  }
 
-	public String getServiceNo() {
-		return serviceNo;
-	}
+  public String getResultRemark() {
+    return resultRemark;
+  }
 
-	public void setServiceNo(String serviceNo) {
-		this.serviceNo = serviceNo;
-	}
+  public void setResultRemark( String resultRemark ) {
+    this.resultRemark = resultRemark;
+  }
 
-	public String getResultRemark() {
-		return resultRemark;
-	}
+  public String getOwnerCode() {
+    return ownerCode;
+  }
 
-	public void setResultRemark(String resultRemark) {
-		this.resultRemark = resultRemark;
-	}
+  public void setOwnerCode( String ownerCode ) {
+    this.ownerCode = ownerCode;
+  }
 
-	public String getOwnerCode() {
-		return ownerCode;
-	}
-
-	public void setOwnerCode(String ownerCode) {
-		this.ownerCode = ownerCode;
-	}
-
-	public String getResultCustName() {
-		return resultCustName;
-	}
-
-	public void setResultCustName(String resultCustName) {
-		this.resultCustName = resultCustName;
-	}
-
-	public String getResultIcMobileNo() {
-		return resultIcMobileNo;
-	}
-
-	public void setResultIcMobileNo(String resultIcMobileNo) {
-		this.resultIcMobileNo = resultIcMobileNo;
-	}
-
-	public String getResultReportEmailNo() {
-		return resultReportEmailNo;
-	}
-
-	public void setResultReportEmailNo(String resultReportEmailNo) {
-		this.resultReportEmailNo = resultReportEmailNo;
-	}
-
-	public String getResultAcceptanceName() {
-		return resultAcceptanceName;
-	}
-
-	public void setResultAcceptanceName(String resultAcceptanceName) {
-		this.resultAcceptanceName = resultAcceptanceName;
-	}
-
-	public String getSignData() {
-		return signData;
-	}
-
-	public void setSignData(String signData) {
-		this.signData = signData;
-	}
-
-	public String getTransactionId() {
-		return transactionId;
-	}
-
-	public void setTransactionId(String transactionId) {
-		this.transactionId = transactionId;
-	}
-
-	private String scanSerial;
-
-	public String getScanSerial() {
-		return scanSerial;
-	}
-
-	public void setScanSerial(String scanSerial) {
-		this.scanSerial = scanSerial;
-	}
-
-	private String fraSerialNo;
-	public String getFraSerialNo() {
-		return fraSerialNo;
-	}
-
-	public void setFraSerialNo(String fraSerialNo) {
-		this.fraSerialNo = fraSerialNo;
-	}
-
-	public String getRetnCodeFailRemark() {
-		return retnCodeFailRemark;
-	}
-
-	public void setRetnCodeFailRemark(String retnCodeFailRemark) {
-		this.retnCodeFailRemark = retnCodeFailRemark;
-	}
-
-	public static  List<Map<String, Object>>   createMaps(ProductReturnResultForm productReturnResultForm) {
-
-		List<Map<String, Object>> list = new ArrayList<>();
-			   Map<String, Object> map;
-
-				map = BeanConverter.toMap(productReturnResultForm, "signData");
-
-
-				map.put("resultCode",  	productReturnResultForm.getResultCode());
-				map.put("ccCode",  		productReturnResultForm.getCcCode());
-				map.put("signData", Base64.decodeBase64(productReturnResultForm.getSignData()));
-				//map.put("signData",    productReturnResultForm.getSignData());
-				map.put("checkinDt",  	productReturnResultForm.getCheckInDate());
-				map.put("checkinTm",  	productReturnResultForm.getCheckInTime());
-		    	map.put("checkinGps",  	productReturnResultForm.getCheckInGps());
-		    	map.put("signRegDt",  	productReturnResultForm.getSignRegDate());
-		    	map.put("signRegTm",   productReturnResultForm.getSignRegTime());
-				map.put("userId",		    productReturnResultForm.getUserId());
-				map.put("salesOrderNo", productReturnResultForm.getSalesOrderNo());
-				map.put("serviceNo", productReturnResultForm.getServiceNo());
-				map.put("resultRemark", productReturnResultForm.getResultRemark());
-				map.put("ownerCode", productReturnResultForm.getOwnerCode());
-				map.put("resultCustName", productReturnResultForm.getResultCustName());
-				map.put("resultIcMobileNo", productReturnResultForm.getResultIcMobileNo());
-				map.put("resultReportEmailNo", productReturnResultForm.getResultReportEmailNo());
-				map.put("resultAcceptanceName", productReturnResultForm.getResultAcceptanceName());
-				map.put("transactionId", productReturnResultForm.getTransactionId());
-				map.put("scanSerial", productReturnResultForm.getScanSerial());
-				map.put("fraSerialNo", productReturnResultForm.getFraSerialNo());
-				map.put("retnCodeFailRemark", productReturnResultForm.getRetnCodeFailRemark());
-
-
-				list.add(map);
-
-				return list;
-	}
-
-
-
+  public String getResultCustName() {
+    return resultCustName;
+  }
+
+  public void setResultCustName( String resultCustName ) {
+    this.resultCustName = resultCustName;
+  }
+
+  public String getResultIcMobileNo() {
+    return resultIcMobileNo;
+  }
+
+  public void setResultIcMobileNo( String resultIcMobileNo ) {
+    this.resultIcMobileNo = resultIcMobileNo;
+  }
+
+  public String getResultReportEmailNo() {
+    return resultReportEmailNo;
+  }
+
+  public void setResultReportEmailNo( String resultReportEmailNo ) {
+    this.resultReportEmailNo = resultReportEmailNo;
+  }
+
+  public String getResultAcceptanceName() {
+    return resultAcceptanceName;
+  }
+
+  public void setResultAcceptanceName( String resultAcceptanceName ) {
+    this.resultAcceptanceName = resultAcceptanceName;
+  }
+
+  public String getSignData() {
+    return signData;
+  }
+
+  public void setSignData( String signData ) {
+    this.signData = signData;
+  }
+
+  public String getTransactionId() {
+    return transactionId;
+  }
+
+  public void setTransactionId( String transactionId ) {
+    this.transactionId = transactionId;
+  }
+
+  private String scanSerial;
+
+  public String getScanSerial() {
+    return scanSerial;
+  }
+
+  public void setScanSerial( String scanSerial ) {
+    this.scanSerial = scanSerial;
+  }
+
+  private String fraSerialNo;
+
+  public String getFraSerialNo() {
+    return fraSerialNo;
+  }
+
+  public void setFraSerialNo( String fraSerialNo ) {
+    this.fraSerialNo = fraSerialNo;
+  }
+
+  public String getRetnCodeFailRemark() {
+    return retnCodeFailRemark;
+  }
+
+  public void setRetnCodeFailRemark( String retnCodeFailRemark ) {
+    this.retnCodeFailRemark = retnCodeFailRemark;
+  }
+
+  public String getPartnerCode() {
+    return partnerCode;
+  }
+
+  public void setPartnerCode( String partnerCode ) {
+    this.partnerCode = partnerCode;
+  }
+
+  public String getPartnerCodeName() {
+    return partnerCodeName;
+  }
+
+  public void setPartnerCodeName( String partnerCodeName ) {
+    this.partnerCodeName = partnerCodeName;
+  }
+
+  public static List<Map<String, Object>> createMaps( ProductReturnResultForm productReturnResultForm ) {
+    List<Map<String, Object>> list = new ArrayList<>();
+    Map<String, Object> map;
+    map = BeanConverter.toMap( productReturnResultForm, "signData" );
+    map.put( "resultCode", productReturnResultForm.getResultCode() );
+    map.put( "ccCode", productReturnResultForm.getCcCode() );
+    map.put( "signData", Base64.decodeBase64( productReturnResultForm.getSignData() ) );
+    //map.put("signData",    productReturnResultForm.getSignData());
+    map.put( "checkinDt", productReturnResultForm.getCheckInDate() );
+    map.put( "checkinTm", productReturnResultForm.getCheckInTime() );
+    map.put( "checkinGps", productReturnResultForm.getCheckInGps() );
+    map.put( "signRegDt", productReturnResultForm.getSignRegDate() );
+    map.put( "signRegTm", productReturnResultForm.getSignRegTime() );
+    map.put( "userId", productReturnResultForm.getUserId() );
+    map.put( "salesOrderNo", productReturnResultForm.getSalesOrderNo() );
+    map.put( "serviceNo", productReturnResultForm.getServiceNo() );
+    map.put( "resultRemark", productReturnResultForm.getResultRemark() );
+    map.put( "ownerCode", productReturnResultForm.getOwnerCode() );
+    map.put( "resultCustName", productReturnResultForm.getResultCustName() );
+    map.put( "resultIcMobileNo", productReturnResultForm.getResultIcMobileNo() );
+    map.put( "resultReportEmailNo", productReturnResultForm.getResultReportEmailNo() );
+    map.put( "resultAcceptanceName", productReturnResultForm.getResultAcceptanceName() );
+    map.put( "transactionId", productReturnResultForm.getTransactionId() );
+    map.put( "scanSerial", productReturnResultForm.getScanSerial() );
+    map.put( "fraSerialNo", productReturnResultForm.getFraSerialNo() );
+    map.put( "partnerCode", productReturnResultForm.getPartnerCode() );
+    map.put( "partnerCodeName", productReturnResultForm.getPartnerCodeName() );
+    list.add( map );
+    return list;
+  }
 }
