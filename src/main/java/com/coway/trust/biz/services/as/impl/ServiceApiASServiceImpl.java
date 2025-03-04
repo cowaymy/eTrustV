2bea1bd1bae769f0ee1c129dc3f54b93f9532b2d
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2025-03-04 08:30:26 +0000

    File Restructure

--- src/main/java/com/coway/trust/biz/services/as/impl/ServiceApiASServiceImpl.java
+++ src/main/java/com/coway/trust/biz/services/as/impl/ServiceApiASServiceImpl.java
@@ -23,23 +23,20 @@ import com.coway.trust.biz.services.mlog.MSvcLogApiService;
 import com.coway.trust.cmmn.exception.BizException;
 import com.coway.trust.util.CommonUtils;
 
-import org.apache.http.client.HttpResponseException;
 import com.coway.trust.cmmn.exception.ApplicationException;
 import com.coway.trust.AppConstants;
 
 import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import egovframework.rte.psl.dataaccess.util.EgovMap;
 
-/**
- * @ClassName : ServiceApiASServiceImpl.java
- * @Description : Mobile After Service Data Save
- * @History Date Author Description ------------- ----------- ------------- 2019. 09. 20. Jun First
- *          creation
- */
+/*********************************************************************************************
+ * DATE          PIC        VERSION     COMMENT
+ *--------------------------------------------------------------------------------------------
+ * 04/03/2025    ONGHC      1.0.1     - Amend File Format
+ *********************************************************************************************/
+
 @Service("serviceApiASService")
-public class ServiceApiASServiceImpl
-  extends EgovAbstractServiceImpl
-  implements ServiceApiASService {
+public class ServiceApiASServiceImpl extends EgovAbstractServiceImpl implements ServiceApiASService {
   private static final Logger logger = LoggerFactory.getLogger( ServiceApiASServiceImpl.class );
 
   @Resource(name = "MSvcLogApiService")
@@ -50,16 +47,16 @@ public class ServiceApiASServiceImpl
 
   @SuppressWarnings("unchecked")
   @Override
-  public ResponseEntity<AfterServiceResultDto> asResult( List<AfterServiceResultForm> afterServiceForms )
-    throws Exception {
+  public ResponseEntity<AfterServiceResultDto> asResult( List<AfterServiceResultForm> afterServiceForms ) throws Exception {
     String transactionId = "";
     String serviceNo = "";
+
     Map<String, Object> errMap = new HashMap<String,Object>();
     EgovMap rtnResultMap = new EgovMap();
     List<Map<String, Object>> asTransLogs = null;
     List<Map<String, Object>> asTransLogs1 = null;
 
-    int totalCnt = 0;
+    // int totalCnt = 0;
     int successCnt = 0;
     int failCnt = 0;
 
@@ -82,7 +79,7 @@ public class ServiceApiASServiceImpl
       asTransLogs1.addAll( AfterServiceResultForm.createMaps1( afterService1 ) );
     }
 
-    totalCnt = asTransLogs1.size();
+    // totalCnt = asTransLogs1.size();
 
     for ( int i = 0; i < asTransLogs1.size(); i++ ) {
       Map<String, Object> insApiresult = asTransLogs1.get( i );
@@ -120,51 +117,56 @@ public class ServiceApiASServiceImpl
   }
 
   @Override
-  public ResponseEntity<ASFailJobRequestDto> asFailJobRequest( ASFailJobRequestForm asFailJobRequestForm )
-    throws Exception {
+  public ResponseEntity<ASFailJobRequestDto> asFailJobRequest( ASFailJobRequestForm asFailJobRequestForm ) throws Exception {
     Map<String, Object> params = ASFailJobRequestForm.createMaps( asFailJobRequestForm );
     String serviceNo = String.valueOf( params.get( "serviceNo" ) );
+
     logger.debug( "==================================[MB]AS FAIL JOB REQUEST ====================================" );
     logger.debug( "### AS FAIL JOB REQUEST FORM : " + params.toString() );
     logger.debug( "==================================[MB]AS FAIL JOB REQUEST ====================================" );
+
     // INSERT LOG HISTORY (SVC0041T)(REQUIRES_NEW)
     if ( RegistrationConstants.IS_INSERT_HSFAIL_LOG ) {
       try {
         MSvcLogApiService.saveAsFailServiceLogs( params );
-      }
-      catch ( Exception e ) {
+      } catch ( Exception e ) {
         e.printStackTrace();
       }
     }
+
     try {
       serviceApiASDetailService.asFailJobRequestProc( params );
-    }
-    catch ( Exception e ) {
+    } catch ( Exception e ) {
       throw new ApplicationException( AppConstants.FAIL, "Fail" );
     }
+
     return ResponseEntity.ok( ASFailJobRequestDto.create( serviceNo ) );
   }
 
+  @SuppressWarnings("static-access")
   @Override
-  public ResponseEntity<AfterServiceResultDto> asDtResult( List<AfterServiceResultForm> afterServiceForms )
-    throws Exception {
+  public ResponseEntity<AfterServiceResultDto> asDtResult( List<AfterServiceResultForm> afterServiceForms ) throws Exception {
     String transactionId = "";
     String serviceNo = "";
-    List<Map<String, Object>> asTransLogs = null;
+
+    // List<Map<String, Object>> asTransLogs = null;
     List<Map<String, Object>> asTransLogs1 = null;
-    int totalCnt = 0;
+    // int totalCnt = 0;
     int successCnt = 0;
     int failCnt = 0;
+
     // INSERT DATA FROM MOBILE INTO LOG TABLE
-    logger.debug( "==================================[MB]AFTER SERVICE RESULT - START - ====================================" );
+    logger.debug( "==================================[MB] HOMECARE AFTER SERVICE RESULT - START - ====================================" );
     logger.debug( "### INSERT HEART LOG? : {}" + RegistrationConstants.IS_INSERT_AS_LOG );
     logger.debug( "### TRANSACTION ID? : {}" + RegistrationConstants.IS_INSERT_AS_LOG );
     logger.debug( "### AS FORM : {}" + afterServiceForms );
+
     asTransLogs1 = new ArrayList<>();
     for ( AfterServiceResultForm afterService1 : afterServiceForms ) {
       asTransLogs1.addAll( afterService1.createMaps1( afterService1 ) );
     }
-    totalCnt = asTransLogs1.size();
+
+    // totalCnt = asTransLogs1.size();
     logger.debug( "### AS TRANSACTION TOTAL : " + asTransLogs1.size() );
     for ( int i = 0; i < asTransLogs1.size(); i++ ) {
       logger.debug( "### AS TRANSACTION DETAILS : " + asTransLogs1.get( i ) );
@@ -175,9 +177,8 @@ public class ServiceApiASServiceImpl
       try {
         serviceApiASDetailService.asDtResultProc( insApiresult );
         successCnt = successCnt + 1;
-      }
-      catch ( BizException bizException ) {
-        Map<String, Object> m = new HashMap();
+      } catch ( BizException bizException ) {
+        Map<String, Object> m = new HashMap<String, Object>();
         m.put( "APP_TYPE", "AS" );
         m.put( "SVC_NO", serviceNo );
         m.put( "ERR_CODE", bizException.getErrorCode() );
@@ -187,9 +188,8 @@ public class ServiceApiASServiceImpl
         MSvcLogApiService.insert_SVC0066T( m );
         failCnt = failCnt + 1;
         throw new ApplicationException( AppConstants.FAIL, bizException.getProcMsg() );
-      }
-      catch ( Exception exception ) {
-        Map<String, Object> m = new HashMap();
+      } catch ( Exception exception ) {
+        Map<String, Object> m = new HashMap<String, Object>();
         m.put( "APP_TYPE", "AS" );
         m.put( "SVC_NO", serviceNo );
         m.put( "ERR_CODE", "01" );
@@ -201,7 +201,7 @@ public class ServiceApiASServiceImpl
         throw new ApplicationException( AppConstants.FAIL, "Fail" );
       }
     }
-    logger.debug( "==================================[MB]AFTER SERVICE RESULT - END - ====================================" );
+    logger.debug( "==================================[MB] HOMECARE AFTER SERVICE RESULT - END - ====================================" );
     return ResponseEntity.ok( AfterServiceResultDto.create( transactionId ) );
   }
 }
