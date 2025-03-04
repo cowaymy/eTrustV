7d62ab240aa349a60e60f9021e83cf61c17d599e
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2025-03-04 08:40:17 +0000

    File Restructure

--- src/main/java/com/coway/trust/api/mobile/services/installation/InstallationJobForm.java
+++ src/main/java/com/coway/trust/api/mobile/services/installation/InstallationJobForm.java
@@ -8,45 +8,32 @@ import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "InstallationJobForm", description = "공통코드 Form")
 public class InstallationJobForm {
+  @ApiModelProperty(value = "USER ID")
+  private String userId;
 
-	@ApiModelProperty(value = "userId [default : '' 전체] 예) CT100559 ", example = "1, 2, 3")
-	private String userId;
+  @ApiModelProperty(value = "REQUEST DATE")
+  private String requestDate;
 
-	@ApiModelProperty(value = "requestDate [default : '' 전체] 예) 201706", example = "1, 2, 3")
-	private String requestDate;
-	
-	
-	
-	public static Map<String, Object> createMap(InstallationJobForm InstallationJobForm) {
-		Map<String, Object> params = new HashMap<>();
-		params.put("userId", InstallationJobForm.getUserId());
-		params.put("requestDate", InstallationJobForm.getRequestDate());
-		return params;
-	} 
-	
-	
-	
-	public String getUserId() {
-		return userId;
-	}
+  public static Map<String, Object> createMap( InstallationJobForm InstallationJobForm ) {
+    Map<String, Object> params = new HashMap<>();
+    params.put( "userId", InstallationJobForm.getUserId() );
+    params.put( "requestDate", InstallationJobForm.getRequestDate() );
+    return params;
+  }
 
+  public String getUserId() {
+    return userId;
+  }
 
+  public void setUserId( String userId ) {
+    this.userId = userId;
+  }
 
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
+  public String getRequestDate() {
+    return requestDate;
+  }
 
-
-
-	public String getRequestDate() {
-		return requestDate;
-	}
-
-
-
-	public void setRequestDate(String requestDate) {
-		this.requestDate = requestDate;
-	}
-
-	
+  public void setRequestDate( String requestDate ) {
+    this.requestDate = requestDate;
+  }
 }
