82b621dfe7512bbd41fff22dd04e7831af8bfdc4
Wayne, Ong Hoong Cheung <wayne.ong@coway.com.my>
2025-03-11 01:38:44 +0000

    File Restructure

--- src/main/java/com/coway/trust/api/mobile/services/productRetrun/ProductReturnResultDto.java
+++ src/main/java/com/coway/trust/api/mobile/services/productRetrun/ProductReturnResultDto.java
@@ -1,31 +1,24 @@
 package com.coway.trust.api.mobile.services.productRetrun;
 
-import com.coway.trust.api.mobile.services.installation.InstallationResultDto;
-
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(value = "ProductReturnResultDto", description = "ProductReturnResultDto")
 public class ProductReturnResultDto {
+  @ApiModelProperty(value = "결과값")
+  private String result;
 
-	@ApiModelProperty(value = "결과값")
-	private String result;
+  public String getResult() {
+    return result;
+  }
 
-	public String getResult() {
-		return result;
-	}
+  public void setResult( String result ) {
+    this.result = result;
+  }
 
-	public void setResult(String result) {
-		this.result = result;
-	}
-	
-	
-	public static ProductReturnResultDto create(String result) {
-		ProductReturnResultDto dto = new ProductReturnResultDto();
-		dto.setResult(result);
-		
-		return dto;
-	}
-	
-	
+  public static ProductReturnResultDto create( String result ) {
+    ProductReturnResultDto dto = new ProductReturnResultDto();
+    dto.setResult( result );
+    return dto;
+  }
 }
